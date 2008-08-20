955a79d3ab517bafedf56659e5b354821b1714c0
Hilary James Oliver <h.oliver@niwa.co.nz>
2008-08-21 11:35:54 +1200

    moved function defs to top of main file

--- controller.py
+++ controller.py
@@ -1,79 +1,97 @@
 #!/usr/bin/python
 
 from system_status import system_status
-from vtasks_dummy import *
 from time import sleep
 from reference_time import reference_time 
+from vtasks_dummy import *
 
 import Pyro.core
 import Pyro.naming
 import os
+import sys
 
 """
 ========= ECOCONNECT CONTROLLER WITH IMPLICIT SCHEDULING ===============
          See repository documentation for more information.
 """
 
-cycle_time = reference_time( "2008053112" )
-
-# Start the pyro nameserver before running this program. There are
-# various ways to do this, with various options.  
-# See http://pyro.sourceforge.net/manual/5-nameserver.html
-
-daemon = Pyro.core.Daemon()
-ns = Pyro.naming.NameServerLocator().getNS()
-daemon.useNameServer(ns)
-
-status = system_status() 
-uri = daemon.connect( status, "system_status" )
-
-tasks = []
 def create_tasks():
-    del tasks[:]
-    tasks.append( A( cycle_time )) 
-    tasks.append( B( cycle_time ))
-    tasks.append( C( cycle_time ))
-    tasks.append( D( cycle_time )) 
+    del task_list[:]
+    task_list.append( A( reference_time )) 
+    task_list.append( B( reference_time ))
+    task_list.append( C( reference_time ))
+    task_list.append( D( reference_time )) 
 
-    tasks.append( Z( cycle_time )) 
+    task_list.append( Z( reference_time )) 
 
-    tasks.append( E( cycle_time ))
-    tasks.append( F( cycle_time ))
+    task_list.append( E( reference_time ))
+    task_list.append( F( reference_time ))
 
-    for task in tasks:
+    for task in task_list:
         uri = daemon.connect( task, task.identity() )
 
-create_tasks()
 
-# Launch any tasks now that have no prerequisites.  Thereafter, things
-# happen only as a result of task state changes via pyro messages.
-for task in tasks:
-    task.run_if_satisfied()
 
 def process_tasks():
-
     finished = []
     status.reset()
 
-    status.update( cycle_time.to_str() )
-    #print cycle_time.to_str(), spnr.spin()
+    status.update( reference_time.to_str() )
 
-    for task in tasks:
-        task.get_satisfaction( tasks )
+    for task in task_list:
+        task.get_satisfaction( task_list )
         task.run_if_satisfied()
-        #print task.get_status()
         status.update( task.get_status() )
         finished.append( task.finished )
 
     status.update_finished() 
 
     if not False in finished:
-        cycle_time.increment()
-        print "NEW REFERENCE TIME: " + cycle_time.to_str()
+        reference_time.increment()
+        print "NEW REFERENCE TIME: " + reference_time.to_str()
         create_tasks()
-
 
     return 1
 
-# process tasks each time a request is handled
+
+# Start the pyro nameserver before running this program. 
+# See http://pyro.sourceforge.net/manual/5-nameserver.html
+# for other ways to do this.
+daemon = Pyro.core.Daemon()
+ns = Pyro.naming.NameServerLocator().getNS()
+daemon.useNameServer(ns)
+
+# one command line argument: initial reference time 
+if len( sys.argv ) != 2:
+    print "USAGE:", sys.argv[0], "<REFERENCE_TIME>"
+    sys.exit(1)
+
+reference_time = reference_time( sys.argv[1] )
+    
+print 
+print "*** EcoConnect Controller Task Manager Startup ***"
+print "    Initial Reference Time " + reference_time.to_str()
+print
+
+# create a system status monitor and connect it to the pyro nameserver
+status = system_status()
+uri = daemon.connect( status, "system_status" )
+
+# create initial task objects and connect them to the pyro nameserver
+task_list = []
+create_tasks()
+
+# Process once to start one or more tasks that have no prerequisites
+# (otherwise nothing will happen; subsequently things only happen only
+# when a running task gets a message via pyro). 
+process_tasks()
+
+# process tasks again each time a request is handled
 daemon.requestLoop( process_tasks )
+
+# NOTE: this seems the easies way to handle incoming pyro calls
+# AND run our task processing at the same time, but I might be 
+# using requestLoop's "condition" argument in an unorthodox way.
+# See pyro docs, as there are other ways to do this, if necessary.
+# E.g. use "handleRequests()" instead of "requestLoop", with a 
+# timeout that drops into our task processing loop.
