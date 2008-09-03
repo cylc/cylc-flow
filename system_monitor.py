cd570d8c2974850ca63b1a317dfe291790b04fa6
Hilary James Oliver <h.oliver@niwa.co.nz>
2008-09-03 13:48:52 +1200

    separate reference time groups in system monitor

--- system_monitor.py
+++ system_monitor.py
@@ -3,8 +3,8 @@
 """
 Simple system monitor program.
 
-It connects to the controller's "state" system_status object via the
-Pyro nameserver, and displays the contents therein in a terminal.
+It connects to the controller's "state" object via Pyro,
+and displays its contents in a terminal.
 
 See system_status.py for class documentation.
 """
@@ -19,19 +19,15 @@ from string import ljust, rjust, split
 
 foo = spinner()
 
-indent = "     "
+def func( rt ):
+    return int( rt )
 
-def sorted_keys( d, func = None ):
-    keys = d.keys()
-    keys.sort( func )
-    return keys
-
-def heading():
+def print_heading():
     os.system( "clear" )
     char = foo.spin()
 
     print 
-    print indent + "\033[34m" + char + " System Monitor " + char + "\033[0m"
+    print "\033[34m " + char + " System Monitor " + char + "\033[0m"
     print
 
 while True:
@@ -45,9 +41,9 @@ while True:
             max_name_len = 0
             max_state_len = 0
             max_total_len = 0
-            lines = []
+            lines = {}
 
-            for task_id in sorted_keys( status ):
+            for task_id in status.keys():
                 [name, reftime] = split( task_id, "_" )
                 [state, complete, total] = status[ task_id ]
 
@@ -60,7 +56,7 @@ while True:
                 if len( state ) > max_state_len:
                     max_state_len = len( state )
 
-            for task_id in sorted_keys( status ):
+            for task_id in status.keys():
 
                 [name, reftime] = split( task_id, "_" )
                 [state, complete, total] = status[ task_id ]
@@ -83,22 +79,34 @@ while True:
                     ctrl_start = "\033[0m"         # default
 
                 state = ljust( state, max_state_len + 1 )
-
                 name = ljust( name, max_name_len + 1 )
-
                 frac = rjust( complete + "/" + total, 2 * max_total_len + 1 )
+                line = ctrl_start + " " + name + state + " " + frac + " " + prog + ctrl_end 
 
+                if reftime in lines.keys(): 
+                    lines[ reftime ].append( line )
+                else:
+                    lines[ reftime ] = [ line ]
 
-                lines.append( ctrl_start + " " + name + "(" + reftime + ") " + state + " " + frac + " " + prog + ctrl_end )
+            # sort reference times using int( string )
+            reftimes = lines.keys()
+            reftimes.sort( key = int )
 
-            heading()
-            for line in lines:
-                print line
+            print_heading()
+
+            for rt in reftimes:
+                print "\033[35m ** " + rt + " ** \033[0m"
+                print ""
+
+                for line in lines[rt]:
+                    print line
+
+                print ""
 
             sleep(1)
 
     except:
-        heading()
+        print_heading()
         print "Connection to nameserver failed ..."
 
     sleep(1)  
