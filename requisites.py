1128669893531bcf70e5dfb236a620c066c0b2f2
Hilary James Oliver <h.oliver@niwa.co.nz>
2008-10-02 19:26:41 +1200

    had to override will_satisfy_me() for fuzzy runahead case (topnet) to get dead soldier removal completely right.

--- requisites.py
+++ requisites.py
@@ -79,7 +79,7 @@ class requisites:
                 # if my prerequisite is not already satisfied
                 for postreq in postreqs.satisfied.keys():
                     # compare it with each of the other's postreqs
-                    if postreq == prereq:
+                    if postreq == prereq:   # (DIFFERENT FROM ABOVE HERE)
                         # if they match, my prereq has been satisfied
                         self.set_satisfied( prereq )
 
@@ -133,3 +133,32 @@ class fuzzy_requisites( requisites ):
                                 #print "FUZZY PREREQ: " + prereq
                                 #print "SATISFIED BY: " + postreq
                                 self.sharpen_up( prereq, postreq )
+
+    def will_satisfy_me( self, postreqs ):
+        # will another's postreqs, when completed, satisfy any of my prequisites?
+        for prereq in self.satisfied.keys():
+            # for each of my prerequisites
+            if not self.satisfied[ prereq ]:
+                # if my prerequisite is not already satisfied
+
+                # extract reference time from my prerequisite
+                m = re.compile( "^(.*)(\d{10})(.*)$").match( prereq )
+                if not m:
+                    print "FAILED TO MATCH REF TIME IN " + prereq
+                    sys.exit(1)
+
+                [ my_start, my_reftime, my_end ] = m.groups()
+
+                for postreq in postreqs.satisfied.keys():
+
+                    # extract reference time from other's postrequisite
+                    m = re.compile( "^(.*)(\d{10})(.*)$").match( postreq )
+                    if not m:
+                        print "FAILED TO MATCH REF TIME IN " + postreq
+                        sys.exit(1)
+
+                    [ other_start, other_reftime, other_end ] = m.groups()
+
+                    if other_start == my_start and other_end == my_end:
+                        if other_reftime >= my_reftime:
+                            self.sharpen_up( prereq, postreq )
