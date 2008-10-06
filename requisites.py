7db7b00e9d1aa159d2e689518503e74badee0175
Hilary James Oliver <h.oliver@niwa.co.nz>
2008-10-07 11:11:14 +1200

    fuzzy prequisites: >=

--- requisites.py
+++ requisites.py
@@ -141,7 +141,7 @@ class fuzzy_requisites( requisites ):
                         [ other_start, other_reftime, other_end ] = m.groups()
 
                         if other_start == my_start and other_end == my_end:
-                            if other_reftime > my_reftime:
+                            if other_reftime >= my_reftime:
                                 #print "FUZZY PREREQ: " + prereq
                                 #print "SATISFIED BY: " + postreq
                                 self.sharpen_up( prereq, postreq )
