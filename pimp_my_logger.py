a8e7c8720ffd249775c3b01457d62e584c46e441
Hilary James Oliver <h.oliver@niwa.co.nz>
2008-10-22 10:47:04 +1200

    reinstate task log name column in main log

--- pimp_my_logger.py
+++ pimp_my_logger.py
@@ -23,28 +23,29 @@ def pimp_it( log, name, dummy_clock = None ):
     log.setLevel( config.logging_level )
     max_bytes = 1000000
     backups = 5
-    logfile = 'LOGFILES/' + name
+    logfile = config.logging_dir + '/' + name
     h = logging.handlers.RotatingFileHandler( logfile, 'a', max_bytes, backups )
     # the above creates a zero-sized log file if one doesn't already exist
     if os.path.getsize( logfile ) > 0:
         print ' + rotating existing log:', logfile
         h.doRollover()
 
+    originator = ""
     if name == "main":
-        width = 20
-    else:
-        width = len( name ) + 2
+        originator = '%(name)-20s'
 
-    f = logging.Formatter( '%(asctime)s %(levelname)-8s %(name)-'+str(width)+'s - %(message)s', '%Y/%m/%d %H:%M:%S' )
-    # use '%(name)-30s' to get the logger name print too 
+    f = logging.Formatter( '%(asctime)s %(levelname)-8s ' + originator + ' - %(message)s', '%Y/%m/%d %H:%M:%S' )
+
+    if name == "main":
+        # write warnings and worse to stderr as well as to the log
+        h2 = logging.StreamHandler(sys.stderr)
+        h2.setLevel( logging.WARNING )
+        h2.setFormatter( f )
+        log.addHandler( h2 )
+
     h.setFormatter(f)
     log.addHandler(h)
 
-    # write warnings and worse to stderr as well as to the log
-    h2 = logging.StreamHandler(sys.stderr)
-    h2.setLevel( logging.WARNING )
-    h2.setFormatter( f )
-    log.addHandler(h2)
     if config.dummy_mode:
         # replace logged real time with dummy clock time 
         log.addFilter( LogFilter( dummy_clock, "main" ))
