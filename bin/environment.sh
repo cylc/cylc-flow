0ab2307c209a9136f7edff0cc35f1c803da567d6
Hilary James Oliver <h.oliver@niwa.co.nz>
2009-04-02 10:19:46 +1200

    replace sequenz front end wrapper with separate env script

---
+++ bin/environment.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# set PATH and PYTHONPATH for sequenz 
+
+# CAN BE MODIFIED BY DEPLOYMENT SYSTEM AT INSTALL TIME
+# ACCORDING TO WHERE SEQUENZ MODULES ARE INSTALLED TO.
+
+export PATH=$HOME/sequenz-dev/bin:$PATH
+export PYTHONPATH=$HOME/sequenz-dev/src:$HOME/sequenz-dev/example:$PYTHONPATH
