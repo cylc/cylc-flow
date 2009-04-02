6d5d2743ec0b57c25ee5f48ae46e4c9dcad8a9fa
Hilary James Oliver <h.oliver@niwa.co.nz>
2009-04-02 16:49:43 +1200

    oper paths in environment script

--- bin/environment.sh
+++ bin/environment.sh
@@ -5,5 +5,10 @@
 # CAN BE MODIFIED BY DEPLOYMENT SYSTEM AT INSTALL TIME
 # ACCORDING TO WHERE SEQUENZ MODULES ARE INSTALLED TO.
 
-export PATH=$HOME/sequenz-dev/bin:$HOME/sequenz-dev/example/tasks:$PATH
-export PYTHONPATH=$HOME/sequenz-dev/src:$HOME/sequenz-dev/example:$PYTHONPATH
+# example
+#export PATH=$HOME/sequenz-dev/bin:$HOME/sequenz-dev/example/tasks:$PATH
+#export PYTHONPATH=$HOME/sequenz-dev/src:$HOME/sequenz-dev/example:$PYTHONPATH
+
+# ecoconnect operational
+export PATH=$HOME/sequenz-dev/bin:$PATH
+export PYTHONPATH=$HOME/sequenz-dev/src:$HOME/sequenz-dev/ecoconnect/operational:$PYTHONPATH
