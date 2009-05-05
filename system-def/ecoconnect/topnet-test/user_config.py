ee2aad95da7fd694306ac7a2316278892b381dd6
Hilary James Oliver <h.oliver@niwa.co.nz>
2009-05-05 15:25:16 +1200

    go to real mode for topnet testing again

--- system-def/ecoconnect/topnet-test/user_config.py
+++ system-def/ecoconnect/topnet-test/user_config.py
@@ -9,19 +9,20 @@ config = {}
 ####################################### <<<<
 
 config[ 'system_name' ] = 'topnet-test'
+#config[ 'state_dump_file' ] = 'blah'
 
 config[ 'start_time' ] = '2009030200'
 #config[ 'stop_time' ] = '2009030300'
 
-config[ 'logging_level' ] = logging.DEBUG
+config[ 'logging_level' ] = logging.INFO
 
 #config[ 'use_broker' ] = False
 
-config[ 'dummy_mode' ] = True
+config[ 'dummy_mode' ] = False
 config[ 'dummy_clock_rate' ] = 10
 config[ 'dummy_clock_offset' ] = 10 
 
-config[ 'use_qsub' ] = False
+config[ 'use_qsub' ] = True
 config[ 'job_queue' ] = 'topnet_test'
 
 config[ 'task_list' ] = [ 
