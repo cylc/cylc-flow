a2ebf6270d0e407d8eb46372d0ecc017ac7a35be
Hilary James Oliver <h.oliver@niwa.co.nz>
2009-05-22 15:14:57 +1200

    restored topnet test settings after pull

--- system-def/ecoconnect/topnet-test/user_config.py
+++ system-def/ecoconnect/topnet-test/user_config.py
@@ -8,21 +8,22 @@ import logging  # for logging level
 config = {}
 ####################################### <<<<
 
-config[ 'system_name' ] = 'topnet-dev'
+config[ 'system_name' ] = 'topnet-test'
 #config[ 'state_dump_file' ] = 'blah'
 
 config[ 'start_time' ] = '2009030200'
 #config[ 'stop_time' ] = '2009030300'
 
-config[ 'logging_level' ] = logging.INFO
+#config[ 'logging_level' ] = logging.INFO
+config[ 'logging_level' ] = logging.DEBUG
 
 #config[ 'use_broker' ] = False
 
-config[ 'dummy_mode' ] = True
+config[ 'dummy_mode' ] = False
 config[ 'dummy_clock_rate' ] = 20
 config[ 'dummy_clock_offset' ] = 20 
 
-config[ 'use_qsub' ] = False
+config[ 'use_qsub' ] = True
 config[ 'job_queue' ] = 'topnet_test'
 
 config[ 'task_list' ] = [ 
