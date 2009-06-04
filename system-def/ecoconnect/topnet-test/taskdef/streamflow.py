dbdb8740e926e2d07ec4c6ab72f81e30936bcd0c
Hilary James Oliver <h.oliver@niwa.co.nz>
2009-06-04 14:48:14 +1200

    conflict res

--- system-def/ecoconnect/topnet-test/taskdef/streamflow.py
+++ system-def/ecoconnect/topnet-test/taskdef/streamflow.py
@@ -34,11 +34,24 @@ class streamflow( parallel_task ):
         
         # but intercept messages to do with catchup mode
         if self.catchup_re.match( message ):
-            self.log.debug( 'in catching up mode' )
-            streamflow.catchup_mode = True
-            self.MAX_FINISHED = 13
+            # message says we're catching up to real time
+            if not self.catchup_mode:
+                # We were caught up and have apparently slipped back a
+                # bit. Do NOT revert to catching up mode because this
+                # will suddenly reduce topnet's cutoff time
+                # and may result in deletion of a finished nzlam task
+                # that is still needed to satsify topnet prerequisites
+                self.log.debug( 'falling behind the pace a bit here' )
+            else:
+                # We were already catching up; no change.
+                pass
 
         elif self.uptodate_re.match( message ):
-            self.log.debug( 'in caught up mode' )
-            streamflow.catchup_mode = False
-            self.MAX_FINISHED = 25
+            # message says we've caught up to real time
+            if not self.catchup_mode:
+                # were already caught up; no change
+                pass
+            else:
+                # we have just caught up
+                self.log.debug( 'just caught up to real time' )
+                streamflow.catchup_mode = False
