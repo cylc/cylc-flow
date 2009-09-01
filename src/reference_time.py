54ebd51cc915841f95aaf0c8cfe999d263d27e08
Hilary James Oliver <h.oliver@niwa.co.nz>
2009-09-02 09:46:46 +1200

    reference time differencing function

--- src/reference_time.py
+++ src/reference_time.py
@@ -8,6 +8,8 @@ increment themselves, etc. But now that the controller does not have a
 global reference time a procedural module will do.
 """
 
+# do logical comparisons of reference times in integer form: int( rt )
+
 import datetime
 import re
 
@@ -27,7 +29,18 @@ def decrement( rt, hours ):
         dt = _rt_to_dt( rt )
         return _dt_to_rt( dt - datetime.timedelta( 0, 0, 0, 0, 0, hours, 0 ) )
 
-# do rt comparisons in integer form via int( rt )
+def diff_hours( rt2, rt1 ):
+    # rt2 - rt1 in hours
+    dt2 = _rt_to_dt( rt2 )
+    dt1 = _rt_to_dt( rt1 )
+
+    delta = dt2 - dt1
+
+    if delta.microseconds != 0:
+        print "WARNING: reference_time.difference_hours(): unexpected timedelta"
+
+    return delta.days * 24 + delta.seconds/3600
+
 
 def is_valid( rt ):
     if re.compile( "^\d{10}$" ).match( rt ):
