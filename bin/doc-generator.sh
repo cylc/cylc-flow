e9d7ece100623f6e1e125ba251a4468308ea65ce
Hilary James Oliver <h.oliver@niwa.co.nz>
2009-09-24 10:21:36 +1200

    better doc generator script

--- bin/doc-generator.sh
+++ bin/doc-generator.sh
@@ -4,17 +4,40 @@ set -e
 
 # generate pdf documentation from cyclon LaTeX source
 
-[[ $# != 0 ]] && {
-	echo "USAGE: $0"
-	echo "run in cyclon top level directory"
+usage() {
+	echo "USAGE: [-f] $0"
+    echo ' -f, force re-conversion of eps figures to PDF'
+	echo "Run script in the cyclon top level directory"
+}
+
+[[ $# > 1 ]] && {
+    usage
 	exit 1
 }
 
+FORCE=false
+if [[ $# == 1 ]]; then 
+    if [[ $1 = '-f' ]]; then
+        FORCE=true
+    else
+        usage
+        exit 1
+    fi
+fi
 
 [[ ! -f bin/cyclon ]] && {
-	echo "RUN THIS IN CYCLON REPO TOP LEVEL"
+	echo "RUN THIS SCRIPT IN THE CYCLON TOP LEVEL DIRECTORY"
+    usage
 	exit 1
 }
 
 cd doc
+
+for F in *.eps; do
+    [[ ! -f ${F%eps}pdf ]] || $FORCE && {
+        echo converting $F to PDF
+        epstopdf $F
+    }
+done
+
 pdflatex cyclon.tex
