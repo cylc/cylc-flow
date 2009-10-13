f6856ae028b0e44a4a964cff55b937a997a32a7d
Hilary James Oliver <h.oliver@niwa.co.nz>
2009-10-13 13:47:01 +1200

    global exception catch on remote insert function

--- src/manager.py
+++ src/manager.py
@@ -521,19 +521,16 @@ class manager:
             self.log.warning( "task to reset not found: " + task_id )
 
     def insertion( self, ins ):
+        # for remote insertion of a new task, or task group
+
         try:
-            # insert a new task or task group in a waiting state
 
             if re.match( '^GROUP:', ins ):
                 # task group
                 [ junk, group ] = ins.split(':')
                 [ groupname, ref_time ] = group.split( '%' )
 
-                #try:
                 tasknames = self.config.get( 'task_groups')[groupname]
-                #except KeyError:
-                #    self.log.warning( 'insertion group ' + groupname + ' not defined' )
-                #    return
 
                 ids = []
                 for name in tasknames:
@@ -547,39 +544,37 @@ class manager:
                 [ name, ref_time ] = task_id.split( '%' )
 
                 # instantiate the task object
-                try:
-                    itask = self.get_task_instance( 'task_classes', name )( self.clock, ref_time )
+                itask = self.get_task_instance( 'task_classes', name )( self.clock, ref_time )
 
-                except AttributeError:
-                    print 'ERROR: task class', name, 'not found'
-
-                else:
-                    if itask.instance_count == 1:
-                        # first task of its type, so create the log
-                        log = logging.getLogger( 'main.' + name )
-                        pimp_my_logger.pimp_it( log, name, self.config, self.dummy_mode, self.clock )
+                if itask.instance_count == 1:
+                    # first task of its type, so create the log
+                    log = logging.getLogger( 'main.' + name )
+                    pimp_my_logger.pimp_it( log, name, self.config, self.dummy_mode, self.clock )
  
-                    # the initial task reference time can be altered during
-                    # creation, so we have to create the task before
-                    # checking if stop time has been reached.
-                    skip = False
-                    if self.stop_time:
-                        if int( itask.ref_time ) > int( self.stop_time ):
-                            itask.log( 'WARNING', " STOPPING at " + self.stop_time )
-                            itask.prepare_for_death()
-                            del itask
-                            skip = True
+                # the initial task reference time can be altered during
+                # creation, so we have to create the task before
+                # checking if stop time has been reached.
+                skip = False
+                if self.stop_time:
+                    if int( itask.ref_time ) > int( self.stop_time ):
+                        itask.log( 'WARNING', " STOPPING at " + self.stop_time )
+                        itask.prepare_for_death()
+                        del itask
+                        skip = True
 
-                    if not skip:
-                        itask.log( 'DEBUG', "connected" )
-                        self.pyro.connect( itask, itask.get_identity() )
-                        self.tasks.append( itask )
+                if not skip:
+                    itask.log( 'DEBUG', "connected" )
+                    self.pyro.connect( itask, itask.get_identity() )
+                    self.tasks.append( itask )
 
         except:
-            # a failed remote insertion should not bring the system 
-            # down for any reason (badly formatted input or whatever).
+            # global exception catch: a failed remote insertion should
+            # not bring the system down for any reason. This catches
+            # requests to insert undefined tasks and task groups, and 
+            # malformed input, etc.
             print 
-            print 'WARNING: INSERTION FAILED; reason:'
+            print 'WARNING: TASK INSERTION FAILED'
+            print 'continuing after traceback:'
             traceback.print_exc()
             print 
 
