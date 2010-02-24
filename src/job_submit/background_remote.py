e1fae181443c888b21e8f54f030bdf3d650eb030
Hilary James Oliver <h.oliver@niwa.co.nz>
2010-02-24 14:51:13 +1200

    added comments to remote bkg submit class

--- src/job_submit/background_remote.py
+++ src/job_submit/background_remote.py
@@ -5,8 +5,11 @@ from job_submit import job_submit
 
 # invoke task on remote machine and exit immediately
 # ssh normally waits for remote process to end, but we can avoid this by
-# redirecting stdin to /dev/null, stdout to /dev/null, stderr to stdout, 
-# and backgrounding.
+# redirecting stdin to /dev/null, stdout to /dev/null or a JOB LOG FILE, 
+# stderr to stdout, and backgrounding.
+
+# NOTE: for multiple commands separated by ';'s, enclose in parentheses:
+# ssh host '(echo one; echo two) </dev/null >/dev/null 2>&1 &'
 
 class background_remote( job_submit ):
 
