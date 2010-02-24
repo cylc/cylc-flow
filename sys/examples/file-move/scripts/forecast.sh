88949def773b18cd71fb66d0a7fc9f0030e5e57e
Hilary James Oliver <h.oliver@niwa.co.nz>
2010-02-24 13:32:46 +1200

    added a new example system that moves files around

---
+++ sys/examples/file-move/scripts/forecast.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# cylc example system, task forecast
+# depends on task ext and its own restart file.
+
+# run length 10 minutes, one restart file
+
+cylc message started
+
+ACCEL=$(( 3600 / 10 )) # 10 s => 1 hour
+SLEEP=$(( 10 * 60 / ACCEL ))
+
+# check prerequistes
+ONE=$TMPDIR/ext/output/$CYCLE_TIME/extdata
+TWO=$TMPDIR/$TASK_NAME/running/$CYCLE_TIME/restart
+for PRE in $ONE $TWO; do
+    [[ ! -f $PRE ]] && {
+        MSG="file not found: $PRE"
+        echo "ERROR, forecast: $MSG"
+        cylc message -p CRITICAL $MSG
+        cylc message -p CRITICAL failed
+        exit 1
+    }
+done
+
+sleep $SLEEP # 90 min
+
+RUNDIR=$TMPDIR/$TASK_NAME/running/$NEXT_CYCLE_TIME
+mkdir -p $RUNDIR
+touch $RUNDIR/restart
+cylc message $TASK_NAME restart files ready for $NEXT_CYCLE_TIME
+
+OUTDIR=$TMPDIR/$TASK_NAME/output/$CYCLE_TIME
+mkdir -p $OUTDIR
+touch $OUTDIR/forecast.nc
+cylc message "forecast output ready for $CYCLE_TIME"
+
+cylc message finished
