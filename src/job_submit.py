1f570299ac624d93e6d88f0d74474e4d79a489d1
Hilary James Oliver <h.oliver@niwa.co.nz>
2010-02-24 13:28:48 +1200

    allow environment variable interpolation in extra_vars

--- src/job_submit.py
+++ src/job_submit.py
@@ -6,6 +6,7 @@
 # the job_submit sub-directory of the main cylc installation or of the
 # task definition directories for specific cylc systems.
 
+import re
 import os
 import sys
 import subprocess
@@ -23,12 +24,34 @@ class job_submit:
         print >> sys.stderr, 'ERROR: use a job submission derived class'
         sys.exit(1)
 
+    def interpolate( self, string ):
+
+        # $VARNAME
+        m = re.findall( "\$([a-zA-Z0-9_]+)", string )
+        for var in m:
+            if var in os.environ:
+                # replace value with the env value
+                val = os.environ[ var ]
+                string = re.sub( '\$' + var, val, string )
+
+        # ${VARNAME}
+        m = re.findall( "\$\{([a-zA-Z0-9_]+)\}", string )
+        for var in m:
+            if var in os.environ:
+                # replace value with the env value
+                val = os.environ[ var ]
+                string = re.sub( '\$\{' + var + '\}', val, string )
+
+        return string
+
     def set_job_environment( self ):
-        # export cycle time, task name, and task extra vars
+        # export cycle time and task name
         os.environ['CYCLE_TIME'] = self.cycle_time
         os.environ['TASK_NAME'] = self.task_name
+        # and any extra variables
         for entry in self.extra_vars:
             [ var_name, value ] = entry
+            value = self.interpolate( value )
             os.environ[var_name] = value
 
     def submit( self ):
