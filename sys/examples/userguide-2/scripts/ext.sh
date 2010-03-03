a112c7bb50539835ee53255d8fdf169d2399ecdf
Hilary James Oliver <h.oliver@niwa.co.nz>
2010-03-03 19:22:36 +1200

    fix recent bug, userguide-2 example

--- sys/examples/userguide-2/scripts/ext.sh
+++ sys/examples/userguide-2/scripts/ext.sh
@@ -13,8 +13,8 @@ SLEEP=$(( 10 * 60 / ACCEL ))
 
 sleep $SLEEP 
 
-#echo $TMPDIR/${TASK_NAME}_${CYLC_TIME}.output
-touch $TMPDIR/${TASK_NAME}_${CYLC_TIME}.output
+#echo $TMPDIR/${CYLC_TASK}_${CYLC_TIME}.output
+touch $TMPDIR/${CYLC_TASK}_${CYLC_TIME}.output
 cylc message "external data ready for $CYLC_TIME"
 
 cylc message --succeeded
