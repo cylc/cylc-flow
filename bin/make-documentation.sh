687ca45fbd7bcebe7e039f6f03b8d3ffda728d2f
Hilary James Oliver <h.oliver@niwa.co.nz>
2010-03-05 21:57:30 +1200

    took system interrogation options commands out of control and put them into a new command called question.

--- bin/make-documentation.sh
+++ bin/make-documentation.sh
@@ -36,13 +36,14 @@ cylc             help > doc/command-usage/cylc.txt
 cylc configure --help > doc/command-usage/cylc-configure.txt
 cylc register  --help > doc/command-usage/cylc-register.txt
 cylc schedule  --help > doc/command-usage/cylc-scheduler.txt
-cylc control   --help > doc/command-usage/cylc-controller.txt
-cylc run-task  --help > doc/command-usage/cylc-run-task.txt
 cylc message   --help > doc/command-usage/cylc-message.txt
+cylc control   --help > doc/command-usage/cylc-control.txt
+cylc question  --help > doc/command-usage/cylc-question.txt
 cylc monitor   --help > doc/command-usage/monitor.txt
 cylc monitor-r   --help > doc/command-usage/monitor-r.txt
 cylc monitor-d   --help > doc/command-usage/monitor-d.txt
 cylc monitor-p   --help > doc/command-usage/monitor-p.txt
+cylc run-task  --help > doc/command-usage/cylc-run-task.txt
 
 #Comment-stripped taskdef files:
 #perl -e 'while (<>) { if ( ! m/^\s*#/ && ! m/^\s*$/ ) { print }}' < \
