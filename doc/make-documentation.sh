ad3f222ae77f4d3c461ff8caf6cd5f52dd14a203
Hilary James Oliver <h.oliver@niwa.co.nz>
2010-03-15 22:12:38 +1200

    new separate what-is command

--- doc/make-documentation.sh
+++ doc/make-documentation.sh
@@ -31,11 +31,11 @@ if [[ $# == 1 ]]; then
 fi
 
 # extract command help output
-rm -r doc/command-usage; mkdir -p doc/command-usage
+rm -rf doc/command-usage; mkdir -p doc/command-usage
 cylc help > doc/command-usage/cylc-help.txt
 for command in configure register schedule stop pause resume nudge \
-    set-level reset kill purge insert ask message run-task monitor monitor-r \
-    monitor-d monitor-p; do
+    set-level reset kill purge insert task-dump what-is message run-task \
+    monitor monitor-r monitor-d monitor-p; do
     echo "cylc $command --help"
     cylc $command --help > doc/command-usage/cylc-$command.txt
 done
