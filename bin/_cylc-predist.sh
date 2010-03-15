d8403cfc763a28c1282b79b7ec55ba23bcf78eb9
Hilary James Oliver <h.oliver@niwa.co.nz>
2010-03-15 12:34:00 +1200

    mod to predist

--- bin/_cylc-predist.sh
+++ bin/_cylc-predist.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 set -e
+trap "ABORTING PREDIST, DELETE THE NEW TARBALL IF DARCS CONTINUES!" ERR
+
+echo "PRE MARCH 2009 (?) VERSIONS OF DARCS IGNORE NON-ZERO EXIT CODE IN"
+echo "PREDIST SCRIPTS: WATCH FOR THIS AND DELETE THE RESULTING TARBALL!"
 
 # a darcs predist script to:
 #   - set executable permissions
@@ -8,13 +12,20 @@ set -e
 #   - make documentation
 #   - remove documentation source files
 
-# you must export $CYLC_VERSION before running 'darcs dist' in the
-# repository.
+# TO CREATE A CLEAN CYLC DISTRIBUTION IN A NEW REPOSITORY:
+# 1/ record any changes to this script
+# 2/ darcs setpref predist bin/_cylc-predist.sh
+# 3/ final 'darcs tag'
+# 4/ export CYLC_VERSION=[final version tag]
+# 5/ darcs dist
 
 if [[ -z $CYLC_VERSION ]]; then
     echo "\$CYLC_VERSION is not defined"
+    echo "ABORTING PREDIST, DELETE THE NEW TARBALL IF DARCS CONTINUES!"
     exit 1
 fi
+
+echo "SETTING EXECUTABLE PERMISSIONS"
 
 chmod +x bin/*
 
@@ -22,11 +33,14 @@ chmod +x sys/examples/userguide/scripts/*
 chmod +x sys/examples/nested/scripts/*
 chmod +x sys/examples/distributed/scripts/*
 
-perl -pi -e "s/-CYLC-VERSION-/$CYCL_VERSION/" bin/cylc
-perl -pi -e "s/-CYLC-VERSION-/$CYCL_VERSION/" doc/userguide.tex
+echo "SETTING VERSION TAG IN MAIN COMMAND AND USERGUIDE"
+perl -pi -e "s/-CYLC-VERSION-/$CYLC_VERSION/" bin/cylc
+perl -pi -e "s/-CYLC-VERSION-/$CYLC_VERSION/" doc/userguide.tex
 
+echo "MAKING DOCUMENTATION (USERGUIDE)"
 doc/make-documentation.sh
 
+echo "DELETING DOCUMENTATION SOURCE"
 cp doc/userguide.pdf .
 rm -r doc
 mkdir doc
