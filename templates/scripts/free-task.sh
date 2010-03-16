a2e9af4cd6bb7ccb7812face04836882afd4fb30
Hilary James Oliver <h.oliver@niwa.co.nz>
2010-03-17 09:14:15 +1200

    task script template cleanup

--- templates/scripts/free-task.sh
+++ templates/scripts/free-task.sh
@@ -3,41 +3,36 @@
 # THIS IS A CYLC TASK SCRIPT TEMPLATE FOR THE 'FREE' TASK TYPE, I.E.
 # NON-FORECAST MODEL TASKS THAT HAVE NO PREVIOUS-INSTANCE DEPENDENCE.
 
-# START MESSAGE
+# TASK STARTED
 cylc message --started
 
 # CHECK TASK PREREQUISITES
-# For example, set $PREREQUISITES to a list of $CYCLE_TIME dependent
-# input files then: 
-for FILE in $PREREQUISITES; do
-    if [[ ! -f $FILE ]]; then
-        # FAILURE MESSAGE
+# {CODE: define $CYCLE_TIME dependent input files in $PREREQUISITES}
+for PRE in $PREREQUISITES; do
+    if [[ ! -f $PRE ]]; then
+        # TASK FAILED
         cylc message -p CRITICAL "file not found: $PRE"
         cylc message --failed
         exit 1
     fi
 done
 
-# EXECUTE THE GUTS OF THE TASK:
-
-# (1) to call an external script or executable that generates task outputs:
+# NOW EXECUTE THE GUTS OF THE TASK:
+# -- (1) call an EXTERNAL SCRIPT OR EXECUTABLE that generates task outputs:
 if ! $EXTERNAL; then
-    # FAILURE MESSAGE
-    cylc message -p CRITICAL "file not found: $PRE"
+    # TASK FAILED
+    cylc message -p CRITICAL "Error in $EXTERNAL"
     cylc message --failed
     exit 1
 fi
-
-# IF possible the aforementioned external script or executable should
-# report completion of task outputs as it goes, as in (2) below.
-
-# IF it does NOT report completion of task outputs as it goes, do so now
+# -- IF $EXTERNAL does NOT report outputs as it goes, do it now en masse:
 cycl message --all-outputs-completed
-# OR send each output message explicitly as in (2) below.
 
-# (2) for outputs scripted in this file, report each completed as we go:
-# {CODE: COMPLETE A REGISTERED TASK OUTPUT}
+# -- OR (2) for outputs scripted in this file, report as we go:
+   # {CODE: COMPLETE A REGISTERED TASK OUTPUT}
 cylc message "foo files ready for $CYCLE_TIME"
+   # {CODE: COMPLETE ANOTHER REGISTERED TASK OUTPUT}
+cylc message "bar data processing done for $CYCLE_TIME"
 
-# FINISHED MESSAGE
+# TASK FINISHED
 cylc message --succeeded
--- templates/scripts/tied-task.sh
+++ templates/scripts/tied-task.sh
@@ -3,46 +3,40 @@
 # THIS IS A CYLC TASK SCRIPT TEMPLATE FOR THE 'TIED' TASK TYPE: I.E.
 # FORECAST MODELS WITH PREVIOUS-INSTANCE DEPENDENCE VIA RESTART FILES.
 
-# START MESSAGE
+# TASK STARTED
 cylc message --started
 
 # CHECK TASK PREREQUISITES
-# For example, set $PREREQUISITES to a list of $CYCLE_TIME dependent
-# input files then: 
-for FILE in $PREREQUISITES; do
-    if [[ ! -f $FILE ]]; then
-        # FAILURE MESSAGE
-        cylc message -p CRITICAL "file not found: $FILE"
+# {CODE: define $CYCLE_TIME dependent input files in $PREREQUISITES}
+for PRE in $PREREQUISITES; do
+    if [[ ! -f $PRE ]]; then
+        # TASK FAILED
+        cylc message -p CRITICAL "file not found: $PRE"
         cylc message --failed
         exit 1
     fi
 done
 
-# EXECUTE THE GUTS OF THE TASK:
-
-# (1) to call an external script or executable that generates task outputs:
+# NOW EXECUTE THE GUTS OF THE TASK:
+# -- (1) call an EXTERNAL SCRIPT OR EXECUTABLE that generates task outputs:
 if ! $EXTERNAL; then
-    # FAILURE MESSAGE
-    cylc message -p CRITICAL "file not found: $PRE"
+    # TASK FAILED
+    cylc message -p CRITICAL "Error in $EXTERNAL"
     cylc message --failed
     exit 1
 fi
-
-# IF possible the aforementioned external script or executable should
-# report completion of task outputs as it goes, as in (2) below.
-
-# IF it does NOT report completion of task outputs as it goes, do so now
+# -- IF $EXTERNAL does NOT report outputs as it goes, do it now en masse:
 cycl message --all-outputs-completed
-# OR send each output message explicitly as in (2) below.
 
-# (2) for outputs scripted in this file, report each completed as we go:
-   # (i)  {CODE: COMPLETE A REGISTERED non-restart TASK OUTPUT}
+# -- OR (2) for outputs scripted in this file, report as we go:
+   # {CODE: COMPLETE THE RESTART FILE FOR THE NEXT CYCLE}
+cycl message --next-restart-completed
+   # {CODE: COMPLETE THE RESTART FILE FOR THE NEXT NEXT CYCLE}
+cycl message --next-restart-completed
+   # {CODE: COMPLETE A REGISTERED non-restart TASK OUTPUT}
 cylc message "foo files ready for $CYCLE_TIME"
-   # (ii) {CODE: COMPLETE THE RESTART FILE FOR THE NEXT CYCLE}:
-cycl message --next-restart-completed
-   #      {CODE: COMPLETE THE RESTART FILE FOR THE NEXT NEXT CYCLE}:
-cycl message --next-restart-completed
-   # AND SO ON.
+   # {CODE: COMPLETE ANOTHER REGISTERED TASK OUTPUT}
+cylc message "bar data processing done for $CYCLE_TIME"
 
-# FINISHED MESSAGE
+# TASK FINISHED 
 cylc message --succeeded
