6560df0bd9504be2be6c1f6b45bd1d077b2d7ec1
Hilary James Oliver <h.oliver@niwa.co.nz>
2010-03-19 12:51:57 +1200

    better cylc-env script

--- cylc-env.sh
+++ cylc-env.sh
@@ -1,25 +1,33 @@
-#!/bin/bash-nonexistent # <-- prevent accidental non-dot-run!
+#!/bin/bashX # <-- prevent accidental non-dot-run!
 
 # Source (dot-run) this script to configure your shell for THIS cylc.
-# . cylc-env.sh
 
-# You must do this in the top level directory of THIS cylc installation
-# (i.e. the directory in which this file resides).
+# You must move to the top level of your cylc installation before
+# sourcing this script OR first set $CYLC_DIR to that directory.
 
-# You only need to do this if your login scripts do not provide access
-# to this particular cylc installation.
+function cycl {
+    echo "TYPO ALERT: it's 'cylc' NOT 'cycl'!"
+    cylc $@
+    return 0
+}
 
-if [[ ! -f bin/cylc ]]; then
-    echo "ERROR: you are not in the top level of a cylc installation"
+if [[ -f bin/cylc ]]; then
+    # we're in the top level of a cylc installation
+    CYLC_DIR=$PWD
 
+elif [[ ! -z $CYLC_DIR ]]; then
+    if [[ ! -f $CYLC_DIR/bin/cylc ]]; then
+        echo "ERROR: $CYLC_DIR is not a cylc installation"
+        return 1
+    fi
 else
-
-    PATH=$(bin/_cylc-clean-path $PATH)
-    PYTHONPATH=$(bin/_cylc-clean-path $PYTHONPATH)
-
-    export PATH=$PWD/bin:$PATH
-    export PYTHONPATH=$PWD/src:$PYTHONPATH
-
-    echo "SHELL CONFIGURED FOR $(which cylc)"
-
+    echo "ERROR: source from top level cylc, or set CYLC_DIR"
+    return 0
 fi
+
+echo "CONFIGURING THIS SHELL FOR $CYLC_DIR/bin/cylc"
+
+PATH=$($CYLC_DIR/bin/_cylc-clean-path $PATH)
+PYTHONPATH=$($CYLC_DIR/bin/_cylc-clean-path $PYTHONPATH)
+export PATH=$CYLC_DIR/bin:$PATH
+export PYTHONPATH=$CYLC_DIR/src:$PYTHONPATH
