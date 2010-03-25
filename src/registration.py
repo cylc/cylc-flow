532222d8e432d60d3323a023507c04265d2e773b
Hilary James Oliver <h.oliver@niwa.co.nz>
2010-03-25 13:02:05 +1200

    registration cleanup, got rid of unregister

--- src/registration.py
+++ src/registration.py
@@ -24,8 +24,32 @@ class registrations:
                 print x
                 sys.exit(1)
 
+        else:
+            self.load_from_file()
+
+    def load_from_file( self ):
+        print "Loading your cylc system registrations"
+        if not os.path.exists( self.filename ):
+            # no systems registered yet, so the file does not exist
+            return
+
+        input = open( self.filename, 'rb' )
+        self.registrations = pickle.load( input )
+        input.close()
+        
+    def dump_to_file( self ):
+        output = open( self.filename, 'wb' )
+        pickle.dump( self.registrations, output )
+        output.close()
+
     def count( self ):
         return len( self.registrations.keys() )
+
+    def is_registered( self, name ):
+        if name in self.registrations.keys():
+            return True
+        else:
+            return False
 
     def get( self, name ):
         # return system directory registered under name
@@ -33,12 +57,6 @@ class registrations:
             return self.registrations[ name ]
         else:
             return None
-
-    def is_registered( self, name ):
-        if name in self.registrations.keys():
-            return True
-        else:
-            return False
 
     def get_all( self ):
         return self.registrations.keys()
@@ -63,27 +81,10 @@ class registrations:
                 self.print_reg( name )
                 return False
 
-        if not self.check_dir( dir ):
-            return False
-
         print "New:",
         self.registrations[ name ] = dir
         self.print_reg( name )
         return True
-
-    def load_from_file( self ):
-        if not os.path.exists( self.filename ):
-            # no systems registered yet, so the file does not exist
-            return
-
-        input = open( self.filename, 'rb' )
-        self.registrations = pickle.load( input )
-        input.close()
-        
-    def dump_to_file( self ):
-        output = open( self.filename, 'wb' )
-        pickle.dump( self.registrations, output )
-        output.close()
 
     def print_reg( self, name, pre='', post='' ):
         if name not in self.registrations.keys():
@@ -100,61 +101,20 @@ class registrations:
             count +=1
             self.print_reg( name, pre=' [' + str(count) + '] ' )
 
-    def get_invalid( self ):
-        print 'Number of registrations:', self.count()
-        invalid = []
-        for name in self.registrations.keys():
-            if not self.check_reg( name ):
-                invalid.append( name )
-        return invalid
-
-    def check_reg( self, name ):
-        if self.check_dir( self.registrations[ name ] ):
-            self.print_reg( name, pre=' + ', post=' ... OK' )
-            return True
-        else:
-            self.print_reg( name, pre=' + ', post=' ... INVALID' )
-            return False
-
-    def check_dir( self, dir ):
-        if not os.path.exists( dir ):
-            print "ERROR, dir not found: " + dir
-            return False
-
-        files = [\
-                dir + '/system_config.py',
-                dir + '/task_classes.py',
-                dir + '/task_list.py',
-                dir + '/job_submit_methods.py',
-                ]
-
-        good = True
-        for file in files:
-            if not os.path.exists( file ):
-                print "ERROR, file not found: " + file
-                good = False
-
-        return good
-       
 
 if __name__ == '__main__':
     # module test code
 
     reg = registrations( 'REGISTRATIONS' )
 
-    reg.register( 'userguide', 'systems/userguide' )
-    reg.register( 'userguide', 'systems/userguide/waz' )
     reg.register( 'foo', 'systems/userguide' )
-    reg.register( 'baz', 'systems/foo' )
-    reg.register( 'bar', 'systems/nonexistent' )
+    reg.register( 'bar', 'systems/userguide' )
+    reg.register( 'bar', 'systems/userguidex' )
 
-    reg.check_all()
     reg.print_all()
-
     reg.dump_to_file()
 
+    print
     reg2 = registrations( 'REGISTRATIONS' )
     reg2.load_from_file()
-
-    print
     reg2.print_all()
