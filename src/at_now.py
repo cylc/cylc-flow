8c79b367a8cfdc40098337e1978241335a3e95fe
Hilary James Oliver <h.oliver@niwa.co.nz>
2010-04-29 23:52:20 +1200

    document at_now in-file

--- src/at_now.py
+++ src/at_now.py
@@ -17,6 +17,12 @@ class at_now( job_submit ):
 # submit a jobs using 'at -f FILE now'
 
     def submit( self ):
+
+        if self.owner:
+            print 'WARNING: at_now cannot submit local jobs as ' + self.owner
+            # because I cannot figure out how to get environment
+            # variables through 'sudo -u user at -f FILE now'
+    
         self.set_local_environment()
         # 'at' requires file path, hence use of 'which' here:
         self.execute_local( 'at -f $(which ' + self.task + ') now' )
