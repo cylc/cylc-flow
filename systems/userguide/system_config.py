c2d8f20fb4f8f89fb439e0e20acbb521df444499
Hilary James Oliver <h.oliver@niwa.co.nz>
2010-05-03 09:36:22 +1200

    userguide system_config conflict res

--- systems/userguide/system_config.py
+++ systems/userguide/system_config.py
@@ -51,11 +51,13 @@ class system_config( config ):
 
         # default job submit method, e.g.:
         self.items['job_submit_method'] = 'background'
+        #self.items['job_submit_method'] = 'background2'
         #self.items['job_submit_method'] = 'at_now'
 
         # to override the default job submit method for specific tasks, e.g.:
         # self.items['job_submit_overrides']['background2'] = [ 'task1', 'task2' ]
-        self.items['job_submit_overrides'][ 'background2' ] = [ 'A', 'B', 'C' ]
+        #self.items['job_submit_overrides'][ 'background2' ] = [ 'A', 'B', 'C' ]
+        #self.items['job_submit_overrides']['loadleveler_ecoconnect'] = [ 'A' ]
 
         # environment variables available to all tasks, can include
         # the registered system name, e.g.:
