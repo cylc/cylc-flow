cd41d671f19747a13029d548999c070609ab1a36
Hilary James Oliver <h.oliver@niwa.co.nz>
2010-07-06 12:10:19 +1200

    example system doc change

--- systems/userguide/system_info.py
+++ systems/userguide/system_info.py
@@ -14,30 +14,34 @@
 # 'cylc system-info SYSTEM'.
 
 info = """
-An implementation of the task set that illustrates the cylc userguide.
-______________________
-HOW THE SYSTEM WORKS
-Each task "writes output files" using the touch command, and "reads
-input files" by detecting the existence of these (empty) files.
-___________________________________________
-RUNNING MULTIPLE SYSTEM INSTANCES AT ONCE 
-All tasks in this system read from and write to $CYLC_TMPDIR, which is
-defined in the system config module to be /tmp/$USER/{system-name}
-Thus if you register the system under several different names you can
-run multiple instances of it at once. 
-_________________________________
-GETTING TASKS TO FAIL ON DEMAND 
-You can cause any task in THIS SYSTEM to fail via the system config file: 
-    self.items['environment']['FAIL_TASK'] = 'A%2010010106'
-(the identified task will self-abort via scripts/check-env.sh).
-If failed task is reset or the system restarted, the task will not fail
-again (as would be the case after fixing a problem in a real system). 
-The 'cylc start --dummy-mode --fail-out=A%2010010106' has the same
-effect on ANY SYSTEM but ONLY IN DUMMY MODE.
-_________________________________
-ACCELERATED REAL TIME OPERATION 
-Each task script scales its registered run time by $REAL_TIME_ACCEL as
-defined in the system config file, so that the system can run as quickly
-in real mode as it does in dummy mode.  HOWEVER, this will not speed up
-CYCLING when the system catches up to real time operation (because the 
-contact task X will trigger off the real wall clock time.)"""
+___________________________________________________________________________
+| This implements the cylc userguide example system, tasks A,B,C,D,E,F,X. |
+| + HOW THE SYSTEM WORKS                                                  |
+| Each task aborts if its expected input files do not exist (but does not |
+| use them), and then creates its (empty) output files using 'touch'.     |
+|_________________________________________________________________________|
+| + RUNNING MULTIPLE SYSTEM INSTANCES AT ONCE                             |
+| All task I/O is done under $CYLC_TMPDIR, defined at run time to include |
+| the registered system name (see systems/userguide/system_config.py).    |
+| Thus if you register the system under multiple names you can run        | 
+| system instances at once without any interference between them.         |
+|_________________________________________________________________________|
+| + GETTING TASKS TO FAIL ON DEMAND IN REAL MODE OPERATION                |
+| Set $FAIL_TASK in the system config file, e.g.:                         |
+|    self.items['environment']['FAIL_TASK'] = 'A%2010010106'              |
+| The task will self-abort via systems/userguide/scripts/check-env.sh.    |
+| If the task is reset, or the system restarted, it will run successfully |
+| as if the "problem" has been fixed.                                     |                           
+|                                                                         |
+| Note: this is system-specific, but you can do the same thing for any    |
+| system in DUMMY MODE using 'cylc (re)start -d --fail-task=A%2010010106' |
+|_________________________________________________________________________|
+| + REAL TIME OPERATION                                                   |
+| The tasks in this system are designed to run quickly (~10 seconds), but |
+| note that if the system catches up to real time operation there will be |
+| a real 6 hour delay between cycles - so you might want to make sure the |
+| initial cycle time in signficantly in the past. This is not an issue in |
+| dummy mode, however, because of the accelerated dummy mode clock.       |
+|_________________________________________________________________________|
+"""
+
