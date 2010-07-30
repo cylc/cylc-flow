e63dd016a0ba07cce16c317cfb95da66b76647bd
Hilary James Oliver <h.oliver@niwa.co.nz>
2010-07-31 00:42:32 +1200

    added ensemble example suite

---
+++ suites/ensemble/task_list.py
@@ -0,0 +1,40 @@
+#/usr/bin/python
+
+#         __________________________
+#         |____C_O_P_Y_R_I_G_H_T___|
+#         |                        |
+#         |  (c) NIWA, 2008-2010   |
+#         | Contact: Hilary Oliver |
+#         |  h.oliver@niwa.co.nz   |
+#         |    +64-4-386 0461      |
+#         |________________________|
+
+# LIST OF TASK NAMES (order irrelevant)
+task_list = \
+    [
+       'A',
+       'B',
+       'ensemble',
+       'C1',
+       'C2',
+       'C3',
+       'coldstart',
+       'D',
+       'startup',
+       'X',
+    ]
+
+task_list_shortnames = \
+    [
+       'a',
+       'b',
+       'ens',
+       'c1',
+       'c2',
+       'c3',
+       'cld',
+       'd',
+       'stp',
+       'x',
+    ]
+# END OF FILE
