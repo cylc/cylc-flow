43c2bb17ad612de5573409e76b7e8e2bb7328bad
Hilary James Oliver <h.oliver@niwa.co.nz>
2010-09-15 21:43:47 +1200

    change to loadleveler initialdir directive

--- src/job-submission/ll_basic.py
+++ src/job-submission/ll_basic.py
@@ -17,6 +17,7 @@ class ll_basic( job_submit ):
     # Submit a job to run via loadleveler (llsubmit)
 
     def __init__( self, task_id, ext_task, task_env, dirs, extra, logs, owner, host ): 
+        # parent class init first
         job_submit.__init__( self, task_id, ext_task, task_env, dirs, extra, logs, owner, host ) 
 
         out = tempfile.mktemp( prefix = task_id + '-', dir= self.joblog_dir, suffix = ".out" ) 
@@ -32,7 +33,15 @@ class ll_basic( job_submit ):
         directives[ 'job_name' ] = task_id
         directives[ 'output'   ] = out
         directives[ 'error'    ] = err
-        directives[ 'initialdir' ] = self.joblog_dir
+
+        if self.homedir:
+            # For local job submits self.homedir is defined
+            # need to set initialdir directive, as we may be submitting
+            # as a different user.
+            directives[ 'initialdir' ] = self.homedir
+            # For remote job submits we don't necessarily know the
+            # remote homedir, but ssh automatically puts us there, so
+            # don't need the directive.
 
         # add (or override with) taskdef directives
         for d in self.directives:
