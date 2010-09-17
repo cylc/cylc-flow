b46c95906a5342af4ebf7f0651683793491044ca
Hilary James Oliver <h.oliver@niwa.co.nz>
2010-09-17 14:35:59 +1200

    at now job submit redirects to file instead of mail

--- src/job-submission/at_now.py
+++ src/job-submission/at_now.py
@@ -9,13 +9,32 @@
 #         |    +64-4-386 0461      |
 #         |________________________|
 
-
+import os, re
+import tempfile
 from job_submit import job_submit
 
 class at_now( job_submit ):
     # This class overrides job submission command construction so that
     # the cylc task execution file will be submitted to the Unix 'at'
-    # scheduler ('at -f FILE now').
+    # scheduler, with output redirected to file instead of to mail.
 
     def construct_command( self ):
-        self.command = 'at -f ' + self.jobfile_path + ' now'
+
+        if self.local_job_submit:
+            # can uniquify the name locally
+            out = tempfile.mktemp( 
+                suffix = ".out", 
+                prefix = self.task_id + "-",
+                dir = os.path.join( self.homedir, self.__class__.joblog_dir ) )
+
+            err = re.sub( '\.out$', '.err', out )
+
+            # record log files for access by cylc view
+            self.logfiles.replace_path( '/.*/' + self.task_id + '-.*\.out', out )
+            self.logfiles.replace_path( '/.*/' + self.task_id + '-.*\.err', err )
+
+        else:
+            out = self.task_id + '.out'
+            err = self.task_id + '.err'
+
+        self.command = 'at now <<eof\n' + self.jobfile_path + ' 1> ' + out + ' 2> ' + err + '\neof'
