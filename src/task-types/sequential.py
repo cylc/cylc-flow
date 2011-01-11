7e6c4b2e509f22495efea3b3ccc87f57c5920034
Hilary James Oliver <h.oliver@niwa.co.nz>
2011-01-11 13:11:28 +1200

    superficial change to sequential task mod

--- src/task-types/sequential.py
+++ src/task-types/sequential.py
@@ -13,9 +13,7 @@
 class sequential(object):
     # not "ready to spawn" unless 'finished'.
     def ready_to_spawn( self ):
-        if self.state.has_spawned():
-            return False
-        if self.state.is_finished():
+        if self.state.is_finished() and not self.state.has_spawned():
             return True
         else:
             return False
