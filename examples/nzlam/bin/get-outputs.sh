ffa72d15d1c0853881eb70e3707a3bcafa3eda1c
Hilary James Oliver <h.oliver@niwa.co.nz>
2011-01-17 08:12:51 +1200

    documented and improved new grep and edit commands

---
+++ examples/nzlam/bin/get-outputs.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+# Hilary Oliver, NIWA, 2010
+
+# Retrieve fieldsfiles from UM output directories (DATAM for sets of
+# reinitialized PP files; DATAW for single PP files), and move them to
+# final locations defined by cylc filename templates (see below):
+
+# USAGE:
+#  get-um-ouput.sh <UM RUNID> <PP STREAM LIST>
+# For example:
+#  get-um-output.sh xaald pp0 pp1 pc
+
+# Corresponding filename templates must exist in the environment, e.g.:
+#  TEMPLATE_pp0 = $OUTPUT_DIR/tn_YYYYMMDDHH_utc_nzlam_12.um
+#  TEMPLATE_pp1 = $OUTPUT_DIR/sls_YYYYMMDDHH_utc_nzlam_12.um
+#  TEMPLATE_pc  = $OUTPUT_DIR/met_YYYYMMDDHH_utc_nzlam_12.um
+
+# The HHH hour suffix on reinitialized files is transferred as follows:
+# xaald_pc024 -> $OUTPUT_DIR/met_YYYYMMDDHH_utc_nzlam_12_024.um
+
+set -e
+
+# get UM RUN ID:
+RUNID=$1
+shift
+
+# find UM output directories
+cylcutil check-vars TEMPLATE_DATAM_DIR TEMPLATE_DATAW_DIR
+DATAM=$( cylcutil template TEMPLATE_DATAM_DIR )
+DATAW=$( cylcutil template TEMPLATE_DATAW_DIR )
+
+# loop through PP suffixes to process
+for ITEM in $@; do
+    if [[ $ITEM = pp* ]]; then
+        # single PP files in DATAW
+
+        export PPFILE=$DATAW/$RUNID.$ITEM 
+        cylcutil check-vars -f PPFILE
+ 
+        TEMPLATE_NAME=TEMPLATE_$ITEM
+        cylcutil check-vars $TEMPLATE_NAME
+
+        export OUTFILE=$( cylcutil template $TEMPLATE_NAME )
+        # ensure output file parent directory exists
+        cylcutil check-vars -p OUTFILE
+
+        echo "moving $PPFILE -> $OUTFILE"
+        mv $PPFILE $OUTFILE
+    else
+        # multiple reinitialized PP files in DATAM
+
+        TEMPLATE_NAME=TEMPLATE_$ITEM
+        cylcutil check-vars $TEMPLATE_NAME
+
+        PPFILES=$( ls $DATAM/${RUNID}a_${ITEM}??? )
+        OUTFILE_BASE=$( cylcutil template $TEMPLATE_NAME )
+
+        for PPFILE in $PPFILES; do
+            SUFFIX=${PPFILE#*a_$ITEM}
+            export OUTFILE=${OUTFILE_BASE%.um}_${SUFFIX}.um
+            # ensure output file parent directory exists
+            cylcutil check-vars -p OUTFILE
+            echo "moving $PPFILE -> $OUTFILE"
+            mv $PPFILE $OUTFILE
+        done
+    fi
+done
