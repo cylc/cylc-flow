c018fbf710365421da45b993cf4700a3d318d60f
Hilary James Oliver <h.oliver@niwa.co.nz>
2011-02-07 08:42:58 +1200

    add trivial import test module

---
+++ src/cylc.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+# A trivial module imported by the main cylc command to test that the cylc
+# environment has been configured. Using any non-trivial module is
+# dangerous because bugs introduced during development, say, will result
+# in the main cylc command aborting immediately (during the import test).
+
+pass
