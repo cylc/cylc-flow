1f0bc5cb2cf273b79242ec133a6a136e81de0d93
Hilary James Oliver <h.oliver@niwa.co.nz>
2011-02-11 22:18:27 +1200

    fixed bug introduced to userguide example suite

--- examples/userguide/bin/E.sh
+++ examples/userguide/bin/E.sh
@@ -7,13 +7,13 @@ cylc checkvars -d INPUT_DIR
 cylc checkvars -c OUTPUT_DIR
 
 # CHECK INPUT FILES EXIST
-PRE=$INPUT_DIR/sea-state-${CYCLETIME}.nc
+PRE=$INPUT_DIR/sea-state-${CYCLE_TIME}.nc
 if [[ ! -f $PRE ]]; then
     echo "ERROR, file not found $PRE" >&2
     exit 1
 fi
 
-echo "Hello from $TASK_NAME at $CYCLETIME in $CYLC_SUITNAME"
+echo "Hello from $TASK_NAME at $CYCLE_TIME in $CYLC_SUITNAME"
 
 sleep $TASK_EXE_SECONDS
 
--- examples/userguide/bin/F.sh
+++ examples/userguide/bin/F.sh
@@ -7,7 +7,7 @@ cylc checkvars -d INPUT_DIR
 cylc checkvars -c OUTPUT_DIR
 
 # CHECK INPUT FILES EXIST
-PRE=$F_INPUT_DIR/river-flow-${CYCLE_TIME}.nc
+PRE=$INPUT_DIR/river-flow-${CYCLE_TIME}.nc
 if [[ ! -f $PRE ]]; then
     echo "ERROR, file not found $PRE" >&2
     exit 1
@@ -17,4 +17,4 @@ echo "Hello from $TASK_NAME at $CYCLE_TIME in $CYLC_SUITNAME"
 sleep $TASK_EXE_SECONDS
 
 # generate outputs
-touch $F_OUTPUT_DIR/river-flow-products-${CYCLE_TIME}.nc
+touch $OUTPUT_DIR/river-flow-products-${CYCLE_TIME}.nc
