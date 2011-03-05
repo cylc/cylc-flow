98f600d4e3fb7c0d2a343262063d9c71b25df07a
Hilary James Oliver <h.oliver@niwa.co.nz>
2011-03-06 09:06:54 +1200

    simple general parser for help windows

--- src/gui/helpwindow.py
+++ src/gui/helpwindow.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 
+import re
 import gtk
 import pango
+import string
 
-class helpwindow( object ):
+class helpwindow_base( object ):
     def __init__( self, title ):
         self.window = gtk.Window()
         #window.set_border_width( 10 )
@@ -41,10 +43,10 @@ class helpwindow( object ):
         self.add_main_heading( title )
          
     def add_main_heading( self, text ):
-        self.tb.insert_with_tags( self.tb.get_end_iter(), text, self.tag_bold, self.tag_title )
+        self.tb.insert_with_tags( self.tb.get_end_iter(), text + '\n', self.tag_bold, self.tag_title )
 
     def add_heading( self, text ):
-        self.tb.insert_with_tags( self.tb.get_end_iter(), '\n\n' + text, self.tag_bold, self.tag_heading )
+        self.tb.insert_with_tags( self.tb.get_end_iter(), '\n\n' + text + '\n', self.tag_bold, self.tag_heading )
  
     def add_subheading( self, text ):
         self.tb.insert_with_tags( self.tb.get_end_iter(), '\n    ' + text, self.tag_bold, self.tag_subheading )
@@ -61,91 +63,120 @@ class helpwindow( object ):
     def show( self ):
         self.window.show_all()
 
+class helpwindow( helpwindow_base ):
+    def __init__( self, title, text ):
+        helpwindow_base.__init__(self, title )
+        self.parse( text )
+
+    def parse( self, text ):
+        def strip( line ):
+            return re.sub( '%[\w\d]+ ', '', line )
+
+        lines = string.split( text, '\n' )
+        for line in lines:
+            if re.match( '^%h1', line ):
+                self.add_main_heading( strip(line) )
+            elif re.match( '^%h2', line ):
+                self.add_heading( strip(line) )
+            elif re.match( '^%h3', line ):
+                self.add_subheading( strip(line) )
+            elif re.match( '^%b', line ):
+                self.add_text_bold( strip(line ))
+            elif re.match( '^%i', line ):
+                self.add_list_item( strip(line ))
+            else:
+                self.add_text( line + ' ')
+
 ##########
 def main( b ):
-    help = helpwindow( "Gcylc Main Window Help" )
-    help.add_heading( "\nOverview" )
-    help.add_text( 
-            "The gcylc main window shows your registered suites. Using " 
-            "the available buttons and right-click menu choices you can "
-            "register new suites; copy, rename, and unregister existing "
-            "ones; start suites running or connect to ones that are "
-            "already running; edit, search, validate, or graph suite "
-            "definitions; and import suites from or export them to the "
-            "central suite registration database (seen be all users). You "
-            "can't run suites directly from the central database, but you "
-            "can view, search, and graph them when considering whether to "
-            "import them to your local database for your own use." )
+    help = helpwindow( "Gcylc Main Window Help", """%h2 Overview
 
-    help.add_heading( "Buttons" )
-    help.add_subheading( "Switch To Local/Central DB" )
-    help.add_text(
-            "Toggle between the local "
-            "and central suite registration databases. Right-"
-            "click menu options vary somewhat according to which "
-            "database is being viewed." )
-    help.add_subheading( "Filter" )
-    help.add_text(
-            "Use group and name match patterns to filter which suites "
-            "are visisble." ) 
-    help.add_subheading( "Register Another Suite" )
-    help.add_text(
-            "Open a file chooser dialog to load cylc suite definition "
-            "(suite.rc) files and thereby register a new suite.") 
-    help.add_subheading( "Quit" )
-    help.add_text(
-            "This quits the application but does not close down any "
-            "suite editing or control windows, etc., that you have "
-            "opened.") 
+The gcylc main window shows your registered suites. Using the available
+buttons and right-click menu choices you can register new suites; copy,
+rename, and unregister existing ones; start suites running or connect to
+ones that are already running; edit, search, validate, or graph suite
+definitions; and import suites from or export them to the central suite
+registration database (seen be all users). You can't run suites directly
+from the central database, but you can view, search, and graph them when
+considering whether to import them to your local database for your own
+use.
 
-    help.add_heading( "Right Click Menu Options" )
-    help.add_text(
-            "Each menu option, except Control, launches one of the cylc "
-            "commandline programs  inside a wrapper that captures command "
-            "output in real time for display in a GUI window. The Control "
-            "option launches a self-contained GUI application for suite "
-            "control and montoring." )
-    help.add_text_bold( "To operate on a whole group of suites" )
-    help.add_text(" (some options only), click on any one of the "
-            "group members and set 'Apply To Parent Group' or similar in "
-            "the subsequent option popup window (you will be able to select "
-            "the group itself when gcylc changes to tree-view registration display)." )
-    help.add_subheading( "Control" )
-    help.add_text(
-            "Launch a suite control GUI to start a suite running "
-            "or connect to a suite that is already running.") 
-    help.add_subheading( "Edit" )
-    help.add_text(
-            "Edit the suite config (suite.rc) file") 
-    help.add_subheading( "Graph" )
-    help.add_text(
-            "Graph the suite. The graph will update in real time "
-            "as you edit the suite.") 
-    help.add_subheading( "Search" )
-    help.add_text(
-            "Search in the suite config file and bin directory.") 
-    help.add_subheading( "Validate" )
-    help.add_text(
-            "Parse the suite config file, validate it against the "
-            "spec, and report any errors.") 
-    help.add_subheading( "Copy" )
-    help.add_text(
-            "Copy an existing suite and register it for use.") 
-    help.add_subheading( "Rename" )
-    help.add_text(
-            "Reregister an existing suite under a different group:name.") 
-    help.add_subheading( "Export" )
-    help.add_text(
-            "Export a suite to the central database to make it available "
-            "to others.") 
-    help.add_subheading( "Unregister" )
-    help.add_text(
-            "Unregister a suite (note this does not delete the suite "
-            "definition directory).") 
+%h2 Buttons
+
+%h3 Switch To Local/Central DB
+
+Toggle between the local and central suite registration databases.
+Right-click menu options vary somewhat according to which database is
+being viewed.
+
+%h3 Filter
+
+Use group and name match patterns to filter which suites are visisble.
+
+%h3 Register Another Suite
+
+Open a file chooser dialog to load cylc suite definition (suite.rc)
+files and thereby register a new suite.
+
+%h3 Quit
+
+This quits the application but does not close down any suite editing or
+control windows, etc., that you have opened.
+
+%h2 Right Click Menu Options
+
+Each menu option, except Control, launches one of the cylc commandline
+programs  inside a wrapper that captures command output in real time for
+display in a GUI window. The Control option launches a self-contained
+GUI application for suite control and montoring.
+%b To operate on a whole group of suites
+(some options only), click on any one of the group members and set
+'Apply To Parent Group' or similar in the subsequent option popup window
+(you will be able to select the group itself when gcylc changes to
+tree-view registration display).  
+
+%h3 Control
+
+Launch a suite control GUI to start a suite running or connect to a
+suite that is already running.
+
+%h3 Edit
+
+Edit the suite config (suite.rc) file
+
+%h3 Graph
+
+Graph the suite. The graph will update in real time as you edit the
+suite.
+
+%h3 Search
+
+Search in the suite config file and bin directory.
+
+%h3 Validate
+
+Parse the suite config file, validate it against the spec, and report
+any errors.
+
+%h3 Copy
+
+Copy an existing suite and register it for use.
+
+%h3 Reregister
+
+Reregister an existing suite under a different group:name.
+
+%h3 Export
+
+Export a suite to the central database to make it available to others.
+
+%h3 Unregister
+
+Unregister a suite (note this does not delete the suite definition directory).""")
     help.show()
 
 def filter( b ):
-    help = helpwindow( "Filter Window Help" )
+    help = helpwindow_base( "Filter Window Help" )
     help.add_heading( "\nOverview" )
     help.add_text( 
             "Change suite registration visibility by filtering on "
@@ -158,3 +189,4 @@ def filter( b ):
     help.add_list_item( "(foo|bar) - match 'foo' or 'bar'" )
     help.show()
  
+
