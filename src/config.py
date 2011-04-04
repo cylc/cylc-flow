6d920f11f66f19f3ca79275af1f3f17374ff4496
Hilary James Oliver <h.oliver@niwa.co.nz>
2011-04-04 19:56:24 +1200

    fix recent bug: default graph color

--- src/config.py
+++ src/config.py
@@ -850,8 +850,9 @@ class config( CylcConfigObj ):
         # set node URLs:
         for n in graph.nodes():
             n.attr['URL'] = 'foo'
-            n.attr['style'] = 'filled'
-            n.attr['fillcolor'] = 'cornsilk'
+            if not colored:
+                n.attr['style'] = 'filled'
+                n.attr['fillcolor'] = 'cornsilk'
 
         return graph
 
