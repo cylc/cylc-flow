8e8a9470ffcd733f98e4551a313c8a17e31214c6
Hilary James Oliver <h.oliver@niwa.co.nz>
2011-04-27 09:09:31 +1200

    added top level README

---
+++ README.txt
@@ -0,0 +1,4 @@
+This is the cylc metascheduler code base, version: THIS IS NOT A VERSIONED RELEASE
+
+For all documentation, including installation and run time instructions:
+ doc/userguide.pdf 
--- dev/bin/cylc-predist.sh
+++ dev/bin/cylc-predist.sh
@@ -45,6 +45,7 @@ perl -pi -e "s/THIS IS NOT A VERSIONED RELEASE/$CYLC_VERSION/" src/view/chooser.
 perl -pi -e "s/THIS IS NOT A VERSIONED RELEASE/$CYLC_VERSION/" src/view/gtkmonitor.py
 perl -pi -e "s/THIS IS NOT A VERSIONED RELEASE/$CYLC_VERSION/" bin/cylc
 perl -pi -e "s/THIS IS NOT A VERSIONED RELEASE/$CYLC_VERSION/" doc/userguide.tex
+perl -pi -e "s/THIS IS NOT A VERSIONED RELEASE/$CYLC_VERSION/" README.txt
 
 echo "MAKING DOCUMENTATION (USERGUIDE)"
 # make sure documentation processing uses the release versions
