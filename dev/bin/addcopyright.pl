846b8b560b631e2025ee7de9926f1f29bfb38f2b
Hilary James Oliver <h.oliver@niwa.co.nz>
2011-05-21 17:48:18 +1200

    added GPL to all bin scripts

--- dev/bin/addcopyright.pl
+++ dev/bin/addcopyright.pl
@@ -1,13 +1,24 @@
 #!/usr/bin/perl
 
+# Add licensing preamble to source files. If the file is a script
+# put it immediately after the '#!' interpreter line, otherwise 
+# put it right at the top.
+
 $count = 0;
 while (<>) {
-    if ( $count == 1 ) {
+    if ( $count == 0 ) {
+        if ( m/#!/ ) {
+            $skip = 1;
+        } else {
+            $skip = 0;
+        }
+    }
+    if ( $skip and $count == 1 or ! $skip and $count == 0 ) {
         print <<eof
 
 #C: THIS FILE IS PART OF THE CYLC FORECAST SUITE METASCHEDULER.
 #C: Copyright (C) 2008-2011 Hilary Oliver, NIWA
-#C: 
+#C:
 #C: This program is free software: you can redistribute it and/or modify
 #C: it under the terms of the GNU General Public License as published by
 #C: the Free Software Foundation, either version 3 of the License, or
@@ -20,7 +31,6 @@ while (<>) {
 #C:
 #C: You should have received a copy of the GNU General Public License
 #C: along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 eof
     }
     $count += 1;
