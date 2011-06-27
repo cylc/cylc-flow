ad27a8a174a0bb893fb00b4bcd233ef6ccdf1b09
Hilary James Oliver <h.oliver@niwa.co.nz>
2011-06-27 12:24:33 +1200

    Tree-structured scaling test suites

--- dev/bin/scaling-gen.py
+++ dev/bin/scaling-gen.py
@@ -16,13 +16,9 @@
 #C: You should have received a copy of the GNU General Public License
 #C: along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-"""Generate a test suite with N tasks with the following structure:
-  T1 => T2 => T3 => ...
-This is sufficient to test cylc pretty well on large suites because the
-scheduling algorithm does not know that the suite is a simple linear
-sequence ... however it may be better to generate more complex
-tree-structured suites so that more task outputs are satisfied
-earlier in the run."""
+"""Generate a suite with N dummy tasks arranged in a simple tree structure:
+  T1 => T2 & T3; T2 => T4 & T5; T3 => T6 & T7; ...
+"""
 
 import os, sys
 from mkdir_p import mkdir_p
@@ -64,6 +60,7 @@ FILE = open( suiterc, 'wb' )
 
 FILE.write( "title = scaling test" + N + '\n' )
 FILE.write( "description = A test suite containing " + N + " tasks\n" )
+FILE.write( "job submission method = at_now\n" )
 
 FILE.write( """
 [special tasks]
@@ -78,8 +75,9 @@ FILE.write( """
         graph = \"\"\"
 """)
 
-for i in range(1,int(N)+1):
-    FILE.write( 'T' + str(i) + ' => ' )
+for i in range(2,int(N)+1,2):
+    FILE.write( 'T' + str(i/2) + ' => T' + str(i) + '\n')
+    FILE.write( 'T' + str(i/2) + ' => T' + str(i+1) + '\n')
 
 FILE.write('\n"""')
 FILE.close()
