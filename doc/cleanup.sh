cb8e8255c39876f7340f973cae53b39368c86ba3
Hilary James Oliver <h.oliver@niwa.co.nz>
2011-07-12 14:18:38 +1200

    Updated doc processing cleanup script

--- doc/cleanup.sh
+++ doc/cleanup.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-rm -f *.4tc *.aux *.4ct *.dvi *.idv *.lg *.lof *.tmp *.toc *.xref *.out *.log
-
+# remove all evidence of document processing
+if [[ ! -f doc/cug.tex ]]; then
+    echo "Run this script from $CYLC_DIR in your cylc repository."
+    # We don't change to $CYLC_DIR automatically in case the environment
+    # is configured for another cylc installation.
+    exit 1
+fi
+echo Cleaning $PWD/doc
+cd doc
+rm -f *.4tc *.aux *.4ct *.dvi *.idv *.lg *.lof *.tmp *.toc *.xref *.out *.log *.css
+rm *.html
+rm *.pdf
+rm suiterc.spec.tex
 
