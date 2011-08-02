cylc/cylc-flow

commit e67e1d827061b183380d9ff2236b3f803de614dc

Do not load taskdefs from get_graph.

src/config.py
@@ -617,7 +617,7 @@ class config( CylcConfigObj ):
         all_tasknames.sort(key=str.lower)  # case-insensitive sort
         return all_tasknames
 
-    def process_graph_line( self, line, section ):
+    def process_graph_line( self, line, section, graph_only=False ):
         # Extract dependent pairs from the suite.rc textual dependency
         # graph to use in constructing graphviz graphs.
 
@@ -703,12 +703,12 @@ class config( CylcConfigObj ):
 
             # task defs
             for r in rights:
-                # ignore output labels on the right (they are only
-                # meaningful on the left, in chained tasks)
                 if r:
+                    # ignore output labels on the right (they are only
+                    # meaningful on the left, in chained tasks)
                     r = re.sub( ':\w+', '', r )
-
-                self.generate_taskdefs( lconditional, r, section )
+                if not graph_only:
+                    self.generate_taskdefs( lconditional, r, section )
 
             # graph
             lefts  = re.split( '\s*&\s*', lgroup )
@@ -841,7 +841,7 @@ class config( CylcConfigObj ):
 
     def get_graph( self, start_ctime, stop, colored=True, raw=False ):
         if not self.graph_loaded:
-            self.load()
+            self.load(graph_only=True)
         if colored:
             graph = graphing.CGraph( self.suite, self['visualization'] )
         else:
@@ -981,7 +981,7 @@ class config( CylcConfigObj ):
             prev = cycles[i-1]
         return prev
 
-    def load( self ):
+    def load( self, graph_only=False ):
         # parse the suite dependencies section
         for item in self['dependencies']:
             if item == 'graph':
@@ -1012,8 +1012,9 @@ class config( CylcConfigObj ):
                 line = re.sub( '\s*$', '', line )
 
                 # generate pygraphviz graph nodes and edges, and task definitions
-                self.process_graph_line( line, section )
-                self.graph_loaded = True
+                self.process_graph_line( line, section, graph_only )
+
+        self.graph_loaded = True
 
         # task families
         members = []
@@ -1061,6 +1062,7 @@ class config( CylcConfigObj ):
         self.load_raw_task_definitions()
 
         self.__check_tasks()
+
         self.tasks_loaded = True
 
     def get_taskdef( self, name, strict=False ):

commit 64f403429e9dedb2cb27b5b20eba89a5f3616dc3

Fixed a minor graphing bug introduced yesterday.
The bug affected edges connecting asynchronous and cycling tasks, and
was introduced in e67e1d827061b183380d9ff2236b3f803de614dc: lists of
asynchronous tasks need to be generated for graphing as well as task
proxy generation.

src/config.py
@@ -652,10 +652,18 @@ class config( CylcConfigObj ):
         # reality NWP suites have simple conditional trigger needs).
 
         # [list of valid hours], or ["once"], or ["ASYNCID:pattern"]
+        async_oneoff = False
+        async_repeat = False
+        cycling = False
         validity = []
-        if section == "once" or re.match( '^ASYNCID:', section ):
-            validity.append( section )
+        if section == "once":
+            async_oneoff = True
+            validity = [section]
+        elif re.match( '^ASYNCID:', section ):
+            async_repeat = True
+            validity = [section]
         elif re.match( '^[\s,\d]+$', section ):
+            cycling = True
             # Cycling task.
             hours = re.split( '\s*,\s*', section )
             for hr in hours:
@@ -701,37 +709,48 @@ class config( CylcConfigObj ):
             else:
                 rights = [None]
 
-            # task defs
             for r in rights:
                 if r:
                     # ignore output labels on the right (they are only
                     # meaningful on the left, in chained tasks)
                     r = re.sub( ':\w+', '', r )
-                if not graph_only:
-                    self.generate_taskdefs( lconditional, r, section )
 
-            # graph
             lefts  = re.split( '\s*&\s*', lgroup )
+
+            if not cycling:
+                for n in lefts + rights:
+                    if async_oneoff:
+                        if n not in self.async_oneoff_tasks:
+                            self.async_oneoff_tasks.append(n)
+                    elif async_repeat: 
+                        if n not in self.async_oneoff_tasks:
+                            self.async_repeating_tasks.append(n)
+
+            # generate graph nodes and edges
             sasl = False
             for r in rights:
                 for l in lefts:
                     if l in self.async_oneoff_tasks + self.async_repeating_tasks:
                         sasl = True
                     e = edge( l,r, sasl )
-                    # store edges by hour (or "once" or "ASYNCID:pattern")
-                    for val in validity:
-                        if val == "once":
-                            if e not in self.async_oneoff_edges:
-                                self.async_oneoff_edges.append( e )
-                        elif re.match( '^ASYNCID:', str(val) ):
-                            if e not in self.async_repeating_edges:
-                                self.async_repeating_edges.append( e )
-                        else:
+                    if async_oneoff:
+                        if e not in self.async_oneoff_edges:
+                            self.async_oneoff_edges.append( e )
+                    elif async_repeat:
+                        if e not in self.async_repeating_edges:
+                            self.async_repeating_edges.append( e )
+                    else:
+                        for val in validity:
                             if val not in self.edges:
                                 self.edges[val] = []
                             if e not in self.edges[val]:
                                 self.edges[val].append( e )
 
+            if not graph_only:
+                # generate task definitions
+                for r in rights:
+                    self.generate_taskdefs( lconditional, r, section )
+ 
             # self.edges left side members can be:
             #   foo           (task name)
             #   foo:N         (specific output)
@@ -739,7 +758,6 @@ class config( CylcConfigObj ):
             #   foo:N(T-DD)   (both)
 
     def generate_taskdefs( self, lcond, right, section ):
-
         async = False
         asyncid_pattern = None
         m = re.match( '^ASYNCID:(.*)$', section )
@@ -766,12 +784,8 @@ class config( CylcConfigObj ):
                 self.taskdefs[ name ] = self.get_taskdef( name )
                 if section == "once":
                     self.taskdefs[name].type = 'async_oneoff'
-                    if name not in self.async_oneoff_tasks:
-                        self.async_oneoff_tasks.append(name)
                 elif async:
-                    if name not in self.async_repeating_tasks:
-                        self.async_repeating_tasks.append(name)
-                        self.taskdefs[name].asyncid_pattern = asyncid_pattern
+                    self.taskdefs[name].asyncid_pattern = asyncid_pattern
                     if name == daemon:
                         self.taskdefs[name].type = 'async_daemon'
                     else: