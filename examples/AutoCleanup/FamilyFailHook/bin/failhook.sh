14a967c486aa82f571c77c667319fce3f9ae6ed3
dpmatthews <david.matthews@metoffice.gov.uk>
2011-09-16 16:12:41 +0100

    Simplied hook script in examples/AutoCleanup/FamilyFailHook

--- examples/AutoCleanup/FamilyFailHook/bin/failhook.sh
+++ examples/AutoCleanup/FamilyFailHook/bin/failhook.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Hook script to clean up if the fammo family fails, by removing failed
-# members from the suite. You probably don't want to do this in practice
+# Hook script to clean up if the fammo family fails, by removing the failed
+# task from the suite. You probably don't want to do this in practice
 # (failed tasks should be removed manually, after determining the reason
 # for the failure); but this shows that hook scripts can intervene in
 # the running of their own suite.
@@ -13,21 +13,7 @@ if [[ $TASK != m_* ]]; then
     exit 1
 fi
 
-CTIME=${TASK#*%}
-
 sleep 10 # (time to observe the failed tasks in the suite monitor).
-# Determine which family member(s) failed, if any
-FAILED_TASKS=$(cylc dump $SUITE | grep $CTIME | grep failed | sed -e 's/,.*$//')
 
-found_failed_member=false
-echo "FAILED TASKS:"
-for T in $FAILED_TASKS; do
-    echo -n "   $T ..."
-    if [[ $T == m_* ]]; then
-        found_failed_member=true
-        echo "REMOVING family member"
-        cylc control remove --force $CYLC_SUITE_REG_NAME ${T}%$CTIME
-    else
-        echo "NOT REMOVING (not family member)"
-    fi
-done
+echo "REMOVING FAILED TASK: $TASK"
+cylc control remove --force $CYLC_SUITE_REG_NAME $TASK
