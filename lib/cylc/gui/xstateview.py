cylc/cylc-flow

commit a8cefa9470524d18a20224c1071cbb2fa3dcdd69

Fixed a graphing bug that was previously compensated for.
Cylc graph classes were inadvertently overriding add_node() in the
pygraphviz base class.

lib/cylc/gui/xstateview.py
@@ -230,27 +230,27 @@ class xupdater(threading.Thread):
             self.best_fit = False
 
     def add_graph_key(self):
-        self.graphw.add_node( 'waiting' )
-        self.graphw.add_node( 'runahead' )
-        self.graphw.add_node( 'submitted' )
-        self.graphw.add_node( 'running' )
-        self.graphw.add_node( 'succeeded' )
-        self.graphw.add_node( 'failed' )
-        self.graphw.add_node( 'held' )
-        self.graphw.add_node( 'base' )
-        self.graphw.add_node( 'runtime family' )
-        self.graphw.add_node( 'trigger family' )
+        self.graphw.cylc_add_node( 'waiting', True )
+        self.graphw.cylc_add_node( 'runahead', True )
+        self.graphw.cylc_add_node( 'submitted', True )
+        self.graphw.cylc_add_node( 'running', True )
+        self.graphw.cylc_add_node( 'succeeded', True )
+        self.graphw.cylc_add_node( 'failed', True )
+        self.graphw.cylc_add_node( 'held', True )
+        self.graphw.cylc_add_node( 'base', True )
+        self.graphw.cylc_add_node( 'runtime family', True )
+        self.graphw.cylc_add_node( 'trigger family', True )
 
-        waiting = self.graphw.get_node( 'waiting' )
-        runahead = self.graphw.get_node( 'runahead' )
-        submitted = self.graphw.get_node( 'submitted' )
-        running = self.graphw.get_node( 'running' )
-        succeeded = self.graphw.get_node( 'succeeded' )
-        failed = self.graphw.get_node( 'failed' )
-        held = self.graphw.get_node( 'held' )
-        base = self.graphw.get_node( 'base' )
-        family = self.graphw.get_node( 'runtime family' )
-        grfamily = self.graphw.get_node( 'trigger family' )
+        waiting = self.graphw.get_node( 'waiting', True )
+        runahead = self.graphw.get_node( 'runahead', True )
+        submitted = self.graphw.get_node( 'submitted', True )
+        running = self.graphw.get_node( 'running', True )
+        succeeded = self.graphw.get_node( 'succeeded', True )
+        failed = self.graphw.get_node( 'failed', True )
+        held = self.graphw.get_node( 'held', True )
+        base = self.graphw.get_node( 'base', True )
+        family = self.graphw.get_node( 'runtime family', True )
+        grfamily = self.graphw.get_node( 'trigger family', True )
 
 
         for node in [ waiting, runahead, submitted, running, succeeded, failed, held, base, family, grfamily ]:
@@ -282,15 +282,15 @@ class xupdater(threading.Thread):
         held.attr['fillcolor'] = 'yellow'
         held.attr['color'] = 'black'
 
-        self.graphw.add_edge( waiting, submitted, autoURL=False, style='invis')
-        self.graphw.add_edge( submitted, running, autoURL=False, style='invis')
-        self.graphw.add_edge( running, runahead, autoURL=False, style='invis')
+        self.graphw.cylc_add_edge( waiting, submitted, False, style='invis')
+        self.graphw.cylc_add_edge( submitted, running, False, style='invis')
+        self.graphw.cylc_add_edge( running, runahead, False, style='invis')
 
-        self.graphw.add_edge( succeeded, failed, autoURL=False, style='invis')
-        self.graphw.add_edge( failed, held, autoURL=False, style='invis')
+        self.graphw.cylc_add_edge( succeeded, failed, False, style='invis')
+        self.graphw.cylc_add_edge( failed, held, False, style='invis')
 
-        self.graphw.add_edge( base, grfamily, autoURL=False, style='invis')
-        self.graphw.add_edge( grfamily, family, autoURL=False, style='invis')
+        self.graphw.cylc_add_edge( base, grfamily, False, style='invis')
+        self.graphw.cylc_add_edge( grfamily, family, False, style='invis')
 
     def set_live_node_attr( self, node, id, shape=None ):
         # override base graph URL to distinguish live tasks
@@ -447,7 +447,7 @@ class xupdater(threading.Thread):
 
             # replace collapsed node with a stand-in
             new_node_label = 'SUBTREE:' + id
-            self.graphw.add_node( new_node_label )
+            self.graphw.cylc_add_node( new_node_label, True )
             new_node = self.graphw.get_node( new_node_label )
             #new_node.attr['shape'] = 'doublecircle'
             new_node.attr['shape'] = 'tripleoctagon'
@@ -457,7 +457,7 @@ class xupdater(threading.Thread):
             new_node.attr['URL'] = new_node_label
 
             for n in self.graphw.predecessors( node ):
-                self.graphw.add_edge( n, new_node, autoURL=False )
+                self.graphw.cylc_add_edge( n, new_node, True )
 
             name, topctime = id.split('%')
             for n in self.feedins:
@@ -465,7 +465,7 @@ class xupdater(threading.Thread):
                 #self.follow_up(n,topctime)
                 #if n not in self.collapsems and n not in self.feedintops:
                 if n not in self.collapsems:
-                    self.graphw.add_edge( n, new_node, autoURL=False )
+                    self.graphw.cylc_add_edge( n, new_node, True )
                 #for m in self.feedintops:
                 #    self.graphw.remove_node( m )
 
@@ -493,15 +493,14 @@ class xupdater(threading.Thread):
         # and family members that aren't plotted in the main graph).
         for state in extra_node_ids:
             for id in extra_node_ids[state]:
-                self.graphw.add_node( id )
+                self.graphw.cylc_add_node( id, True )
                 self.set_live_node_attr( self.graphw.get_node(id), id, shape='box')
             # add invisible edges to force vertical alignment
             for i in range( 0, len(extra_node_ids[state])):
                if i == len(extra_node_ids[state]) -1:
                    break
-               self.graphw.add_edge( extra_node_ids[state][i],
-                       extra_node_ids[state][i+1], autoURL=False,
-                       style='invis')
+               self.graphw.cylc_add_edge( extra_node_ids[state][i],
+                       extra_node_ids[state][i+1], True, style='invis')
 
         self.action_required = False
 

lib/cylc/config.py
@@ -1002,9 +1002,9 @@ class config( CylcConfigObj ):
             if l== None and r == None:
                 pass
             elif l == None:
-                graph.add_node( r )
+                graph.cylc_add_node( r, True )
             elif r == None:
-                graph.add_node( l )
+                graph.cylc_add_node( l, True )
             else:
                 style=None
                 arrowhead='normal'
@@ -1015,7 +1015,7 @@ class config( CylcConfigObj ):
                     arrowhead='dot'
                 if conditional:
                     arrowhead='onormal'
-                graph.add_edge( l, r, False, style=style, arrowhead=arrowhead )
+                graph.cylc_add_edge( l, r, True, style=style, arrowhead=arrowhead )
 
         for n in graph.nodes():
             if not colored:

lib/cylc/graphing.py
@@ -30,8 +30,10 @@ class GraphvizError( Exception ):
     def __str__( self ):
         return repr(self.msg)
 
-# TO DO: CONSOLIDATE THE FOLLOWING TESTS WITH THE OTHER GRAPH-DISABLING
-# TESTS IN CYLC.
+# TO DO:
+# 1/ Consolidate graph-disabling tests within cylc.
+# 2/ Does we still need autoURL below?
+
 try:
     import pygraphviz
 except ImportError:
@@ -71,7 +73,7 @@ class CGraphPlain( pygraphviz.AGraph ):
         else:
             return []
 
-    def add_node( self, n, autoURL=True, **attr ):
+    def cylc_add_node( self, n, autoURL, **attr ):
         pygraphviz.AGraph.add_node( self, n, **attr )
         node = self.get_node(n)
         label = re.sub( ddmmhh, tformat, n )
@@ -79,7 +81,7 @@ class CGraphPlain( pygraphviz.AGraph ):
         if autoURL:
             node.attr['URL'] = n
 
-    def add_edge( self, l, r, autoURL=True, **attr ):
+    def cylc_add_edge( self, l, r, autoURL, **attr ):
         # l and r are cylc task IDs 
         pygraphviz.AGraph.add_edge( self, l, r, **attr )
 
@@ -138,7 +140,7 @@ class CGraph( CGraphPlain ):
                         self.task_attr[item] = []
                     self.task_attr[item].append( attr )
 
-    def add_node( self, n, autoURL=True, **attr ):
+    def cylc_add_node( self, n, autoURL, **attr ):
         pygraphviz.AGraph.add_node( self, n, **attr )
         node = self.get_node(n)
         label = re.sub( ddmmhh, tformat, n )
@@ -149,12 +151,12 @@ class CGraph( CGraphPlain ):
             attr, value = re.split( '\s*=\s*', item )
             node.attr[ attr ] = value
 
-    def add_edge( self, l, r, autoURL=True, **attr ):
+    def cylc_add_edge( self, l, r, autoURL, **attr ):
         # l and r are cylc task IDs 
         if l == r:
             # pygraphviz 1.1 adds a node instead of a self-edge
             # which results in a KeyError in get_edge() below.
-            self.add_node( l, autoURL, **attr )
+            self.cylc_add_node( l, autoURL, **attr )
             return
 
         pygraphviz.AGraph.add_edge( self, l, r, **attr )

lib/cylc/scheduler.py
@@ -1563,7 +1563,7 @@ class scheduler(object):
         for id in task.get_resolved_dependencies():
             l = id
             r = task.id 
-            self.runtime_graph.add_edge( l,r )
+            self.runtime_graph.add_edge( l,r,False )
             self.write_runtime_graph()
 
     def update_runtime_graph_async( self, task ):
@@ -1581,7 +1581,7 @@ class scheduler(object):
         for id in task.get_resolved_dependencies():
             l = id
             r = task.id 
-            self.runtime_graph.add_edge( l,r )
+            self.runtime_graph.add_edge( l,r,False )
             self.write_runtime_graph()
 
     def write_runtime_graph( self ):

commit a3cfb70cfbd6db48005771785a1cb6a19230a0ed

Fixed a bug: the graph GUI crashed if the graph key was turned on.
Bug introduced recently in a8cefa9470524d18a20224c1071cbb2fa3dcdd69.

lib/cylc/gui/xstateview.py
@@ -241,16 +241,16 @@ class xupdater(threading.Thread):
         self.graphw.cylc_add_node( 'runtime family', True )
         self.graphw.cylc_add_node( 'trigger family', True )
 
-        waiting = self.graphw.get_node( 'waiting', True )
-        runahead = self.graphw.get_node( 'runahead', True )
-        submitted = self.graphw.get_node( 'submitted', True )
-        running = self.graphw.get_node( 'running', True )
-        succeeded = self.graphw.get_node( 'succeeded', True )
-        failed = self.graphw.get_node( 'failed', True )
-        held = self.graphw.get_node( 'held', True )
-        base = self.graphw.get_node( 'base', True )
-        family = self.graphw.get_node( 'runtime family', True )
-        grfamily = self.graphw.get_node( 'trigger family', True )
+        waiting = self.graphw.get_node( 'waiting' )
+        runahead = self.graphw.get_node( 'runahead' )
+        submitted = self.graphw.get_node( 'submitted' )
+        running = self.graphw.get_node( 'running' )
+        succeeded = self.graphw.get_node( 'succeeded' )
+        failed = self.graphw.get_node( 'failed' )
+        held = self.graphw.get_node( 'held' )
+        base = self.graphw.get_node( 'base' )
+        family = self.graphw.get_node( 'runtime family' )
+        grfamily = self.graphw.get_node( 'trigger family' )
 
 
         for node in [ waiting, runahead, submitted, running, succeeded, failed, held, base, family, grfamily ]: