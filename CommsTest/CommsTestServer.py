cac8c8785c9c98f05daaf3a1d2cf8ebde1c036a7
Hilary James Oliver <h.oliver@niwa.co.nz>
2012-03-13 00:00:28 +1300

    Added CommsTest Pyro network communication demo.

---
+++ CommsTest/CommsTestServer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+# See CommsTest/README
+
+import sys
+import socket
+import Pyro.core
+
+if len(sys.argv) != 1:
+    print "USAGE: CommsTestServer.py HOST PORT"
+    sys.exit(1)
+
+my_host = socket.getfqdn()
+
+class Report(Pyro.core.ObjBase):
+    def get_report(self, name):
+        return "***Hello " + name + ", this is the CommsTest server on " + my_host + "***"
+
+daemon=Pyro.core.Daemon()
+uri=daemon.connect( Report(),"report")
+print "[CommsTest Server listening on", my_host + ":" + str(daemon.port) + "]"
+daemon.requestLoop()
