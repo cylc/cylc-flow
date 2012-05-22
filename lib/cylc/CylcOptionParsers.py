b32b1ef043b6878e37092d1e39afba5b96b373c4
Hilary Oliver <hilary.j.oliver@gmail.com>
2012-05-22 17:11:16 +0100

    Started disabling remote suite access controls.

--- lib/cylc/CylcOptionParsers.py
+++ lib/cylc/CylcOptionParsers.py
@@ -20,12 +20,11 @@
 
 # TO DO: THESE CLASSES NEED CLEANING UP AND UNIFIED USE IN THE COMMANDS.
 
-import os
-import re
+import os, re
 from optparse import OptionParser
 from hostname import hostname
 
-class NoPromptOptionParser_u( OptionParser ):
+class NoPromptOptionParser( OptionParser ):
 
     def __init__( self, usage, extra_args=None ):
 
@@ -48,7 +47,7 @@ Arguments:
                 action="store", dest="owner" )
 
         self.add_option( "--host",
-                help="Cylc suite host (defaults to local host).",
+                help="Cylc suite host (defaults to localhost).",
                 metavar="HOST", action="store", default=hostname,
                 dest="host" )
 
@@ -61,7 +60,7 @@ Arguments:
         #DISABLED         action="store_true", default=False, dest="practice" )
 
         self.add_option( "-f", "--force",
-                help="(No effect; for consistency with interactive commands)",
+                help="(no effect; for consistency with interactive commands)",
                 action="store_true", default=False, dest="force" )
 
         self.add_option( "--debug",
@@ -78,124 +77,34 @@ Arguments:
 
         self.suite_name = args[0]
 
-        # user name 
-        # self.owner = options.owner  # see default above!
-
-        # cylc suite host
-        # self.host = options.host   # see default above!
-
         #DISABLED self.practice = options.practice  # practice mode or not
-
         return ( options, args )
 
     def get_suite_name( self ):
        return self.suite_name
 
-    def get_groupname( self ):
-        # TO DO: USER PYREX MODULE HERE
-        groupname = ':cylc.' + self.owner + '.' + self.suite_name
-        #DISABLED if self.practice:
-        #DISABLED     groupname += '-practice'
-        return groupname
-
-
-class NoPromptOptionParser( OptionParser ):
-    # same, but no owner
-
-    def __init__( self, usage, extra_args=None ):
-
-        usage += """
-
-You must be the owner of the target suite to use this command.
-
-Arguments:
-   SUITE                Target suite.""" 
-
-        self.n_args = 1  # suite name
-        if extra_args:
-            for arg in extra_args:
-                usage += '\n   ' + arg
-                self.n_args += 1
-
-        OptionParser.__init__( self, usage )
-
-        #self.add_option( "--host",
-        #        help="Cylc suite host (default: localhost).",
-        #        metavar="HOSTNAME", action="store", default=hostname,
-        #        dest="host" )
-
-        #self.add_option( "--port",
-        #        help="Cylc suite port (default: scan cylc ports).",
-        #        metavar="INT", action="store", default=None, dest="port" )
-
-        #DISABLED self.add_option( "-p", "--practice",
-        #DISABLED         help="Target a suite running in practice mode.", 
-        #DISABLED         action="store_true", default=False, dest="practice" )
-
-        self.add_option( "-f", "--force",
-                help="(No effect; for consistency with interactive commands)",
-                action="store_true", default=False, dest="force" )
-
-        self.add_option( "--debug",
-                help="Turn on exception tracebacks.",
-                action="store_true", default=False, dest="debug" )
-
-        self.owner = os.environ['USER']
-
-    def parse_args( self ):
-
-        (options, args) = OptionParser.parse_args( self )
-
-        if len( args ) == 0:
-            self.error( "Please supply a target suite name" )
-        elif len( args ) > self.n_args:
-            self.error( "Too many arguments" )
-
-        self.suite_name = args[0]
-        #self.host = options.host   # see default above!
-        #DISABLED self.practice = options.practice  # practice mode or not
-
-        return ( options, args )
-
-    def get_suite_name( self ):
-        return self.suite_name
-
-    def get_groupname( self ):
-        # TO DO: USER PYREX MODULE HERE
-        groupname = ':cylc.' + self.owner + '.' + self.suite_name
-        #DISABLED if self.practice:
-        #DISABLED     groupname += '-practice'
-        return groupname
-
 
 class PromptOptionParser( NoPromptOptionParser ):
 
     def __init__( self, usage, extra_args=None ):
-
         NoPromptOptionParser.__init__( self, usage, extra_args )
-
         self.remove_option( "-f" )
         self.add_option( "-f", "--force",
                 help="Do not ask for confirmation before acting.",
                 action="store_true", default=False, dest="force" )
 
     def parse_args( self ):
-
         (options, args) = NoPromptOptionParser.parse_args( self )
-
         if options.force:
             self.force = True
         else:
             self.force = False
-
         return (options, args)
 
     def prompt( self, reason ):
         msg =  reason + " '" + self.suite_name + "'"
-
         if self.force:
             return True
-
         response = raw_input( msg + ': ARE YOU SURE (y/n)? ' )
         if response == 'y':
             return True
