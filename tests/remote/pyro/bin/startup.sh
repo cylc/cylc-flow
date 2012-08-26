7aff28fcb894a5480b92641fb345d32e017f6cfa
Hilary James Oliver <h.oliver@niwa.co.nz>
2012-08-27 11:22:17 +1200

    Added remote reference tests

---
+++ tests/remote/pyro/bin/startup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+OWNER=$CYLC_TEST_TASK_OWNER
+HOST=$CYLC_TEST_TASK_HOST
+PPHRASE=$CYLC_SUITE_DEF_PATH/passphrase
+if [[ -z $OWNER ]]; then 
+    echo "ERROR: \$CYLC_TEST_TASK_OWNER is not defined"
+    exit 1
+elif [[ -z $HOST ]]; then
+    echo "ERROR: \$CYLC_TEST_TASK_HOST is not defined"
+    exit 1
+fi
+echo "Copying suite passphrase to ${OWNER}@$HOST"
+DEST=.cylc/$CYLC_SUITE_REG_NAME
+ssh -oBatchmode=yes ${OWNER}@$HOST mkdir -p $DEST
+scp -oBatchmode=yes $PPHRASE ${OWNER}@${HOST}:$DEST/
+echo "Done"
+
