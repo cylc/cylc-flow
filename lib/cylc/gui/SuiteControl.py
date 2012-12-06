cylc/cylc-flow

commit 33a55d174128acb45e7eb839c54b8667b35fd2a5

get-config command line in the style of the broadcast command.

bin/cylc-get-config
@@ -21,6 +21,7 @@ from cylc.remote import remrun
 if remrun().execute():
     sys.exit(0)
 
+import re
 from cylc.config import config, SuiteConfigError
 from cylc.CylcOptionParsers import cop
 from cylc.command_prep import prep_file
@@ -33,19 +34,17 @@ def prdict( dct, level=0, indent=0, prefix='' ):
         else:
             print prefix + '   '*indent + str(key) + ' = ' + str(val) 
 
-parser = cop( usage = """cylc [info] get-config [OPTIONS] REG [ITEMS...]
+parser = cop( usage = """cylc [info] get-config [OPTIONS] REG
 
-Print configuration settings from a parsed suite definition, after
+Print configuration settings parsed from a suite definition, after
 runtime inheritance processing and including default values for items
-that are not explicitly set in the suite.rc file. This command enables,
-for example, automated handling of suites and suite output without
-assuming directory locations and the like.
+that are not explicitly set in the suite.rc file.
 
 Config items containing spaces must be quoted on the command line. If
 a single item is requested only its value will be printed; otherwise the 
 full nested structure below the requested config section is printed. 
 
-Example, from a suite registered as foo.bar.baz:
+Example, from a suite registered as foo.bar:
 |# SUITE.RC
 |[runtime]
 |    [[modelX]]
@@ -53,22 +52,25 @@ Example, from a suite registered as foo.bar.baz:
 |            FOO = foo
 |            BAR = bar
 
-$ cylc get-config foo.bar.baz runtime modelX environment FOO
+$ cylc get-config --item=[runtime][modelX][environment]FOO foo.bar
 foo
 
-$ cylc get-config foo.bar.baz runtime modelX environment
+$ cylc get-config --item=[runtime][modelX][environment] foo.bar 
 FOO = foo
 BAR = bar
 
-$ cylc get-config foo.bar.baz runtime modelX
+$ cylc get-config --item=[runtime][modelX] foo.bar 
 ...
 work directory = $CYLC_SUITE_DEF_PATH/share
 [[[environment]]]
     FOO = foo
     BAR = bar
-...""",
-    argdoc=[('REG', 'Suite name'), 
-        ('[ITEMS...]', 'suite.rc section and item hierarchy')])
+...""" )
+
+parser.add_option( "-i", "--item", metavar="[SEC...]ITEM", 
+        help="Set a runtime item by broadcast. Can be used multiple "
+        "times to broadcast several settings at once.",
+        action="append", dest="item", default=[] )
 
 parser.add_option( "-t", "--tasks",
         help="Print configured task list.",
@@ -78,7 +80,7 @@ parser.add_option( "-d", "--directories",
         help="Print configured suite directory paths.",
         action="store_true", default=False, dest="dpaths" )
 
-parser.add_option( "-m", "--mark-output",
+parser.add_option( "-m", "--mark-up",
         help="Prefix output lines with '!cylc!' to aid "
         "in automated parsing (output can be contaminated by "
         "stdout from login scripts, for example, for remote "
@@ -94,9 +96,15 @@ parser.add_option( "--sparse",
         "file, not underlying default settings.",
         action="store_true", default=False, dest="sparse" )
 
+parser.add_option( "-o", "--one-line", 
+        help="Combine the result from multiple --item requests onto "
+        "one line, with internal spaces replaced by the '*' character. "
+        "For single-value items only.",
+        action="store_true", default=False, dest="oneline" )
+
 parser.add_option( "-a", "--all-tasks",
-        help="For [runtime] items, report values for all tasks. "
-        "Results are prefixed by task name.",
+        help="For [runtime] items (e.g. --item='command scripting') report "
+        "values for all tasks prefixed by task name.",
         action="store_true", default=False, dest="alltasks" )
 
 ( options, args ) = parser.parse_args()
@@ -116,23 +124,20 @@ except Exception,x:
         raise
     raise SystemExit(x)
 
-if options.alltasks:
-    if args[1] == 'runtime':
-        raise SystemExit( "ERROR: --all-tasks is only for items below '[runtime][NAME]' as first argument" )
-    try:
-        data = config.get_config_all_tasks( args[1:], options.sparse )
-    except SuiteConfigError, x:
-        raise SystemExit(x)
-    tasks = data.keys()
-    tasks.sort()
-    for key in tasks:
-        print key, data[key]
+if options.markoutput:
+    prefix = '!cylc!'
+else:
+    prefix = ''
+
+if options.tasks:
+    for task in config.get_task_name_list():
+        print prefix + task
     sys.exit(0)
 
 if options.dpaths:
-    print "SUITE LOG DIRECTORY:\n ", config['cylc']['logging']['directory']
-    print "SUITE STATE DUMP DIRECTORY:\n ", config['cylc']['state dumps']['directory']
-    print "TASK LOG DIRECTORIES:"
+    print prefix + "SUITE LOG DIRECTORY:\n ", config['cylc']['logging']['directory']
+    print prefix + "SUITE STATE DUMP DIRECTORY:\n ", config['cylc']['state dumps']['directory']
+    print prefix + "TASK LOG DIRECTORIES:"
     logdirs = {}
     root_ldir = config.get_config(['runtime','root','log directory'])
     ldirs = [root_ldir]
@@ -145,33 +150,68 @@ if options.dpaths:
         if dir not in ldirs:
             ldirs.append(dir)
             logdirs[ns] = dir
-    print "  + root:", root_ldir
+    print prefix + "  + root:", root_ldir
     for ns in logdirs.keys():
         if ns == "root":
             continue
-        print "  + " + ns + ":", logdirs[ns]
+        print prefix + "  + " + ns + ":", logdirs[ns]
     sys.exit(0)
 
-if options.markoutput:
-    prefix = '!cylc!'
-else:
-    prefix = ''
+get_these = []
+for i in options.item:
+    # construct lists in item nest ordering from --item inputs, i.e.:
+    # "[runtime][foo]command scripting" -> [runtime, foo, "command scripting"
+    i = i.lstrip('[')
+    i = i.rstrip(']')
+    j = re.split( '\]\[*', i )
+    get_these.append(j)
 
-if options.tasks:
-    for task in config.get_task_name_list():
-        print prefix + task
+if options.alltasks:
+    data = []
+    for i in get_these:
+        if i[0] == 'runtime':
+            raise SystemExit( "ERROR: --all-tasks is only for items below '[runtime][NAME]'" )
+        try:
+            data.append( config.get_config_all_tasks( i, options.sparse ))
+        except SuiteConfigError, x:
+            raise SystemExit(x)
+
+    tasks = data[0].keys()
+
+    tasks.sort()
+
+    for task in tasks:
+        if options.oneline:
+            try:
+                print prefix + task, ' '.join( [ d[task].replace(' ','*') for d in data ] )
+            except AttributeError:
+                raise SystemExit( "ERROR: -o is compatible with single-value items only" )
+        else:
+            for d in data:
+                print prefix + task, d[task]
     sys.exit(0)
 
-try:
-    res = config.get_config( args[1:], options.sparse )
-except SuiteConfigError, x:
-    raise SystemExit(x)
+data = []
+for i in get_these:
+    try:
+        data.append( config.get_config( i, options.sparse ))
+    except SuiteConfigError, x:
+        raise SystemExit(x)
 
-if isinstance( res, dict ):
-    if options.pnative:
-        print res
+if options.oneline:
+    try:
+        line = ' '.join( d.replace( ' ', '*' ) for d in data )
+    except AttributeError, x:
+        raise SystemExit( "ERROR: -o is compatible with single-value items only" )
     else:
-        prdict( res, level=len(args[1:]), prefix=prefix)
+        print line
 else:
-    print prefix + str(res)
+    for d in data:
+        if isinstance( d, dict ):
+            if options.pnative:
+                print prefix + d
+            else:
+                prdict( d, level=len(args[1:]), prefix=prefix)
+        else:
+            print prefix + str(d)
 

lib/cylc/gui/SuiteControl.py
@@ -757,12 +757,12 @@ Main Control GUI that displays one or more views or interfaces to the suite.
             #    info_dialog( result.reason, self.window ).inform()
 
     def loadctimes( self, bt, startentry, stopentry ):
-        command = "cylc get-config --mark-output --host=" + \
-                self.cfg.host + " --owner=" + self.cfg.owner + \
+        item1 = " -i [scheduling]['initial cycle time']"
+        item2 = " -i [scheduling]['final cycle time']"
+        command = "cylc get-config --mark-up --host=" + self.cfg.host + \
                 " " + self.cfg.template_vars_opts + " " + \
+                " --owner=" + self.cfg.owner + item1 + item2 + " " + \
                 self.cfg.suite 
-        item1 = " scheduling 'initial cycle time'"
-        item2 = " scheduling 'final cycle time'"
         res1 = run_get_stdout( command + item1, filter=True ) # (T/F,[lines])
         res2 = run_get_stdout( command + item2, filter=True )
 
@@ -2617,8 +2617,7 @@ For more Stop options use the Control menu.""" )
             for option in ["'initial cycle time'", "'final cycle time'"]:
                 command = ( "cylc get-config" + self.get_remote_run_opts() + \
                             " " + self.cfg.template_vars_opts + \
-                            " " + self.cfg.suite + \
-                            " visualization " + option )
+                            " -i [visualization]" + option + " " + self.cfg.suite )
                 res, pieces = run_get_stdout( command )
                 if not res or not pieces:
                     return False
@@ -2658,11 +2657,10 @@ for local suites; I will call "cylc cat-log" instead.""" ).warn()
             return
 
         # just for local suites (so --host and --owner are not needed here)
-        command = "cylc get-config --mark-output " + " " + \
-                self.cfg.template_vars_opts + " " + self.cfg.suite + " cylc logging directory"
+        command = "cylc get-config --mark-up -i [cylc][logging][directory] " + self.cfg.template_vars_opts + " " + self.cfg.suite 
         res, lst = run_get_stdout( command, filter=True )
         logging_dir = lst[0]
-        command = "cylc get-config --mark-output --tasks " + self.cfg.template_vars_opts + " " + self.cfg.suite
+        command = "cylc get-config --mark-up --tasks " + self.cfg.template_vars_opts + " " + self.cfg.suite
         res, tasks = run_get_stdout( command, filter=True )
         if res:
             task_list = tasks

bin/cylc-cat-log
@@ -31,7 +31,7 @@ Print and filter cylc suite (not task) log files.""" )
 
 parser.add_option( "-l", "--location", 
         help="Print the suite log file location and exit. This is "
-        "equivalent to 'cylc get-config SUITE cylc logging directory'.",
+        "equivalent to 'cylc get-config -i [cylc][logging][directory] SUITE'.",
         action="store_true", default=False, dest="print_location" )
 
 parser.add_option( "-t", "--task", 

doc/cug.tex
@@ -1008,7 +1008,7 @@ configured in the example suite, so it defaults, for all tasks, to the
 {\em dummy task} scripting inherited from the {\em root namespace} (see
 Section~\ref{SuiteDefinition}):
 \begin{lstlisting}
-% cylc get-config examples.QuickStart.a runtime GetData 'command scripting'
+% cylc get-config -i [runtime][GetData]'command scripting' examples.QuickStart.a 
 ['echo Dummy command scripting; sleep 10']
 \end{lstlisting}
 The command arguments above reflect suite definition section nesting. 
@@ -1324,7 +1324,7 @@ the suite.rc {\em job submission log directory}, default location
 variable \lstinline=$CYLC_SUITE_REG_NAME= evalutes to the registered
 suite name hierarchy, e.g.\ foo.bar.baz): 
 \begin{lstlisting}
-% get-config examples.QuickStart.a runtime GetData 'job submission' 'log directory'
+% get-config -i '[runtime][GetData][job submission][log directory]' examples.QuickStart.a 
 /home/oliverh/cylc-run/QuickStart.a/log/job
 \end{lstlisting}
 (or use the \lstinline=--directories= argument as shown just above).
@@ -3329,39 +3329,6 @@ right-clicking on tasks in the suite control GUI graph view.}
 \end{figure} 
 
 
-%%\subsection{An Example}
-%%
-%%The following suite.rc file defines a default job
-%%submission method (and an environment variable \lstinline=$COLOR=) for
-%%all tasks; and a family called {\em obs} that contains the tasks {\em
-%%land} and {\em ship}, each of which add to or override some of their
-%%inherited properties.
-%%\lstset{language=suiterc}
-%%\lstinputlisting{../examples/ToDo/appendix/suite.rc}
-%%\lstset{language=transcript}
-%%You can use \lstinline=cylc get-config= to look at the final value 
-%%of any item:
-%%\begin{lstlisting}
-%%% cylc db reg $CYLC_DIR/examples/appendix appx
-%%% cylc get-config appx runtime land environment RUNNING_DIR
-%%$HOME/running/$CYLC_TASK_NAME
-%%\end{lstlisting}
-%%
-%%Using \lstinline=cylc jobscript= you can generate job scripts (used by cylc
-%%to run tasks) showing the complete runtime configuration, resulting from 
-%%namespace inheritance, for a task. The namespace hierarchy itself can be
-%%printed with \lstinline=cylc list --tree=:
-%%
-%%\begin{lstlisting}
-%%% cylc list -t appx
-%%root      
-%% |-obs    
-%% | |-land land obs processing
-%% | `-ship ship obs processing
-%% |-prep   No description provided
-%% `-prod   No description provided
-%%\end{lstlisting}
-
 \subsection{Jinja2 Suite Templates}
 \label{Jinja2}
 
@@ -3540,7 +3507,7 @@ example, from \lstinline=$CYLC_DIR/examples/jinja2/dict=:
 Here's the result:
 \lstset{language=transcript}
 \begin{lstlisting}
-% cylc get-config SUITE runtime airs directives
+% cylc get-config -i [runtime][airs]directives SUITE 
 -I = ncpus=9
 \end{lstlisting}
 

lib/cylc/config.py
@@ -521,10 +521,7 @@ class config( CylcConfigObj ):
     def get_config( self, args, sparse=False ):
         target = self
         keys = args
-        if len(args) == 0 or len(args) == 1 or sparse:
-            # don't populate [runtime] with all default settings
-            print >> sys.stderr, "WARNING: returning sparse [runtime]!"
-        elif args[0] == 'runtime':
+        if args[0] == 'runtime' and not sparse:
             # load and override runtime defaults
             rtcfg = {}
             replicate( rtcfg, self.runtime_defaults )

lib/cylc/gui/gcylc.py
@@ -1540,8 +1540,8 @@ The Cylc Suite Engine.
         foo.run()
 
     def describe_suite( self, w, name ):
-        command = """echo '> TITLE:'; cylc get-config """ + self.dbopt + " " + name + """ title; echo
-echo '> DESCRIPTION:'; cylc get-config """ + self.dbopt + " --notify-completion " + name + " description"
+        command = """echo '> TITLE:'; cylc get-config """ + self.dbopt + " -i title " + name + """; echo
+echo '> DESCRIPTION:'; cylc get-config """ + self.dbopt + " --notify-completion -i description " + name 
         foo = gcapture_tmpfile( command, self.tmpdir, 800, 400 )
         self.gcapture_windows.append(foo)
         foo.run()

commit ca8a71f0edafb370f5a70321fb21ef5c3e2cec0f

Bug fix gcylc start-suite popup load cycle times.
The initial cycle time was also being put into entry panel for the final
cycle time. This feature was broken by
33a55d174128acb45e7eb839c54b8667b35fd2a5 on 19 Nov 2012.

bin/cylc-get-config
@@ -199,7 +199,7 @@ if options.oneline:
     except AttributeError, x:
         raise SystemExit( "ERROR: -o is compatible with single-value items only" )
     else:
-        print line
+        print prefix + line
 else:
     for d in data:
         if isinstance( d, dict ):

lib/cylc/gui/SuiteControl.py
@@ -68,7 +68,7 @@ def run_get_stdout( command, filter=False ):
         # output is a single string with newlines; but we return a list of
         # lines filtered (optionally) for a special '!cylc!' prefix.
         res = []
-        for line in out.split():
+        for line in out.split('\n'):
             line.strip()
             if filter:
                 if line.startswith( '!cylc!' ):
@@ -759,18 +759,16 @@ Main Control GUI that displays one or more views or interfaces to the suite.
             #    info_dialog( result.reason, self.window ).inform()
 
     def loadctimes( self, bt, startentry, stopentry ):
-        item1 = " -i [scheduling]['initial cycle time']"
-        item2 = " -i [scheduling]['final cycle time']"
+        item1 = " -i '[scheduling]initial cycle time'"
+        item2 = " -i '[scheduling]final cycle time'"
         command = "cylc get-config --mark-up --host=" + self.cfg.host + \
                 " " + self.cfg.template_vars_opts + " " + \
-                " --owner=" + self.cfg.owner + item1 + item2 + " " + \
+                " --owner=" + self.cfg.owner + " --one-line" + item1 + item2 + " " + \
                 self.cfg.suite 
-        res1 = run_get_stdout( command + item1, filter=True ) # (T/F,[lines])
-        res2 = run_get_stdout( command + item2, filter=True )
+        res = run_get_stdout( command, filter=True ) # (T/F,['ct ct'])
 
-        if res1[0] and res2[0]:
-            out1 = res1[1][0]
-            out2 = res2[1][0]
+        if res[0]:
+            out1, out2 = res[1][0].split()
             if out1 == "None" and out2 == "None":  # (default value from suite.rc spec)
                 info_dialog( """Initial and final cycle times have not
 been defined for this suite""").inform()