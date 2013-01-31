7ddc9351f9ae405855269068ce493253883f8d56
Hilary James Oliver <h.oliver@niwa.co.nz>
2013-02-01 00:38:34 +1300

    Automate gh-pages updates from master.
    1) gh-pages content is now held in master doc/gh-pages/
    2) scaled images for html docs are now version controlled rather than
    scaled on-the-fly from the originals (these have to be stored on the
    gh-pages branch and we want to avoid "new" binary files with each update).
    3) new admin script to automatically update the gh-pages branch, and
    optionally push to github, with said content and fresh html user guides.

---
+++ admin/update-gh-pages.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+set -e
+set -x
+
+function usage {
+cat <<eof
+USAGE update-gh-pages.sh [-p] COMMIT-MESSAGE
+
+Check out gh-pages, update its files from doc/ in master branch, and
+then optionally [-p] push to the cylc repository on github.
+
+Before running this you should 'make html-single' in the master doc
+directory and record any changes to the single-page html user guide or
+its css file (and any changes to doc/online/index.html).
+
+eof
+}
+
+if [[ $# < 1 ]]; then
+    usage
+    exit 1
+fi
+
+PUSH=false
+if [[ $1 == -p ]]; then
+    PUSH=true
+    shift
+fi
+
+if [[ $# == 0 ]]; then
+    usage
+    exit 1
+fi
+
+COMMITMSG=$@
+
+LATESTTAG=$( git describe --abbrev=0 --tags )
+
+[[ -z $COMMITMSG ]] && exit 1
+
+# make html user guides
+cd doc
+make html
+
+# cp online content to a temp dir
+TMPD=$( mktemp -d )
+cp -r gh-pages/ graphics/ html/ $TMPD/
+
+# return to top level
+cd ..
+
+# checkout gh-pages branch
+git checkout gh-pages
+
+# replace the online content
+cp -r $TMPD/gh-pages/* .
+cp $TMPD/html/single/*.{css,html} html/single/
+cp $TMPD/html/multi/*.{css,html} html/multi/
+cp $TMPD/graphics/png/scaled/* graphics/png/scaled/
+# substitute latest version number in the homepage
+perl -pi -e "s/Current Version:.*/Current Version: $LATESTTAG ($( date +%F ))/" index.html
+
+# any changes to update?
+git update-index -q --refresh
+if [[ -n "$(git diff-index --name-only HEAD --)" ]]; then 
+    echo "committing changes"; 
+    git commit -a -m "$COMMITMSG"
+else 
+    # (attempted commit is an error if there's nothing to commit)
+    echo "no changes to commit";
+fi
+
+# push to github if requested
+$PUSH && git push cylc gh-pages
+
+# return to master
+git checkout master
+
+echo "DONE: gh-pages updated."
+
