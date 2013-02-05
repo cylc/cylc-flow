0f1e16b981c6e051b6303165d622a8812fa5a7a5
Hilary James Oliver <h.oliver@niwa.co.nz>
2013-02-05 13:20:09 +1300

    Better use of flag to stimulate dependency negotiation
    Incoming messages that change task outputs set flags.pflag to signal
    that dependency negotation is required. The flag should only be turned
    off again after it has been used, else dependency negotation could be
    missed if the flag gets turned on then off in quick succession.

--- lib/cylc/task_types/task.py
+++ lib/cylc/task_types/task.py
@@ -712,17 +712,16 @@ class task( object ):
             queue.task_done()
 
     def process_incoming_message( self, (priority, message) ):
+
+        # always update the suite state summary for latest message
+        flags.iflag = True
+
         if self.reject_if_failed( message ):
             # Failed tasks do not send messages unless declared resurrectable
             return
 
         self.latest_message = message
         self.latest_message_priority = priority
-
-        # Set this to stimulate task processing
-        flags.pflag = True
-        # Set this to stimulate suite state summary update even if not task processing.
-        flags.iflag = False
 
         # Handle warning events
         handler = self.event_handlers['warning']
@@ -736,6 +735,7 @@ class task( object ):
 
         if message == self.id + ' started':
             # Received a 'task started' message
+            flags.pflag = True
             self.set_running()
 
         elif message == self.id + ' submitted':
@@ -751,6 +751,7 @@ class task( object ):
             # (note not 'elif' here as started messages must go through
             # the elif block below)
             # Received a 'task failed' message
+            flags.pflag = True
             self.succeeded_time = task.clock.get_datetime()
             try:
                 # Is there a retry lined up for this task?
@@ -783,6 +784,7 @@ class task( object ):
             # Received a registered internal output message
             # (this includes 'task succeeded')
             if not self.outputs.is_completed( message ):
+                flags.pflag = True
                 self.log( priority,  message )
                 self.outputs.set_completed( message )
                 if message == self.id + ' succeeded':
@@ -801,14 +803,10 @@ class task( object ):
                 # Currently this is treated as an error condition
                 self.log( 'WARNING', "UNEXPECTED OUTPUT (already completed):" )
                 self.log( 'WARNING', "-> " + message )
-                flags.pflag = False
-
         else:
             # A general unregistered progress message: log with a '*' prefix
             message = '*' + message
             self.log( priority, message )
-            flags.iflag = True
-            flags.pflag = False
 
     def update( self, reqs ):
         for req in reqs.get_list():
