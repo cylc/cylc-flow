9f24e4a8fb5386ceb793efa0fbdbdfe407e486fd
Hilary James Oliver <hilary.j.oliver@gmail.com>
2013-02-27 10:21:11 +1300

    Only inherit from root after processing all other inheritance levels.
    Otherwise root items that get inherited into higher namespaces can
    subsequently override the same item in intermediate namespaces.

--- lib/cylc/config.py
+++ lib/cylc/config.py
@@ -433,6 +433,8 @@ class config( CylcConfigObj ):
         computed_already = {}
         
         for label in self['runtime']:
+            ##print label,
+
             if label == 'root':
                 continue
 
@@ -448,23 +450,47 @@ class config( CylcConfigObj ):
             hierarchy = copy(self.runtime['linearized ancestors'][label])
             hierarchy.reverse()
 
+            ##print hierarchy
+
             rtns = OrderedDict()
 
             mro = []
             prev = None
 
-            ##print
-            ##print 'IN:', label, hierarchy
-            for name in hierarchy:
+            # First inherit all excluding root. Why? Consider this:
+            #___________________
+            # [runtime]
+            #   [[root]]
+            #       [[[environment]]]
+            #           FOO = root
+            #   [[A]]
+            #       # (inherits FOO=root from root)
+            #   [[B]]
+            #       [[[environment]]]
+            #           (override FOO from root)
+            #           FOO = B
+            #   [[C]]
+            #      inherit = A,B
+            #-------------------
+            # Here A inherits FOO from root and B overrides it. When we
+            # compute the content of C, if A and B have already
+            # inherited down to root (in the sense that the content of A
+            # and B is now the result of all inheritance) then A:FOO
+            # will override B:FOO even though A:FOO has come through
+            # from root and B:FOO should take precedence. SO: we do all
+            # inheritance except root here, and just use root to supply
+            # defaults at the end.
+            for name in hierarchy[1:]:
                 mro.append( name )
                 i_mro = '*'.join( mro )
-                ##print 'MRO', i_mro
+                ##print i_mro,
                 if i_mro in computed_already:
+                    ##print '(done)'
                     rtns = computed_already[i_mro]
                 else:
                     ##print 'INHERIT:',
-                    ##if len(mro) > 1:
-                    ##    print mro[:-1], mro[-1]
+                    ###if len(mro) > 1:
+                    ###   print mro[:-1], mro[-1]
                     ##else:
                     ##    print mro[0]
 
@@ -480,8 +506,18 @@ class config( CylcConfigObj ):
                 prev = name
 
             self['runtime'][label] = rtns
-            ##print 'OUT:', label, taskconf.items()
 
+        # Now (with refence to the comment section above) apply root
+        # wherever its items have not been overridden in the hierarchy.
+        for label in self['runtime']:
+            tmp = OrderedDict()
+            # copy root
+            replicate( tmp, self['runtime']['root'].odict())
+            # override root into tmp, and replace
+            replicate( tmp, self['runtime'][label].odict())
+            self['runtime'][label] = tmp
+
+ 
     def compute_runahead_limit( self ):
         # take the smallest of the default limits from each graph section
         rl = None
