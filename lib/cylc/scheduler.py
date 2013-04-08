004ec99331ac92ab53b2b3222daf0ba1bdf2762c
matthew.shin <matthew.shin@metoffice.gov.uk>
2013-04-08 13:03:44 +0100

    suite environment file: correct syntax for scp.

--- lib/cylc/scheduler.py
+++ lib/cylc/scheduler.py
@@ -291,20 +291,25 @@ class scheduler(object):
 
         self.configure_environments()
 
-        task_log_dir = gcfg.get_derived_host_item( self.suite, 'suite job log directory' )
-        env_file_path = os.path.join(task_log_dir, "cylc-suite-env")
+        suite_run_dir = os.path.expandvars(
+                gcfg.get_derived_host_item(self.suite, 'suite run directory'))
+        env_file_path = os.path.join(suite_run_dir, "cylc-suite-env")
         f = open(env_file_path, 'wb')
         for key, value in task.task.cylc_env.items():
             f.write("%s=%s\n" % (key, value))
         f.close()
+        r_suite_run_dir = os.path.expandvars(
+                gcfg.get_derived_host_item(self.suite, 'suite run directory'))
         for user_at_host in self.old_user_at_host_set:
             if '@' in user_at_host:
                 user, host = user_at_host.split('@', 1)
             else:
                 user, host = None, user_at_host
             # this handles defaulting to localhost:
-            task_log_dir = gcfg.get_derived_host_item( self.suite, 'suite job log directory', host, user )
-            r_env_file_path = '%s:%s/cylc-suite-env' % (user_at_host, task_log_dir)
+            r_suite_run_dir = gcfg.get_derived_host_item(
+                    self.suite, 'suite run directory', host, user)
+            r_env_file_path = '%s:%s/cylc-suite-env' % (
+                    user_at_host, r_suite_run_dir)
             cmd = ['scp', '-oBatchMode=yes', env_file_path, r_env_file_path]
             if subprocess.call(cmd): # return non-zero
                 raise Exception("ERROR: " + str(cmd))
