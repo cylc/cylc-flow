e7a7a71f4a5a7726bed2b28b621e62f765e4e9d8
Hilary James Oliver <h.oliver@niwa.co.nz>
2014-02-18 09:55:28 +1300

    Test battery help, and default test env vars.

--- tests/remote/basic/bin/startup.sh
+++ tests/remote/basic/bin/startup.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
 set -e
-OWNER=$CYLC_TEST_TASK_OWNER
-HOST=$CYLC_TEST_TASK_HOST
 PPHRASE=$CYLC_SUITE_DEF_PATH/passphrase
-if [[ -z $OWNER ]]; then 
-    echo "ERROR: \$CYLC_TEST_TASK_OWNER is not defined"
-    exit 1
-elif [[ -z $HOST ]]; then
-    echo "ERROR: \$CYLC_TEST_TASK_HOST is not defined"
-    exit 1
-fi
-if [[ $OWNER@$HOST == $USER@localhost || $OWNER@$HOST == $USER@$(hostname) ]]; then
+if [[ $CYLC_TEST_TASK_OWNER@$CYLC_TEST_TASK_HOST == $USER@localhost || $CYLC_TEST_TASK_OWNER@$CYLC_TEST_TASK_HOST == $USER@$(hostname) ]]; then
     exit
 fi
 # copy the passphrase over in case this host does not use ssh messaging
-echo "Copying suite passphrase to ${OWNER}@$HOST"
+echo "Copying suite passphrase to ${CYLC_TEST_TASK_OWNER}@$CYLC_TEST_TASK_HOST"
 DEST=.cylc/$CYLC_SUITE_REG_NAME
-ssh -oBatchmode=yes ${OWNER}@$HOST mkdir -p $DEST
-scp -oBatchmode=yes $PPHRASE ${OWNER}@${HOST}:$DEST/
+ssh -oBatchmode=yes ${CYLC_TEST_TASK_OWNER}@$CYLC_TEST_TASK_HOST mkdir -p $DEST
+scp -oBatchmode=yes $PPHRASE ${CYLC_TEST_TASK_OWNER}@${CYLC_TEST_TASK_HOST}:$DEST/
 echo "Done"
