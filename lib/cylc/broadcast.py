ff7ddfe0838bcc269ee36078c1df9ce9160854d8
Matt Shin <matthew.shin@metoffice.gov.uk>
2015-03-23 16:06:55 +0000

    cylc broadcast: improve diagnostics on changes
    For add, cancel and clear, report broadcast settings to suite log and
    back to client.
    
    For cancel and clear, if any `--point=CYCLE_POINT`, `--namespace=NAME`
    or `--cancel=[SEC]ITEM` options have no broadcasts, report them to suite
    log and back to client, and cause client to return non-zero code.

--- lib/cylc/broadcast.py
+++ lib/cylc/broadcast.py
@@ -21,6 +21,8 @@ from copy import deepcopy
 from datetime import datetime
 import logging, os, sys
 import cPickle as pickle
+from cylc.broadcast_report import (
+    get_broadcast_change_report, get_broadcast_bad_options_report)
 from cylc.task_id import TaskID
 from cycling.loader import get_point
 from rundb import RecordBroadcastObject
@@ -33,71 +35,70 @@ class broadcast( Pyro.core.ObjBase ):
     #self.settings[ 'all-cycle-points' ][ 'root' ] = "{ 'environment' : { 'FOO' : 'bar' }}
     #self.settings[ '2010080806' ][ 'root' ] = "{ 'command scripting' : 'stuff' }
 
-    def __init__( self, linearized_ancestors ):
+    def __init__(self, linearized_ancestors):
         self.log = logging.getLogger('main')
         self.settings = {}
-        self.last_settings = self.get_dump()
+        self.prev_dump = self.get_dump()
         self.settings_queue = []
         self.linearized_ancestors = linearized_ancestors
         Pyro.core.ObjBase.__init__(self)
 
-    def prune( self, target ):
-        # remove empty leaves left by unsetting broadcast values
-        def _prune( target ):
-            # recursive sub-function
-            pruned = False
-            for key, val in target.items():
-                if isinstance( val, dict ):
-                    if not val:
-                        del target[key]
-                        pruned = True
-                    else:
-                        pruned = _prune( target[key] )
-                else:
-                    if not val:
-                        del target[key]
-                        pruned = True
-            return pruned
-        # prune until no further changes
-        while _prune( target ):
-            continue
+    def prune(self):
+        """Remove empty leaves left by unsetting broadcast values."""
+        prunes = []
+        stuffs = [([], self.settings, True)]
+        while stuffs:
+            keys, stuff, is_new = stuffs.pop()
+            if is_new:
+                stuffs.append((keys, stuff, False))
+                for key, value in stuff.items():
+                    if isinstance(value, dict):
+                        stuffs.append((keys + [key], value, True))
+            else:
+                for key, value in stuff.items():
+                    if not value:
+                        del stuff[key]
+                        prunes.append(keys + [key])
+        return prunes
 
-    def addict( self, target, source ):
+    def addict(self, target, source):
+        """Recursively add source dict to target dict."""
         for key, val in source.items():
-            if isinstance( val, dict ):
+            if isinstance(val, dict):
                 if key not in target:
                     target[key] = {}
-                self.addict( target[key], val )
+                self.addict(target[key], val)
             else:
                 if source[key]:
                     target[key] = source[key]
                 elif key in target:
                     del target[key]
 
-    def put( self, namespaces, cycles, settings ):
-        """Add new broadcast settings, or prune newly unset ones"""
+    def put(self, namespaces, point_strings, settings):
+        """Add new broadcast settings."""
+        modified_settings = []
         for setting in settings:
-            for cycle in cycles:
-                if cycle not in self.settings.keys():
-                    self.settings[cycle] = {}
+            for point_string in point_strings:
+                if point_string not in self.settings:
+                    self.settings[point_string] = {}
                 for namespace in namespaces:
-                    if namespace not in self.settings[cycle]:
-                        self.settings[cycle][namespace] = {}
-                    self.addict( self.settings[cycle][namespace], setting )
-        # prune any empty branches
-        self.prune( self.settings )
+                    if namespace not in self.settings[point_string]:
+                        self.settings[point_string][namespace] = {}
+                    self.addict(
+                        self.settings[point_string][namespace], setting)
+                    modified_settings.append(
+                        (point_string, namespace, setting))
 
-        if self.get_dump() != self.last_settings:
-            current_time_string = get_current_time_string(
-                display_sub_seconds=True)
-            self.settings_queue.append(RecordBroadcastObject(
-                current_time_string, self.get_dump() ))
-            self.last_settings = self.settings
+        self.prune()
+        self._update_db_queue()
 
-        return ( True, 'OK' )
+        # Log the broadcast
+        self.log.info(get_broadcast_change_report(modified_settings))
 
-    def get( self, task_id=None ):
-        # Retrieve all broadcast variables that target a given task ID.
+        return modified_settings
+
+    def get(self, task_id=None):
+        """Retrieve all broadcast variables that target a given task ID."""
         if not task_id:
             # all broadcast settings requested
             return self.settings
@@ -108,18 +109,18 @@ class broadcast( Pyro.core.ObjBase ):
         #    all:root -> all:FAM -> ... -> all:task
         # -> tag:root -> tag:FAM -> ... -> tag:task
         # DEPRECATED at cylc 6: 'all-cycles'
-        for cycle in [ 'all-cycle-points', 'all-cycles', point_string ]:
+        for cycle in ['all-cycle-points', 'all-cycles', point_string]:
             if cycle not in self.settings:
                 continue
             for ns in reversed(self.linearized_ancestors[name]):
                 if ns in self.settings[cycle]:
-                    self.addict( ret, self.settings[cycle][ns] )
+                    self.addict(ret, self.settings[cycle][ns])
         return ret
 
-    def expire( self, cutoff ):
+    def expire(self, cutoff):
         """Clear all settings targetting cycle points earlier than cutoff."""
         if not cutoff:
-            self.log.info( 'Expiring all broadcast settings now' )
+            self.log.info('Expiring all broadcast settings now')
             self.settings = {}
         for point_string in self.settings.keys():
             # DEPRECATED at cylc 6: 'all-cycles'
@@ -127,50 +128,117 @@ class broadcast( Pyro.core.ObjBase ):
                 continue
             point = get_point(point_string)
             if point < cutoff:
-                self.log.info( 'Expiring ' + str(point) + ' broadcast settings now' )
+                self.log.info('Expiring ' + str(point) + ' broadcast settings now')
                 del self.settings[ point_string ]
 
-    def clear( self, namespaces, point_strings ):
-        """
-        Clear settings globally, or for listed namespaces and/or points.
-        """
-        if not namespaces and not point_strings:
-            # clear all settings
-            self.settings = {}
-        elif point_strings:
-            # clear all settings specific to given point_strings
-            for point_string in point_strings:
-                try:
-                    del self.settings[point_string]
-                except:
-                    pass
-        elif namespaces:
-            # clear all settings specific to given namespaces
-            for point_string in self.settings.keys():
-                for ns in namespaces:
-                    try:
-                        del self.settings[point_string][ns]
-                    except:
-                        pass
-        if self.get_dump() != self.last_settings:
-            self.settings_queue.append(RecordBroadcastObject(
-                get_current_time_string(display_sub_seconds=True),
-                self.get_dump()
-            ))
-            self.last_settings = self.settings
+    def clear(self, namespaces, point_strings, cancel_settings=None):
+        """Clear settings globally, or for listed namespaces and/or points."""
+        # If cancel_settings defined, only clear specific settings
+        cancel_keys_list = []
+        if cancel_settings:
+            for cancel_setting in cancel_settings:
+                stuffs = [([], cancel_setting)]
+                while stuffs:
+                    keys, stuff = stuffs.pop()
+                    for key, value in stuff.items():
+                        if isinstance(value, dict):
+                            stuffs.append((keys + [key], value))
+                        else:
+                            cancel_keys_list.append(keys + [key])
+        # Clear settings
+        modified_settings = []
+        for point_string, point_string_settings in self.settings.items():
+            if point_strings and point_string not in point_strings:
+                continue
+            for namespace, namespace_settings in point_string_settings.items():
+                if namespaces and namespace not in namespaces:
+                    continue
+                stuffs = [([], namespace_settings)]
+                while stuffs:
+                    keys, stuff = stuffs.pop()
+                    for key, value in stuff.items():
+                        if isinstance(value, dict):
+                            stuffs.append((keys + [key], value))
+                        elif (not cancel_keys_list or
+                                keys + [key] in cancel_keys_list):
+                            stuff[key] = None
+                            setting = {key: value}
+                            for rkey in reversed(keys):
+                                setting = {rkey: setting}
+                            modified_settings.append(
+                                (point_string, namespace, setting))
 
-    def dump( self, FILE ):
-        # write broadcast variables to the suite state dump file
-        FILE.write( pickle.dumps( self.settings) + '\n' )
+        # Prune any empty branches
+        bad_options = self._get_bad_options(
+            self.prune(), point_strings, namespaces, cancel_keys_list)
+        self._update_db_queue()
+
+        # Log the broadcast
+        self.log.info(
+            get_broadcast_change_report(modified_settings, is_cancel=True))
+        if bad_options:
+            self.log.error(get_broadcast_bad_options_report(bad_options))
+
+        return (modified_settings, bad_options)
+
+    def dump(self, file_):
+        """Write broadcast variables to the state dump file."""
+        pickle.dump(self.settings, file_)
+        file_.write("\n")
 
     def get_db_ops(self):
+        """Return the next DB operations from DB queue."""
         ops = self.settings_queue
         self.settings_queue = []
         return ops
 
-    def get_dump( self ):
-        # return the broadcast variables as written to the suite state dump file
-        return pickle.dumps( self.settings ) + '\n'
+    def get_dump(self):
+        """Return broadcast variables as written to the state dump file."""
+        return pickle.dumps(self.settings) + "\n"
 
-    def load( self, pickled_settings ):
-        self.settings = pickle.loads( pickled_settings )
+    def load(self, pickled_settings):
+        """Load broadcast variables from the state dump file."""
+        self.settings = pickle.loads(pickled_settings)
+
+    @staticmethod
+    def _get_bad_options(prunes, point_strings, namespaces, cancel_keys_list):
+        """Return unpruned namespaces and/or point_strings options."""
+        bad_options = {
+            "point_strings": None, "namespaces": None, "cancel": None}
+        if point_strings:
+            bad_options["point_strings"] = set(point_strings)
+            for point_string in point_strings:
+                if point_string in [prune[0] for prune in prunes]:
+                    bad_options["point_strings"].discard(point_string)
+                    if not bad_options["point_strings"]:
+                        break
+        if namespaces:
+            bad_options["namespaces"] = set(namespaces)
+            for namespace in namespaces:
+                if namespace in [prune[1] for prune in prunes if prune[1:]]:
+                    bad_options["namespaces"].discard(namespace)
+                    if not bad_options["namespaces"]:
+                        break
+        if cancel_keys_list:
+            bad_options["cancel"] = set(
+                [tuple(cancel_keys) for cancel_keys in cancel_keys_list])
+            for cancel_keys in cancel_keys_list:
+                if (list(cancel_keys) in
+                        [prune[2:] for prune in prunes if prune[2:]]):
+                    bad_options["cancel"].discard(tuple(cancel_keys))
+                    if not bad_options["cancel"]:
+                        break
+        for key, value in bad_options.items():
+            if value:
+                bad_options[key] = list(value)
+            else:
+                del bad_options[key]
+        return bad_options
+
+    def _update_db_queue(self):
+        """Update the queue to the runtime DB."""
+        this_dump = self.get_dump()
+        if this_dump != self.prev_dump:
+            now = get_current_time_string(display_sub_seconds=True)
+            self.settings_queue.append(RecordBroadcastObject(now, this_dump))
+            self.prev_dump = this_dump
