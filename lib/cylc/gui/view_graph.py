7d7e61b87223fb93d9cefd1586d82c6a08717fbd
Oliver sanders <oliver.sanders@metoffice.gov.uk>
2016-02-26 15:02:23 +0000

    re-enabled right click menu(s).

--- lib/cylc/gui/view_graph.py
+++ lib/cylc/gui/view_graph.py
@@ -184,15 +184,16 @@ Dependency graph suite control interface.
         menu.append(ungroup_rec_item)
 
         if type == 'live task':
-            # incase stopped
-            if task_id not in self.t.fam_state_summary:
-                return False
 
             is_fam = (name in self.t.descendants)
             if is_fam:
+                if task_id not in self.t.fam_state_summary:
+                    return False
                 t_state = self.t.fam_state_summary[task_id]['state']
                 submit_num = None
             else:
+                if task_id not in self.t.state_summary:
+                    return False
                 t_state = self.t.state_summary[task_id]['state']
                 submit_num = self.t.state_summary[task_id]['submit_num']
             default_menu = self.get_right_click_menu(
