969982f579e36af4c56a540130c79141938eb60b
RosalynHatcher <r.s.hatcher@reading.ac.uk>
2017-10-30 15:08:21 +0000

    Remove trailing whitespace

--- lib/cylc/batch_sys_handlers/sge.py
+++ lib/cylc/batch_sys_handlers/sge.py
@@ -63,7 +63,7 @@ class SGEHandler(object):
     def get_poll_many_cmd(cls, job_ids):
         """Return poll command"""
         # No way to run POLL_CMD on specific job id(s). List all user's jobs.
-        # batch_sys_manager._jobs_poll_batch_sys checks requested id in list. 
+        # batch_sys_manager._jobs_poll_batch_sys checks requested id in list.
         return [cls.POLL_CMD]
 
 BATCH_SYS_HANDLER = SGEHandler()
