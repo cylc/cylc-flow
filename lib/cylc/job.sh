54f669ef7ca4ebc225a369584085b9acbcd1b41a
Hilary James Oliver <hilary.j.oliver@gmail.com>
2018-10-21 16:42:17 +1300

    New job env var: CYLC_TASK_LOG_DIR.

--- lib/cylc/job.sh
+++ lib/cylc/job.sh
@@ -98,7 +98,8 @@ cylc__job__main() {
     # Otherwise, a zero padded number will be interpreted as an octal.
     export CYLC_TASK_SUBMIT_NUMBER=$((10#$(cut -d '/' -f 3 <<<"${CYLC_TASK_JOB}")))
     export CYLC_TASK_ID="${CYLC_TASK_NAME}.${CYLC_TASK_CYCLE_POINT}"
-    export CYLC_TASK_LOG_ROOT="${CYLC_SUITE_RUN_DIR}/log/job/${CYLC_TASK_JOB}/job"
+    export CYLC_TASK_LOG_DIR="${CYLC_SUITE_RUN_DIR}/log/job/${CYLC_TASK_JOB}"
+    export CYLC_TASK_LOG_ROOT="${CYLC_TASK_LOG_DIR}/job"
     if [[ -n "${CYLC_TASK_WORK_DIR_BASE:-}" ]]; then
         # Note: value of CYLC_TASK_WORK_DIR_BASE may contain variable
         # substitution syntax including some of the derived variables above, so
