dc873b0da9584330c51345a43c34f50d635a23ee
Martin Ryan <martynmryan@gmail.com>
2018-12-17 12:16:36 +1100

    defused xml

--- lib/cherrypy/lib/xmlrpcutil.py
+++ lib/cherrypy/lib/xmlrpcutil.py
@@ -5,6 +5,10 @@ from cherrypy._cpcompat import ntob
 
 
 def get_xmlrpclib():
+    # XMLRPC is particularly dangerous as it is also concerned with
+    # communicating data over a network. Use defused.xmlrpc.monkey_patch()
+    # function to monkey-patch xmlrpclib and mitigate remote XML attacks.
+    # https://docs.openstack.org/developer/bandit/blacklists/blacklist_imports.html#b411-import-xmlrpclib
     try:
         import defusedxml.client as x
     except ImportError:
