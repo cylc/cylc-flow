3fc377ea081447fb90849636c43b9c030a50e86d
Martin Ryan <martynmryan@gmail.com>
2019-01-07 23:29:07 +1100

    rebase conflict resolution
    shebang update
    change to python2 to prevent bandit ast errors

--- tests/xtriggers/02-persistence/faker_fail.py
+++ tests/xtriggers/02-persistence/faker_fail.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python2
 
+
 def faker(name, debug=False):
     print "%s: failing" % name
     return (False, {"name": name})
--- tests/xtriggers/02-persistence/faker_succ.py
+++ tests/xtriggers/02-persistence/faker_succ.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python2
 
+
 def faker(name, debug=False):
     print "%s: succeeding" % name
     return (True, {"name": name})
