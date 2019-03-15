7f17d7709931ce5522ec0d7619114cff0c29c151
Thomas Coleman <thomas.coleman@bom.gov.au>
2019-03-15 14:54:42 +1100

    Add tests for quoting of tilde expressions in environment section

---
+++ lib/cylc/tests/test_job_file.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+# THIS FILE IS PART OF THE CYLC SUITE ENGINE.
+# Copyright (C) 2008-2019 NIWA & British Crown (Met Office) & Contributors.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import unittest
+
+from cylc.job_file import JobFileWriter
+
+
+# List of tilde variable inputs
+# input value, expected output value
+TILDE_IN_OUT = [('~foo/bar bar', '~foo/"bar bar"'),
+                ('~/bar bar', '~/"bar bar"'),
+                ('~/a', '~/"a"'),
+                ('test', '"test"'),
+                ('~', '~'),
+                ('~a', '~a')]
+
+
+class TestJobFile(unittest.TestCase):
+    def test_get_variable_value_definition(self):
+        """Test the value for single/tilde variables are correctly quoted"""
+        for in_value, out_value in TILDE_IN_OUT:
+            res = JobFileWriter._get_variable_value_definition(in_value)
+            self.assertEqual(out_value, res)
+
+
+if __name__ == '__main__':
+    unittest.main()
