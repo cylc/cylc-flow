6508fac0d9a0ee9e2c586b790357064306772167
Samuel Gaist <samuel.gaist@idiap.ch>
2019-10-29 00:15:47 +0100

    [scripts][cylc] Fixed old header

--- cylc/flow/scripts/cylc.py
+++ cylc/flow/scripts/cylc.py
@@ -1,5 +1,6 @@
-#!/usr/bin/env python
-# Copyright (C) 2008-2017 NIWA
+#!/usr/bin/env python3
+# THIS FILE IS PART OF THE CYLC SUITE ENGINE.
+# Copyright (C) 2008-2019 NIWA & British Crown (Met Office) & Contributors.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -13,6 +14,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""cylc main entry point"""
 import os
 import sys
 
