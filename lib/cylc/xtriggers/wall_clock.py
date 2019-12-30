294fead2344f4470fa5c81bf48e3eced2a7d1860
Thomas Coleman <thomas.coleman@bom.gov.au>
2019-12-30 14:31:33 +1100

    Extend wall_clock xtrigger to allow a timezone to be specified

--- lib/cylc/xtriggers/wall_clock.py
+++ lib/cylc/xtriggers/wall_clock.py
@@ -22,12 +22,47 @@
 
 from time import time
 from cylc.cycling.iso8601 import interval_parse
+from cylc.cylc_subproc import procopen
 
 
-def wall_clock(offset=None, point_as_seconds=None):
-    """Return True if now > (point + offset) else False."""
+def wall_clock(offset=None, time_zone=None, point_as_seconds=None):
+    """Return True if now > (point + offset) else False.
+
+    If a time zone is provided, it must be a string available from terminal via
+    the `timedatectl list-timezones` command. When used, this method will check
+    for the current cycle point (plus the offset) adjusted to check as if the
+    cycle point is for the provided time zone. The time zone functionality
+    only works with suites with UTC cycle points as the xtrigger has no way of
+    knowing the suites cycle point and has to assume it is in UTC.
+
+    For example:
+    offset=None, time_zone='America/New_York', cycle='20200101T0000Z'
+    checks for for midnight on 1 Jan 2020 local New York time.
+
+    offset=PT30M, time_zone='Australia/Melbourne', cycle='20200101T1200Z'
+    checks for 1230PM on 1 Jan 2020 local Melbourne time.
+    """
+
     if offset is None:
         offset_as_seconds = 0
     else:
         offset_as_seconds = int(interval_parse(offset).get_seconds())
+
+    if time_zone is not None:
+        # Obtain the time zone offset in the format +h:m
+        # Do not use os.environ to adjust the TZ envar and time.tzset() to
+        # obtain the time zone offset because that persists and could lead to
+        # unexpected results
+        tz_offset, _ = procopen(('date', '+%-:z'), env={'TZ': time_zone},
+                                stdoutpipe=True).communicate()
+        hours, mins = tz_offset.split(':')
+        if hours[0] == '-':
+            tz_seconds = int(hours) * 3600 - int(mins) * 60
+        else:
+            tz_seconds = int(hours) * 3600 + int(mins) * 60
+
+        # Subtract the offset from point_as_seconds to adjust the point to be
+        # mimicing the provided time zone
+        point_as_seconds -= tz_seconds
+
     return time() > (point_as_seconds + offset_as_seconds)
