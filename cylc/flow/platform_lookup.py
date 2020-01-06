a459c9313200fb0e15a318b8dcaa293057b539c8
Tim Pillinger <26465611+wxtim@users.noreply.github.com>
2020-01-06 14:23:01 +0000

    updated platform syntax in platform_lookup: hosts -> remote hosts

--- cylc/flow/platform_lookup.py
+++ cylc/flow/platform_lookup.py
@@ -158,7 +158,7 @@ def reverse_lookup(platforms, job, remote):
     #   - In the case of host we also want a regex match to the platform name
     #   - In the case of batch system we want to match the name of the system
     #     to a platform when host is localhost.
-    if 'host' in remote.keys():
+    if 'host' in remote.keys() and remote['host']:
         task_host = remote.pop('host')
     else:
         task_host = 'localhost'
@@ -183,7 +183,6 @@ def reverse_lookup(platforms, job, remote):
         # All items other than batch system and host must be an exact match
         if not generic_items_match:
             continue
-        breakpoint(header=f"{platform_name}")
         # We have some special logic to identify whether task host and task
         # batch system match the platform in question.
         if (
@@ -193,7 +192,7 @@ def reverse_lookup(platforms, job, remote):
             return 'localhost'
 
         elif (
-                'hosts' in platform_spec.keys() and
+                'remote hosts' in platform_spec.keys() and
                 task_host in platform_spec['remote hosts'] and
                 task_batch_system == platform_spec['batch system']
         ):
