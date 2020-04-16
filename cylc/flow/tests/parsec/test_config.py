32d9ac3becd3953a0917d2b6bb80b13a15458b93
Oliver Sanders <oliver.sanders@metoffice.gov.uk>
2020-04-16 13:03:22 +0100

    parsec: upgrade unit tests to pytest

--- cylc/flow/tests/parsec/test_config.py
+++ cylc/flow/tests/parsec/test_config.py
@@ -24,225 +24,226 @@ from cylc.flow.parsec.upgrade import upgrader
 from cylc.flow.parsec.validate import (
     cylc_config_validate, IllegalItemError, CylcConfigValidator as VDR)
 
-SAMPLE_SPEC_1 = {
-    'section1': {
-        'value1': [VDR.V_STRING, ''],
-        'value2': [VDR.V_STRING, 'what?']
-    },
-    'section2': {
-        'enabled': [VDR.V_BOOLEAN, False]
-    },
-    'section3': {
-        'title': [VDR.V_STRING],
-        'entries': {
-            'key': [VDR.V_STRING],
-            'value': [VDR.V_INTEGER_LIST]
+
+@pytest.fixture()
+def sample_spec():
+    return {
+        'section1': {
+            'value1': [VDR.V_STRING, ''],
+            'value2': [VDR.V_STRING, 'what?']
+        },
+        'section2': {
+            'enabled': [VDR.V_BOOLEAN, False]
+        },
+        'section3': {
+            'title': [VDR.V_STRING],
+            'entries': {
+                'key': [VDR.V_STRING],
+                'value': [VDR.V_INTEGER_LIST]
+            }
         }
     }
-}
 
 
-class TestConfig(unittest.TestCase):
+def test_loadcfg(sample_spec):
+    with tempfile.NamedTemporaryFile() as output_file_name:
+        with tempfile.NamedTemporaryFile() as rcfile:
+            parsec_config = config.ParsecConfig(
+               spec=sample_spec,
+                upgrader=None,  # new spec
+                output_fname=output_file_name.name,
+                tvars=None,
+                validator=None  # use default
+            )
+            rcfile.write("""
+            [section1]
+            value1 = 'test'
+            value2 = 'test'
+            [section2]
+            enabled = True
+            [section3]
+            title = 'Ohm'
+            [[entries]]
+            key = 'product'
+            value = 1, 2, 3, 4
+            """.encode())
+            rcfile.seek(0)
+            parsec_config.loadcfg(rcfile.name, "File test_loadcfg")
 
-    def test_loadcfg(self):
-        with tempfile.NamedTemporaryFile() as output_file_name:
-            with tempfile.NamedTemporaryFile() as rcfile:
-                parsec_config = config.ParsecConfig(
-                    spec=SAMPLE_SPEC_1,
-                    upgrader=None,  # new spec
-                    output_fname=output_file_name.name,
-                    tvars=None,
-                    validator=None  # use default
-                )
-                rcfile.write("""
-                [section1]
-                value1 = 'test'
-                value2 = 'test'
-                [section2]
-                enabled = True
-                [section3]
-                title = 'Ohm'
-                [[entries]]
-                key = 'product'
-                value = 1, 2, 3, 4
-                """.encode())
-                rcfile.seek(0)
-                parsec_config.loadcfg(rcfile.name, "File test_loadcfg")
+            sparse = parsec_config.sparse
+            value = sparse['section3']['entries']['value']
+            assert [1, 2, 3, 4] == value
 
-                sparse = parsec_config.sparse
-                value = sparse['section3']['entries']['value']
-                self.assertEqual([1, 2, 3, 4], value)
+            # calling it multiple times should still work
+            parsec_config.loadcfg(rcfile.name, "File test_loadcfg")
+            parsec_config.loadcfg(rcfile.name, "File test_loadcfg")
 
-                # calling it multiple times should still work
-                parsec_config.loadcfg(rcfile.name, "File test_loadcfg")
-                parsec_config.loadcfg(rcfile.name, "File test_loadcfg")
+            sparse = parsec_config.sparse
+            value = sparse['section3']['entries']['value']
+            assert [1, 2, 3, 4] == value
 
-                sparse = parsec_config.sparse
-                value = sparse['section3']['entries']['value']
-                self.assertEqual([1, 2, 3, 4], value)
+def test_loadcfg_with_upgrade(sample_spec):
+    def upg(cfg, description):
+        u = upgrader(cfg, description)
+        u.obsolete('1.0', ['section3', 'entries'])
+        u.upgrade()
 
-    def test_loadcfg_with_upgrade(self):
-        def upg(cfg, description):
-            u = upgrader(cfg, description)
-            u.obsolete('1.0', ['section3', 'entries'])
-            u.upgrade()
+    with tempfile.NamedTemporaryFile() as output_file_name:
+        with tempfile.NamedTemporaryFile() as rcfile:
+            parsec_config = config.ParsecConfig(
+                spec=sample_spec,
+                upgrader=upg,
+                output_fname=output_file_name.name,
+                tvars=None,
+                validator=None  # use default
+            )
+            rcfile.write("""
+            [section1]
+            value1 = 'test'
+            value2 = 'test'
+            [section2]
+            enabled = True
+            [section3]
+            title = 'Ohm'
+            [[entries]]
+            key = 'product'
+            value = 1, 2, 3, 4
+            """.encode())
+            rcfile.seek(0)
+            parsec_config.loadcfg(rcfile.name, "1.1")
 
-        with tempfile.NamedTemporaryFile() as output_file_name:
-            with tempfile.NamedTemporaryFile() as rcfile:
-                parsec_config = config.ParsecConfig(
-                    spec=SAMPLE_SPEC_1,
-                    upgrader=upg,
-                    output_fname=output_file_name.name,
-                    tvars=None,
-                    validator=None  # use default
-                )
-                rcfile.write("""
-                [section1]
-                value1 = 'test'
-                value2 = 'test'
-                [section2]
-                enabled = True
-                [section3]
-                title = 'Ohm'
-                [[entries]]
-                key = 'product'
-                value = 1, 2, 3, 4
-                """.encode())
-                rcfile.seek(0)
-                parsec_config.loadcfg(rcfile.name, "1.1")
+            sparse = parsec_config.sparse
+            # removed by the obsolete upgrade
+            assert 'entries' not in sparse['section3']
 
-                sparse = parsec_config.sparse
-                # removed by the obsolete upgrade
-                self.assertTrue('entries' not in sparse['section3'])
+def test_validate():
+    """
+    An interesting aspect of the ParsecConfig.validate, is that if you
+    have a sparse dict produced by this class, and you call the validate
+    on that dict again, you may have TypeErrors.
 
-    def test_validate(self):
-        """
-        An interesting aspect of the ParsecConfig.validate, is that if you
-        have a sparse dict produced by this class, and you call the validate
-        on that dict again, you may have TypeErrors.
+    That occurs because the values like 'True' are validated against the
+    spec and converted from Strings with quotes, to bool types. So the
+    next type you run the validation if expects Strings...
+    :return:
+    """
 
-        That occurs because the values like 'True' are validated against the
-        spec and converted from Strings with quotes, to bool types. So the
-        next type you run the validation if expects Strings...
-        :return:
-        """
-
-        spec = {
-            'section': {
-                'name': [VDR.V_STRING],
-                'address': [VDR.V_STRING],
-            }
+    spec = {
+        'section': {
+            'name': [VDR.V_STRING],
+            'address': [VDR.V_STRING],
         }
+    }
 
-        parsec_config = config.ParsecConfig(
-            spec=spec,
-            upgrader=None,  # new spec
-            output_fname=None,  # not going to call the loadcfg
-            tvars=None,
-            validator=None  # use default
-        )
+    parsec_config = config.ParsecConfig(
+        spec=spec,
+        upgrader=None,  # new spec
+        output_fname=None,  # not going to call the loadcfg
+        tvars=None,
+        validator=None  # use default
+    )
 
+    sparse = OrderedDictWithDefaults()
+    parsec_config.validate(sparse)  # empty dict is OK
+
+    with pytest.raises(IllegalItemError):
         sparse = OrderedDictWithDefaults()
-        parsec_config.validate(sparse)  # empty dict is OK
+        sparse['name'] = 'True'
+        parsec_config.validate(sparse)  # name is not valid
 
-        with self.assertRaises(IllegalItemError):
-            sparse = OrderedDictWithDefaults()
-            sparse['name'] = 'True'
-            parsec_config.validate(sparse)  # name is not valid
+    sparse = OrderedDictWithDefaults()
+    sparse['section'] = OrderedDictWithDefaults()
+    sparse['section']['name'] = 'Wind'
+    sparse['section']['address'] = 'Corner'
+    parsec_config.validate(sparse)
 
-        sparse = OrderedDictWithDefaults()
-        sparse['section'] = OrderedDictWithDefaults()
-        sparse['section']['name'] = 'Wind'
-        sparse['section']['address'] = 'Corner'
-        parsec_config.validate(sparse)
-
-    def test_expand(self):
-        spec = {
-            'section': {
-                'name': [VDR.V_STRING],
-                'address': [VDR.V_INTEGER_LIST]
-            },
-            'allow_many': {
-                '__MANY__': [VDR.V_STRING, '']
-            }
+def test_expand():
+    spec = {
+        'section': {
+            'name': [VDR.V_STRING],
+            'address': [VDR.V_INTEGER_LIST]
+        },
+        'allow_many': {
+            '__MANY__': [VDR.V_STRING, '']
         }
+    }
 
-        with tempfile.NamedTemporaryFile() as output_file_name:
-            with tempfile.NamedTemporaryFile() as rcfile:
-                parsec_config = config.ParsecConfig(
-                    spec=spec,
-                    upgrader=None,
-                    output_fname=output_file_name.name,
-                    tvars=None,
-                    validator=cylc_config_validate
-                )
-                rcfile.write("""
-                        [section]
-                        name = test
-                        [allow_many]
-                        anything = yup
-                        """.encode())
-                rcfile.seek(0)
-                parsec_config.loadcfg(rcfile.name, "1.0")
+    with tempfile.NamedTemporaryFile() as output_file_name:
+        with tempfile.NamedTemporaryFile() as rcfile:
+            parsec_config = config.ParsecConfig(
+                spec=spec,
+                upgrader=None,
+                output_fname=output_file_name.name,
+                tvars=None,
+                validator=cylc_config_validate
+            )
+            rcfile.write("""
+                    [section]
+                    name = test
+                    [allow_many]
+                    anything = yup
+                    """.encode())
+            rcfile.seek(0)
+            parsec_config.loadcfg(rcfile.name, "1.0")
 
-                parsec_config.expand()
+            parsec_config.expand()
 
-                sparse = parsec_config.sparse
-                self.assertEqual('yup', sparse['allow_many']['anything'])
-                self.assertTrue('__MANY__' not in sparse['allow_many'])
+            sparse = parsec_config.sparse
+            assert 'yup' == sparse['allow_many']['anything']
+            assert '__MANY__' not in sparse['allow_many']
 
-    def test_get_item(self):
-        spec = {
-            'section': {
-                'name': [VDR.V_STRING],
-                'address': [VDR.V_INTEGER_LIST]
-            },
-            'allow_many': {
-                '__MANY__': [VDR.V_STRING, '']
-            }
+def test_get_item():
+    spec = {
+        'section': {
+            'name': [VDR.V_STRING],
+            'address': [VDR.V_INTEGER_LIST]
+        },
+        'allow_many': {
+            '__MANY__': [VDR.V_STRING, '']
         }
+    }
 
-        with tempfile.NamedTemporaryFile() as output_file_name:
-            with tempfile.NamedTemporaryFile() as rcfile:
-                parsec_config = config.ParsecConfig(
-                    spec=spec,
-                    upgrader=None,
-                    output_fname=output_file_name.name,
-                    tvars=None,
-                    validator=cylc_config_validate
-                )
-                rcfile.write("""
-                                [section]
-                                name = test
-                                [allow_many]
-                                anything = yup
-                                """.encode())
-                rcfile.seek(0)
-                parsec_config.loadcfg(rcfile.name, "1.0")
+    with tempfile.NamedTemporaryFile() as output_file_name:
+        with tempfile.NamedTemporaryFile() as rcfile:
+            parsec_config = config.ParsecConfig(
+                spec=spec,
+                upgrader=None,
+                output_fname=output_file_name.name,
+                tvars=None,
+                validator=cylc_config_validate
+            )
+            rcfile.write("""
+                            [section]
+                            name = test
+                            [allow_many]
+                            anything = yup
+                            """.encode())
+            rcfile.seek(0)
+            parsec_config.loadcfg(rcfile.name, "1.0")
 
-                cfg = parsec_config.get(keys=None, sparse=None)
-                self.assertEqual(parsec_config.dense, cfg)
+            cfg = parsec_config.get(keys=None, sparse=None)
+            assert parsec_config.dense == cfg
 
-                cfg = parsec_config.get(keys=None, sparse=True)
-                self.assertEqual(parsec_config.sparse, cfg)
+            cfg = parsec_config.get(keys=None, sparse=True)
+            assert parsec_config.sparse == cfg
 
-                cfg = parsec_config.get(keys=['section'], sparse=True)
-                self.assertEqual(parsec_config.sparse['section'], cfg)
+            cfg = parsec_config.get(keys=['section'], sparse=True)
+            assert parsec_config.sparse['section'] == cfg
 
-                cfg = parsec_config.get(keys=['section', 'name'], sparse=True)
-                self.assertEqual('test', cfg)
+            cfg = parsec_config.get(keys=['section', 'name'], sparse=True)
+            assert 'test' == cfg
 
-                with self.assertRaises(config.ItemNotFoundError):
-                    parsec_config.get(keys=['section', 'a'], sparse=True)
+            with pytest.raises(config.ItemNotFoundError):
+                parsec_config.get(keys=['section', 'a'], sparse=True)
 
-    def test_item_not_found_error(self):
-        error = config.ItemNotFoundError("internal error")
-        self.assertEqual('item not found: internal error', str(error))
+def test_item_not_found_error():
+    error = config.ItemNotFoundError("internal error")
+    assert 'item not found: internal error' == str(error)
 
-    def test_not_single_item_error(self):
-        error = config.NotSingleItemError("internal error")
-        self.assertEqual('not a singular item: internal error', str(error))
+def test_not_single_item_error():
+    error = config.NotSingleItemError("internal error")
+    assert 'not a singular item: internal error' == str(error)
 
 
 if __name__ == '__main__':
-    unittest.main()
+unittest.main()
