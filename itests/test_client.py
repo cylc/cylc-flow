018e3552336340ca49fb6f5d0f01de3eeba6d9d0
Oliver Sanders <oliver.sanders@metoffice.gov.uk>
2020-05-22 12:51:20 +0100

    itests: session, module and function scoping

--- itests/test_client.py
+++ itests/test_client.py
@@ -6,38 +6,33 @@ from cylc.flow.network.server import PB_METHOD_MAP
 
 
 @pytest.mark.asyncio
-async def test_ping(simple_conf, flow, run_flow):
+async def test_ping(flow_a_w_client):
     """It should return True if running."""
-    scheduler = flow(simple_conf)
-    async with run_flow(scheduler):
-        client = SuiteRuntimeClient(scheduler.suite)
-        assert await client.async_request('ping_suite')
-        assert not client.socket.closed
+    scheduler, client = flow_a_w_client
+    assert await client.async_request('ping_suite')
+    assert not client.socket.closed
 
 
 @pytest.mark.asyncio
-async def test_graphql(simple_conf, flow, run_flow):
+async def test_graphql(flow_a_w_client):
     """It should return True if running."""
-    scheduler = flow(simple_conf)
-    async with run_flow(scheduler):
-        client = SuiteRuntimeClient(scheduler.suite)
-        ret = await client.async_request(
-            'graphql',
-            {'request_string': 'query { workflows { id } }'}
-        )
-        workflows = ret['workflows']
-        assert len(workflows) == 1
-        workflow = workflows[0]
-        assert scheduler.suite in workflow['id']
+    scheduler, client = flow_a_w_client
+    ret = await client.async_request(
+        'graphql',
+        {'request_string': 'query { workflows { id } }'}
+    )
+    workflows = ret['workflows']
+    assert len(workflows) == 1
+    workflow = workflows[0]
+    assert scheduler.suite in workflow['id']
 
 
 @pytest.mark.asyncio
-async def test_protobuf(simple_conf, flow, run_flow):
+async def test_protobuf(flow_a_w_client):
     """It should return True if running."""
-    scheduler = flow(simple_conf)
-    async with run_flow(scheduler):
-        client = SuiteRuntimeClient(scheduler.suite)
-        ret = await client.async_request('pb_entire_workflow')
-        pb_data = PB_METHOD_MAP['pb_entire_workflow']()
-        pb_data.ParseFromString(ret)
-        assert scheduler.suite in pb_data.workflow.id
+    scheduler, client = flow_a_w_client
+    client = SuiteRuntimeClient(scheduler.suite)
+    ret = await client.async_request('pb_entire_workflow')
+    pb_data = PB_METHOD_MAP['pb_entire_workflow']()
+    pb_data.ParseFromString(ret)
+    assert scheduler.suite in pb_data.workflow.id
