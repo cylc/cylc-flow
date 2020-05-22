fae0783bedd8162189a3e29512aa0addc96983f0
Oliver Sanders <oliver.sanders@metoffice.gov.uk>
2020-05-22 10:21:18 +0100

    itests: convert SuiteRuntimeClient to itest

--- itests/test_client.py
+++ itests/test_client.py
@@ -1,29 +1,43 @@
+"""Test cylc.flow.client.SuiteRuntimeClient."""
+import pytest
+
+from cylc.flow.network.client import SuiteRuntimeClient
 from cylc.flow.network.server import PB_METHOD_MAP
 
 
-def test_ping(simple_flow):
+@pytest.mark.asyncio
+async def test_ping(simple_conf, flow, run_flow):
     """It should return True if running."""
-    reg, _, client = simple_flow
-    assert client('ping_suite')
+    scheduler = flow(simple_conf)
+    async with run_flow(scheduler):
+        client = SuiteRuntimeClient(scheduler.suite)
+        assert await client.async_request('ping_suite')
+        assert not client.socket.closed
 
 
-def test_graphql(simple_flow):
-    """It should return information about itself."""
-    reg, _, client = simple_flow
-    ret = client(
-        'graphql',
-        {'request_string': 'query { workflows { id } }'}
-    )
-    workflows = ret['workflows']
-    assert len(workflows) == 1
-    workflow = workflows[0]
-    assert reg in workflow['id']
+@pytest.mark.asyncio
+async def test_graphql(simple_conf, flow, run_flow):
+    """It should return True if running."""
+    scheduler = flow(simple_conf)
+    async with run_flow(scheduler):
+        client = SuiteRuntimeClient(scheduler.suite)
+        ret = await client.async_request(
+            'graphql',
+            {'request_string': 'query { workflows { id } }'}
+        )
+        workflows = ret['workflows']
+        assert len(workflows) == 1
+        workflow = workflows[0]
+        assert scheduler.suite in workflow['id']
 
 
-def test_protobuf(simple_flow):
-    """It should return information about itself."""
-    reg, _, client = simple_flow
-    ret = client('pb_entire_workflow')
-    pb_data = PB_METHOD_MAP['pb_entire_workflow']()
-    pb_data.ParseFromString(ret)
-    assert reg in pb_data.workflow.id
+@pytest.mark.asyncio
+async def test_protobuf(simple_conf, flow, run_flow):
+    """It should return True if running."""
+    scheduler = flow(simple_conf)
+    async with run_flow(scheduler):
+        client = SuiteRuntimeClient(scheduler.suite)
+        ret = await client.async_request('pb_entire_workflow')
+        pb_data = PB_METHOD_MAP['pb_entire_workflow']()
+        pb_data.ParseFromString(ret)
+        assert scheduler.suite in pb_data.workflow.id
