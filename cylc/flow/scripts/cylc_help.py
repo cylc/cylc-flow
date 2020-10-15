df4b670fb67fa22e2c5e8d2187e38e25ed131095
Tim Pillinger <26465611+wxtim@users.noreply.github.com>
2020-10-15 18:51:47 +0100

    add copyright notices to files where it was missing.

--- cylc/flow/scripts/cylc_help.py
+++ cylc/flow/scripts/cylc_help.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+# THIS FILE IS PART OF THE CYLC SUITE ENGINE.
 # Copyright (C) NIWA & British Crown (Met Office) & Contributors.
 #
 # This program is free software: you can redistribute it and/or modify
@@ -14,6 +15,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 
 import sys
 
