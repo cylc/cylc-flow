9b640a6de6be01c71c7d53c22b2e0e0810ad617d
John Haiducek <jhaiduce@gmail.com>
2020-11-04 12:43:29 -0500

    Removed unused import

--- cylc/flow/scripts/cylc.py
+++ cylc/flow/scripts/cylc.py
@@ -15,7 +15,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """cylc main entry point"""
-import os
 import sys
 
 import click
