26686ace64dc609a7d5e166ecb23679a90a78063
Hilary James Oliver <hilary.j.oliver@gmail.com>
2020-12-03 12:31:32 +1300

    Document Slurm hetero jobs.

--- cylc/flow/batch_sys_handlers/slurm.py
+++ cylc/flow/batch_sys_handlers/slurm.py
@@ -51,6 +51,39 @@ If :cylc:conf:`execution time limit` is specified, it is used to generate the
 limit known by the suite may be out of sync with what is submitted to the batch
 system.
 
+Cylc supports heterogeneous Slurm jobs via special numbered directive prefixes
+that distinguish repeated directives from one another:
+
+.. code-block:: cylc
+
+   [runtime]
+       # run two heterogenous job components:
+       script = srun sleep 10 : sleep 30
+       [[my_task]]
+           [[[job]]]
+               batch system = slurm
+               execution time limit = PT1H
+           [[[directives]]]
+               --account = QXZ5W2
+               hetjob_0_--mem = 1G  # first prefix must be "0"
+               hetjob_1_--mem = 2G
+
+The resulting formatted directives are:
+
+.. code-block:: bash
+
+   #!/bin/bash
+   #SBATCH --time=60:00
+   #SBATCH --account=QXZ5W2
+   #SBATCH --mem=1G
+   #SBATCH hetjob
+   #SBATCH --mem=2G
+
+.. note::
+
+   For older Slurm versions with *packjob* instead of *hetjob*, use
+   ``batch system = slurm_packjob`` and directive prefixes ``packjob_0_`` etc.
+
 .. cylc-scope::
 
 """
