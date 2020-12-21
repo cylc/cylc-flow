b959d68602cdf865a6950a017bba0229c239eff1
Hilary James Oliver <hilary.j.oliver@gmail.com>
2020-12-22 11:28:12 +1300

    Add missing import.

--- cylc/flow/scripts/jobs_submit.py
+++ cylc/flow/scripts/jobs_submit.py
@@ -24,6 +24,7 @@ On a remote job host, this command reads the job files from STDIN.
 """
 from cylc.flow.option_parsers import CylcOptionParser as COP
 from cylc.flow.terminal import cli_function
+from cylc.flow.job_runner_mgr import JobRunnerManager
 
 INTERNAL = True
 
