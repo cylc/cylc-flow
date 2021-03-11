ba6e5485552a0eec5631aee1c637e35ff574b5cc
Mel Hall <37735232+datamel@users.noreply.github.com>
2021-03-11 09:41:10 +0000

    update get_header logic

--- cylc/flow/network/client.py
+++ cylc/flow/network/client.py
@@ -234,10 +234,11 @@ class SuiteRuntimeClient(ZMQSocketBase):
 
         ssh_connection = os.getenv("SSH_CONNECTION")
         host = socket.gethostname()
-
         if ssh_connection is None:
             comms_method = 'local command'
-        elif socket.gethostbyname(host) == socket.gethostbyname(self.host):
+        elif self.host and (
+                socket.gethostbyname(host) ==
+                socket.gethostbyname(str(self.host))):
             comms_method = CommsMeth.SSH
         else:
             comms_method = CommsMeth.ZMQ
