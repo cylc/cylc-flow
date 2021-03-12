2fd0eca7174df42de1ce39c6cb1078f7afeddc59
Tim Pillinger <26465611+wxtim@users.noreply.github.com>
2021-03-12 16:41:13 +0000

    fix nasty RE.bug

--- cylc/flow/platforms.py
+++ cylc/flow/platforms.py
@@ -187,7 +187,11 @@ def platform_from_name(platform_name=None, platforms=None):
     for platform_name_re in reversed(list(platforms)):
         # We substitue commas with or without spaces to
         # allow lists of platforms
-        if re.fullmatch(re.sub(r',\s*', '|', platform_name_re), platform_name):
+        if (
+            re.fullmatch(re.sub(r',\s*', '|', platform_name_re), platform_name)
+            or
+            re.fullmatch(platform_name_re, platform_name)
+        ):
             # Deepcopy prevents contaminating platforms with data
             # from other platforms matching platform_name_re
             platform_data = deepcopy(platforms[platform_name_re])
