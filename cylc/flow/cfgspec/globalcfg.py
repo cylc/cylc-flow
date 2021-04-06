a2f850185e019e718a4aa2ddee81b6fe4251b270
Oliver Sanders <oliver.sanders@metoffice.gov.uk>
2021-04-06 11:02:56 +0100

    mypy fix

--- cylc/flow/cfgspec/globalcfg.py
+++ cylc/flow/cfgspec/globalcfg.py
@@ -19,6 +19,7 @@ import os
 from typing import List, Optional, Tuple
 
 from pkg_resources import parse_version
+from pkg_resources.extern.packaging.version import Version
 
 from cylc.flow import LOG
 from cylc.flow import __version__ as CYLC_VERSION
@@ -802,7 +803,7 @@ def get_version_hierarchy(version: str) -> List[str]:
         ['', '8', '8.0', '8.0.1', '8.0.1a2', '8.0.1a2.dev']
 
     """
-    smart_ver = parse_version(version)
+    smart_ver: Version = parse_version(version)
     base = [str(i) for i in smart_ver.release]
     hierarchy = ['']
     hierarchy += ['.'.join(base[:i]) for i in range(1, len(base) + 1)]
