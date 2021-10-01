5ef44194a3e28f21944ae894c52f0c523d590f94
Tim Pillinger <26465611+wxtim@users.noreply.github.com>
2021-10-01 11:43:15 +0100

    cylc review show any .cylc file with synatax highlighting

--- lib/cylc/review.py
+++ lib/cylc/review.py
@@ -590,7 +590,7 @@ class CylcReviewService(object):
                         break
         if (
             fnmatch(os.path.basename(path), "suite*.rc*")
-            or fnmatch(os.path.basename(path), "flow.cylc*")
+            or fnmatch(os.path.basename(path), "*.cylc")
         ):
             file_content = "cylc-suite-rc"
         elif fnmatch(os.path.basename(path), "rose*.conf"):
