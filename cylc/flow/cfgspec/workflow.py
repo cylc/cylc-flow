806b63350ddab17a819b21780d50ae132161a6d7
Tim Pillinger <26465611+wxtim@users.noreply.github.com>
2021-10-07 19:24:29 +0100

    Cylc 8 timezone defaults to UTC

--- cylc/flow/cfgspec/workflow.py
+++ cylc/flow/cfgspec/workflow.py
@@ -252,11 +252,17 @@ with Conf(
             represented with this string at the end.
 
             If this isn't set (and :cylc:conf:`flow.cylc[scheduler]UTC mode`
-            is also not set), then it will default to the local time zone at
-            the time of running the workflow. This will persist over local time
-            zone changes (e.g. if the workflow is run during winter time, then
-            stopped, then restarted after summer time has begun, the cycle
-            points will remain in winter time).
+            is also not set), then it will default to:
+
+            - If your workflow is defined in a ``suite.rc`` file (Cylc 7
+              comatibility mode): To the local time zone at the time of running
+              the workflow.
+            - If your workflow is defined in a ``flow.cylc`` file: To "Z" (UTC)
+
+            This will persist over local time zone changes (e.g. if the
+            workflow is run during winter time, then stopped, then restarted
+            after summer time has begun, the cycle points will remain
+            in winter time).
 
             If this isn't set, and UTC mode is set to True, then this will
             default to ``Z``. If you use a custom
