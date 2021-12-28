oliver-sanders/cylc

reg: don't allow a number as the first char in a reg

cylc/flow/unicode_rules.py
@@ -22,6 +22,7 @@ import re
 ENGLISH_REGEX_MAP = {
     r'\w': 'alphanumeric',
     r'a-zA-Z0-9': 'latin letters and numbers',
+    r'\d': 'numbers',
     r'\-': '``-``',
     r'\.': '``.``',
     r'\/': '``/``'
@@ -187,7 +188,7 @@ class SuiteNameValidator(UnicodeRuleChecker):
 
     RULES = [
         length(1, 254),
-        not_starts_with(r'\.', r'\-'),
+        not_starts_with(r'\.', r'\-', r'\d'),
         allowed_characters(r'\w', r'\/', '_', '+', r'\-', r'\.', '@')
     ]
 

cylc/cylc-flow

cylc/flow/unicode_rules.py
@@ -26,6 +26,7 @@ from cylc.flow.task_id import (
 ENGLISH_REGEX_MAP = {
     r'\w': 'alphanumeric',
     r'a-zA-Z0-9': 'latin letters and numbers',
+    r'\d': 'numbers',
     r'\-': '``-``',
     r'\.': '``.``',
     r'\/': '``/``'
@@ -269,8 +270,8 @@ class WorkflowNameValidator(UnicodeRuleChecker):
 
     RULES = [
         length(1, 254),
-        not_starts_with_char(r'\.', r'\-'),
-        allowed_characters(r'\w', r'\/', '_', '+', r'\-', r'\.', '@')
+        not_starts_with_char(r'\.', r'\-', r'\d'),
+        allowed_characters(r'\w', r'\/', '_', '+', r'\-', r'\.', '@'),
     ]
 
 