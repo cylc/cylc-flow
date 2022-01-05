dbe893a3d8506c4a8bb06e2017b70e2470b6f7d8
Ronnie Dutta <61982285+MetRonnie@users.noreply.github.com>
2022-01-05 19:31:36 +0000

    Fix host selection bug
    Avoid selecting localhost if host specified using deprecated `[runtime][<task>][remote]host` can't be reached

--- cylc/flow/task_job_mgr.py
+++ cylc/flow/task_job_mgr.py
@@ -288,17 +288,18 @@ class TaskJobManager:
 
                     # Get another platform, if task config platform is a group
                     use_next_platform_in_group = False
-                    try:
-                        platform = get_platform(
-                            itask.tdef.rtconfig['platform'],
-                            bad_hosts=self.bad_hosts
-                        )
-                    except NoPlatformsError:
-                        pass
-                    else:
-                        # If were able to select a new platform;
-                        if platform and platform != itask.platform:
-                            use_next_platform_in_group = True
+                    if itask.tdef.rtconfig['platform']:
+                        try:
+                            platform = get_platform(
+                                itask.tdef.rtconfig['platform'],
+                                bad_hosts=self.bad_hosts
+                            )
+                        except NoPlatformsError:
+                            pass
+                        else:
+                            # If were able to select a new platform;
+                            if platform and platform != itask.platform:
+                                use_next_platform_in_group = True
 
                     if use_next_platform_in_group:
                         # store the previous platform's hosts so that when
