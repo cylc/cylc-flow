c339913cedd44ac7f915c4594529d3eac78dd8a2
Oliver Sanders <oliver.sanders@metoffice.gov.uk>
2022-02-10 17:44:19 +0000

    main_loop.log_main_loop: fix dev plugin
    * Plugin had become broken with changes to the way timings are recorded.

--- tests/unit/main_loop/test_log_main_loop.py
+++ tests/unit/main_loop/test_log_main_loop.py
@@ -20,7 +20,6 @@ from pathlib import Path
 import pytest
 
 from cylc.flow.main_loop.log_main_loop import (
-    _transpose,
     _normalise,
     _dump,
     _plot
@@ -30,38 +29,23 @@ from cylc.flow.main_loop.log_main_loop import (
 @pytest.fixture()
 def test_data():
     return {
-        'foo': {
-            'timings': deque([(1, 'a'), (2, 'b'), (3, 'c')]),
-            'some other var': None
-        },
-        'bar': {
-            'timings': deque()
-        },
-        'baz': {
-            'timings': deque([(4, 'd')])
-        }
-    }
-
-
-def test_transpose(test_data):
-    """Ensure we can orient the data around main-loop-time/plugin-time."""
-    assert _transpose(test_data) == {
-        'baz': ((4,), ('d',)),
-        'foo': ((1, 2, 3), ('a', 'b', 'c'))
+        # (plugin_name, coro_name): deque([(start_time, duration), ...])
+        ('foo', 'bar'): deque([(2, 1), (3, 2), (4, 3)]),
+        ('baz', 'pub'): deque([(1, 4), (2, 5), (3, 6)]),
     }
 
 
 def test_normalise(test_data):
     """Ensure we correctly normalise the timings against the earliest time."""
-    assert _normalise(_transpose(test_data)) == {
-        'baz': ((3,), ('d',)),
-        'foo': ((0, 1, 2), ('a', 'b', 'c'))
+    assert _normalise(test_data) == {
+        'foo': [(1, 1), (2, 2), (3, 3)],
+        'baz': [(0, 4), (1, 5), (2, 6)],
     }
 
 
 def test_dump(test_data, tmp_path):
     """Ensure the data is serialiseable."""
-    assert _dump(_transpose(test_data), tmp_path)
+    assert _dump(_normalise(test_data), tmp_path)
     assert list(tmp_path.iterdir()) == [
         Path(tmp_path, 'cylc.flow.main_loop.log_main_loop.json')
     ]
@@ -69,7 +53,7 @@ def test_dump(test_data, tmp_path):
 
 def test_plot(test_data, tmp_path):
     """Ensure the plotting mechanism doesn't raise errors."""
-    assert _plot(_transpose(test_data), tmp_path)
+    assert _plot(_normalise(test_data), tmp_path)
     assert list(tmp_path.iterdir()) == [
         Path(tmp_path, 'cylc.flow.main_loop.log_main_loop.pdf')
     ]
