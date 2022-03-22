67178a9e1519bea1d66e80b1f75d0e91058ac1eb
Melanie Hall <37735232+datamel@users.noreply.github.com>
2022-03-22 14:18:36 +0000

    Rsync configurable (#4736)
    * Configurable rsync for file installation
    
    Test config rsync
    
    * Update Documentation
    
    * Fix remote slow file install test
    
    * Review Feedback
    
    Co-authored-by: Oliver Sanders <oliver.sanders@metoffice.gov.uk>
    
    Co-authored-by: Oliver Sanders <oliver.sanders@metoffice.gov.uk>

--- cylc/flow/cfgspec/globalcfg.py
+++ cylc/flow/cfgspec/globalcfg.py
@@ -1037,6 +1037,13 @@ with Conf('global.cylc', desc='''
                 with some initial options or a command that implements a
                 similar interface to ``ssh``.
             ''')
+            Conf('rsync command',
+                 VDR.V_STRING,
+                 'rsync',
+                 desc='''
+                Command used for remote file installation. This supports POSIX
+                compliant rsync implementation e.g. GNU or BSD.
+            ''')
             Conf('use login shell', VDR.V_BOOLEAN, True, desc='''
                 Whether to use a login shell or not for remote command
                 invocation.
