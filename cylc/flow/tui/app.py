cylc/cylc-flow

commit e246b3c346dec954a78ef0b4217331301c529bed

Make TUI refuse to show non-installed workflows (#4748)
* make TUI give useful error message when trying to show non installed workflows
Co-authored-by: Ronnie Dutta <61982285+MetRonnie@users.noreply.github.com>

cylc/flow/tui/app.py
@@ -21,6 +21,7 @@ import sys
 import urwid
 from urwid import html_fragment
 from urwid.wimp import SelectableIcon
+from pathlib import Path
 
 from cylc.flow.network.client_factory import get_client
 from cylc.flow.exceptions import (
@@ -28,6 +29,7 @@ from cylc.flow.exceptions import (
     ClientTimeout,
     WorkflowStopped
 )
+from cylc.flow.pathutil import get_workflow_run_dir
 from cylc.flow.task_state import (
     TASK_STATUSES_ORDERED,
     TASK_STATUS_SUBMITTED,
@@ -56,6 +58,7 @@ from cylc.flow.tui.util import (
     get_workflow_status_str,
     render_node
 )
+from cylc.flow.workflow_files import WorkflowFiles
 
 
 urwid.set_encoding('utf8')  # required for unicode task icons
@@ -310,11 +313,23 @@ class TuiApp:
                 }
             )
         except WorkflowStopped:
-            self.client = None
+            # Distinguish stopped flow from non-existent flow.
+            full_path = Path(get_workflow_run_dir(self.reg))
+            if(
+                (full_path / WorkflowFiles.SUITE_RC).is_file()
+                or (full_path / WorkflowFiles.FLOW_FILE).is_file()
+            ):
+                message = "stopped"
+            else:
+                message = (
+                    f"No {WorkflowFiles.SUITE_RC} or {WorkflowFiles.FLOW_FILE}"
+                    f"found in {self.reg}."
+                )
+
             return dummy_flow({
                 'name': self.reg,
                 'id': self.reg,
-                'status': 'stopped',
+                'status': message,
                 'stateTotals': {}
             })
         except (ClientError, ClientTimeout) as exc:

commit c0e4e4e6887cd395b02d9bfa805c3162da78c9cb

tui: fix traceback/exit for stopped workflows
* Closes #4818
* Reverts regression in #4748

cylc/flow/tui/app.py
@@ -314,6 +314,7 @@ class TuiApp:
             )
         except WorkflowStopped:
             # Distinguish stopped flow from non-existent flow.
+            self.client = None
             full_path = Path(get_workflow_run_dir(self.reg))
             if(
                 (full_path / WorkflowFiles.SUITE_RC).is_file()