63a321c2a3b0da654bbccee53dc938271e9ef61b
Hilary Oliver <hilary.j.oliver@gmail.com>
2022-04-20 17:40:51 +1200

    Extend scan unit tests.

--- tests/unit/scripts/test_scan.py
+++ tests/unit/scripts/test_scan.py
@@ -14,11 +14,16 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import sys
+from pathlib import Path
+
 from cylc.flow.scripts.scan import (
     ScanOptions,
     get_pipe,
     _format_plain,
-    FLOW_STATES
+    FLOW_STATES,
+    format_and_write,
+    BAD_CONTACT_FILE_MSG
 )
 
 
@@ -32,3 +37,43 @@ def test_ping_connection():
     """Ensure scan always connects to the flow when requested via --ping."""
     pipe = get_pipe(ScanOptions(states=FLOW_STATES, ping=True), _format_plain)
     assert 'graphql_query' in repr(pipe)
+
+
+def test_good_contact_info(capsys, monkeypatch):
+    """Check correct printing of workflow contact information."""
+    port = 8888
+    host = "wizard"
+    name = "blargh/run1"
+    format_and_write(
+        sys.stdout.write,
+        _format_plain,
+        {
+            "name": name,
+            "contact": Path("/path/to/blargh/run1"),
+            "CYLC_WORKFLOW_HOST": host,
+            "CYLC_WORKFLOW_PORT": port,
+        },
+        None
+    )
+    outerr = capsys.readouterr()
+    assert name in outerr.out
+    assert f"{host}:{port}" in outerr.out
+
+
+def test_bad_contact_info(capsys, monkeypatch):
+    """Check correct printing of bad contact info.
+
+    Missing contact keys should result in a warning, not crash scan.
+    """
+    name = "blargh/run1"
+    format_and_write(
+        sys.stdout.write,
+        _format_plain,
+        {
+            "name": name,
+            "contact": Path("/path/to/blargh/run1"),
+        },
+        None
+    )
+    outerr = capsys.readouterr()
+    assert BAD_CONTACT_FILE_MSG.format(workflow_name=name) in outerr.err
