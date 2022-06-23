ab5f7627b852dcee54a56b82062d5ed4185f0ac3
Oliver Sanders <oliver.sanders@metoffice.gov.uk>
2022-06-23 15:54:23 +0100

    reinstall: require workflow ID argument
    * Closes #4928
    * With `cylc install` we can omit the source name argument,
      it will look for a workflow in `$PWD`.
    * However, `cylc reinstall` works with installed workflow IDs
      *not* source names.
    * `cylc reinstall` currently supports omitting the workflow ID,
      something which other commands do not support. This has caused issues
      when symlink dirs are involved (#4928).
    * Suggest requiring the workflow ID argument. If we develop the
      capability to omit the workflow ID argument for other commands we
      can add it to `reinstall` when we do so, however, it may require more
      thought to implement.

--- cylc/flow/scripts/reinstall.py
+++ cylc/flow/scripts/reinstall.py
@@ -32,10 +32,6 @@ Examples:
   $ cylc install myflow --no-run-name
   # To reinstall this workflow run:
   $ cylc reinstall myflow
-
-  # To reinstall a workflow from within the cylc-run directory of a previously
-  # installed workflow:
-  $ cylc reinstall
 """
 
 from pathlib import Path
@@ -48,7 +44,7 @@ from cylc.flow.option_parsers import (
     WORKFLOW_ID_ARG_DOC,
     CylcOptionParser as COP,
 )
-from cylc.flow.pathutil import get_cylc_run_dir, get_workflow_run_dir
+from cylc.flow.pathutil import get_workflow_run_dir
 from cylc.flow.workflow_files import (
     get_workflow_source_dir,
     reinstall_workflow,
@@ -61,7 +57,7 @@ if TYPE_CHECKING:
 
 def get_option_parser() -> COP:
     parser = COP(
-        __doc__, comms=True, argdoc=[COP.optional(WORKFLOW_ID_ARG_DOC)]
+        __doc__, comms=True, argdoc=[WORKFLOW_ID_ARG_DOC]
     )
 
     parser.add_cylc_rose_options()
@@ -90,20 +86,10 @@ def main(
 ) -> None:
     run_dir: Optional[Path]
     workflow_id: str
-    if args is None:
-        try:
-            workflow_id = str(Path.cwd().relative_to(
-                Path(get_cylc_run_dir()).resolve()
-            ))
-        except ValueError:
-            raise WorkflowFilesError(
-                "The current working directory is not a workflow run directory"
-            )
-    else:
-        workflow_id, *_ = parse_id(
-            args,
-            constraint='workflows',
-        )
+    workflow_id, *_ = parse_id(
+        args,
+        constraint='workflows',
+    )
     run_dir = Path(get_workflow_run_dir(workflow_id))
     if not run_dir.is_dir():
         raise WorkflowFilesError(
