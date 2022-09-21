1d4f271f98d3780305b773946b3199b8f0b73b47
Oliver Sanders <oliver.sanders@metoffice.gov.uk>
2022-09-21 10:26:34 +0100

    host select: allow unary operators in ranking expressions
    * Whitelist unary operators (required for expressions like `-1 * x`)
      in `[scheduler][run hosts]ranking` expressions.
    * Improve error formatting.

--- tests/unit/test_exceptions.py
+++ tests/unit/test_exceptions.py
@@ -14,9 +14,67 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from cylc.flow.exceptions import CylcError
+from textwrap import dedent
+
+from cylc.flow.exceptions import (
+    CylcError,
+    HostSelectException,
+)
 
 
 def test_cylc_error_str():
     error = CylcError("abcd")
     assert str(error) == "abcd"
+
+
+def test_host_select_exception():
+    """Test exception used for host selection failures.
+
+    * Could not connect to hosts (e.g. SSH failure).
+    * Commands failed (e.g. configuration error).
+    * Could not obtain metrics (e.g. host ranking expression error).
+    * No available hosts (e.g. no hosts met ranking thresholds).
+
+    """
+    # it should format the selection results nicely
+    assert str(HostSelectException({
+        'ranking': 'virtual_memory().available > 1',
+        'host-1': {
+            'returncode': 1
+        },
+        'host-2': {
+            'returncode': 1
+        },
+    })) == dedent('''
+        Could not select host from:
+            host-1:
+                returncode: 1
+            host-2:
+                returncode: 1
+    ''').strip()
+
+    # it should give a useful hint for exit code "2"
+    # (error in the selection ranking expression)
+    assert 'This is likely an error in the ranking expression' in (
+        str(HostSelectException({
+            'ranking': 'virtual_memory().available > 1',
+            'host-1': {
+                'returncode': 2
+            },
+            'host-2': {
+                'returncode': 2
+            },
+        })))
+
+    # it should give a useful hint for the exit code "255"
+    # (ssh error)
+    assert 'Cylc could not establish SSH connection to the run hosts.' in (
+        str(HostSelectException({
+            'ranking': 'virtual_memory().available > 1',
+            'host-1': {
+                'returncode': 255
+            },
+            'host-2': {
+                'returncode': 255
+            },
+        })))
