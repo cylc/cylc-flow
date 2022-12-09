61642468df2eeaeffa05ed641d1f6f5f244ae486
Hilary James Oliver <hilary.j.oliver@gmail.com>
2022-12-09 15:33:34 +1300

    Document final try fail behaviour.

--- cylc/flow/cfgspec/workflow.py
+++ cylc/flow/cfgspec/workflow.py
@@ -1090,16 +1090,17 @@ with Conf(
                 )
             )
             Conf('execution retry delays', VDR.V_INTERVAL_LIST, None, desc=f'''
-                Cylc can automate resubmission of a failed job.
+                Cylc can automate resubmission of failed jobs.
 
-                Execution retry delays are a list of ISO 8601
-                durations/intervals which tell Cylc how long to wait before
-                resubmitting a failed job.
+                Execution retry delays are a list of ISO 8601 durations which
+                tell Cylc how long to wait before resubmitting a failed job.
 
-                Each time Cylc resubmits a job it will increment the
-                variable ``$CYLC_TASK_TRY_NUMBER`` in the task execution
-                environment. ``$CYLC_TASK_TRY_NUMBER`` allows you to vary task
-                behaviour between submission attempts.
+                The job environment variable ``$CYLC_TASK_TRY_NUMBER``
+                increments with each try, allowing you to vary task behaviour
+                between submission attempts.
+
+                Tasks only go to the ``failed`` state if their jobs fail with
+                no retries left.
 
                 .. versionchanged:: 8.0.0
 
