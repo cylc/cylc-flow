51dcc0510b2b5bdc782f841502231c1a9424d004
Tim Pillinger <26465611+wxtim@users.noreply.github.com>
2023-05-12 14:14:14 +0100

    add message on how to use --upgrade

--- cylc/flow/scheduler_cli.py
+++ cylc/flow/scheduler_cli.py
@@ -523,6 +523,7 @@ def _version_check(
                     process=str.lower,
                 )
             # we are in non-interactive mode, abort abort abort
+            print(cparse('Use "--upgrade" to upgrade the workflow.'))
             return False
         elif itt > 2 and this > that:
             # restart would INCREASE the Cylc version in a little way
