63966cc9e5bc466ddbcb029bc8fe1419c09a7c51
Ronnie Dutta <61982285+MetRonnie@users.noreply.github.com>
2023-10-20 12:17:30 +0100

    xfail tests in response to bug discovery
    https://github.com/cylc/cylc-flow/pull/5777#issuecomment-1772521629

--- tests/unit/cycling/test_iso8601.py
+++ tests/unit/cycling/test_iso8601.py
@@ -14,8 +14,10 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import pytest
 from datetime import datetime
+
+import pytest
+from pytest import param
 
 from cylc.flow.cycling.iso8601 import (
     ISO8601Interval,
@@ -683,7 +685,7 @@ def test_simple(set_cycling_type):
         ('next(--0325)', '20110325T0000Z'),
         ('next(---10)', '20100810T0000Z'),
         ('next(---05T1200Z)', '20100905T1200Z'),
-        ('next(--08-08)', '20100808T0000Z'),
+        param('next(--08-08)', '20110808T0000Z', marks=pytest.mark.xfail),
         ('next(T15)', '20100809T1500Z'),
         ('next(T-41)', '20100808T1541Z'),
     ]
@@ -707,7 +709,7 @@ def test_next_simple(value: str, expected: str, set_cycling_type):
         ('previous(--0325)', '20100325T0000Z'),
         ('previous(---10)', '20100710T0000Z'),
         ('previous(---05T1200Z)', '20100805T1200Z'),
-        ('previous(--08-08)', '20090808T0000Z'),
+        param('previous(--08-08)', '20100808T0000Z', marks=pytest.mark.xfail),
         ('previous(T15)', '20100808T1500Z'),
         ('previous(T-41)', '20100808T1441Z'),
     ]
