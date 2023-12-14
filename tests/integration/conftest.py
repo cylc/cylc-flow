458686f7eb8925fa2861b93a514e390f327ef984
Tim Pillinger <26465611+wxtim@users.noreply.github.com>
2023-12-14 14:38:32 +0000

    fix

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -479,7 +479,7 @@ def install(test_dir, run_dir):
 
 
 @pytest.fixture
-def reflog(monkeypatch):
+def reflog():
     """Integration test version of the --reflog CLI option.
 
     This returns a set which captures task triggers.
@@ -512,7 +512,6 @@ def reflog(monkeypatch):
     """
 
     def _reflog(schd, flow_nums=False):
-        nonlocal monkeypatch
 
         submit_task_jobs = schd.task_job_mgr.submit_task_jobs
         triggers = set()
