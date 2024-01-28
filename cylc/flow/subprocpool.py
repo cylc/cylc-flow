wxtim/cylc

Xtrigger function arg validation.

cylc/flow/subprocpool.py
@@ -64,33 +64,38 @@ def _killpg(proc, signal):
     return True
 
 
-def get_func(func_name, src_dir):
-    """Find and return an xtrigger function from a module of the same name.
+def get_func(mod_name, func_name, src_dir):
+    """Find and return a name function from a named module.
 
-    Can be in <src_dir>/lib/python, CYLC_MOD_LOC, or in Python path.
+    Can be in <src_dir>/lib/python, cylc.flow.xtriggers, or in Python path.
+
     Workflow source directory passed in as this is executed in an independent
     process in the command pool and therefore doesn't know about the workflow.
 
+    Raises:
+        ImportError, if the module is not found
+        AtributeError, if the function is not found in the module
+
     """
     if func_name in _XTRIG_FUNCS:
+        # Found and cached already.
         return _XTRIG_FUNCS[func_name]
-    # First look in <src-dir>/lib/python.
+
+    # 1. look in <src-dir>/lib/python.
     sys.path.insert(0, os.path.join(src_dir, 'lib', 'python'))
-    mod_name = func_name
     try:
         mod_by_name = __import__(mod_name, fromlist=[mod_name])
     except ImportError:
-        # Then look in built-in xtriggers.
-        mod_name = "%s.%s" % ("cylc.flow.xtriggers", func_name)
+        # 2. look in built-in xtriggers.
+        mod_name = "%s.%s" % ("cylc.flow.xtriggers", mod_name)
         try:
             mod_by_name = __import__(mod_name, fromlist=[mod_name])
         except ImportError:
             raise
-    try:
-        _XTRIG_FUNCS[func_name] = getattr(mod_by_name, func_name)
-    except AttributeError:
-        # Module func_name has no function func_name.
-        raise
+
+    # Module found and imported, return the named function.
+
+    _XTRIG_FUNCS[func_name] = getattr(mod_by_name, func_name)
     return _XTRIG_FUNCS[func_name]
 
 
@@ -98,6 +103,8 @@ def run_function(func_name, json_args, json_kwargs, src_dir):
     """Run a Python function in the process pool.
 
     func_name(*func_args, **func_kwargs)
+
+    The function is presumed to be in a module of the same name.
 
     Redirect any function stdout to stderr (and workflow log in debug mode).
     Return value printed to stdout as a JSON string - allows use of the
@@ -107,7 +114,7 @@ def run_function(func_name, json_args, json_kwargs, src_dir):
     func_args = json.loads(json_args)
     func_kwargs = json.loads(json_kwargs)
     # Find and import then function.
-    func = get_func(func_name, src_dir)
+    func = get_func(func_name, func_name, src_dir)
     # Redirect stdout to stderr.
     orig_stdout = sys.stdout
     sys.stdout = sys.stderr

cylc/flow/xtriggers/wall_clock.py
@@ -17,6 +17,8 @@
 """xtrigger function to trigger off of a wall clock time."""
 
 from time import time
+from cylc.flow.cycling.iso8601 import interval_parse
+from cylc.flow.exceptions import WorkflowConfigError
 
 
 def wall_clock(trigger_time=None):
@@ -27,3 +29,33 @@ def wall_clock(trigger_time=None):
             Trigger time as seconds since Unix epoch.
     """
     return time() > trigger_time
+
+
+def validate_config(f_args, f_kwargs, f_signature):
+    """Validate and manipulate args parsed from the workflow config.
+
+    wall_clock()  # zero offset
+    wall_clock(PT1H)
+    wall_clock(offset=PT1H)
+
+    And offset must be a valid ISO 8601 interval.
+
+    If f_args used, convert to f_kwargs for clarity.
+
+    """
+
+    n_args = len(f_args)
+    n_kwargs = len(f_kwargs)
+
+    if n_args + n_kwargs > 1:
+        raise WorkflowConfigError(f"xtrigger: too many args: {f_signature}")
+
+    if n_args:
+        f_kwargs["offset"] = f_args[0]
+    elif not n_kwargs:
+        f_kwargs["offset"] = "P0Y"
+
+    try:
+        interval_parse(f_kwargs["offset"])
+    except ValueError:
+        raise WorkflowConfigError(f"xtrigger: invalid offset: {f_signature}")

cylc/cylc-flow

cylc/flow/subprocpool.py
@@ -66,8 +66,10 @@ def _killpg(proc, signal):
     return True
 
 
-def get_func(func_name, src_dir):
-    """Find and return an xtrigger function from a module of the same name.
+def get_func(mod_name, func_name, src_dir):
+    """Find and return a named function from a named module.
+
+    Can be in <src_dir>/lib/python, cylc.flow.xtriggers, or in Python path.
 
     These locations are checked in this order:
     - <src_dir>/lib/python/
@@ -78,13 +80,17 @@ def get_func(func_name, src_dir):
     Workflow source directory passed in as this is executed in an independent
     process in the command pool and therefore doesn't know about the workflow.
 
+    Raises:
+        ImportError, if the module is not found
+        AttributeError, if the function is not found in the module
+
     """
-    if func_name in _XTRIG_FUNCS:
-        return _XTRIG_FUNCS[func_name]
+    if (mod_name, func_name) in _XTRIG_FUNCS:
+        # Found and cached already.
+        return _XTRIG_FUNCS[(mod_name, func_name)]
 
     # First look in <src-dir>/lib/python.
     sys.path.insert(0, os.path.join(src_dir, 'lib', 'python'))
-    mod_name = func_name
     try:
         mod_by_name = __import__(mod_name, fromlist=[mod_name])
     except ImportError:
@@ -101,17 +107,19 @@ def get_func(func_name, src_dir):
         raise
 
     try:
-        _XTRIG_FUNCS[func_name] = getattr(mod_by_name, func_name)
+        _XTRIG_FUNCS[(mod_name, func_name)] = getattr(mod_by_name, func_name)
     except AttributeError:
         # Module func_name has no function func_name, nor an entry_point entry.
         raise
-    return _XTRIG_FUNCS[func_name]
+    return _XTRIG_FUNCS[(mod_name, func_name)]
 
 
 def run_function(func_name, json_args, json_kwargs, src_dir):
     """Run a Python function in the process pool.
 
     func_name(*func_args, **func_kwargs)
+
+    The function is presumed to be in a module of the same name.
 
     Redirect any function stdout to stderr (and workflow log in debug mode).
     Return value printed to stdout as a JSON string - allows use of the
@@ -121,7 +129,7 @@ def run_function(func_name, json_args, json_kwargs, src_dir):
     func_args = json.loads(json_args)
     func_kwargs = json.loads(json_kwargs)
     # Find and import then function.
-    func = get_func(func_name, src_dir)
+    func = get_func(func_name, func_name, src_dir)
     # Redirect stdout to stderr.
     orig_stdout = sys.stdout
     sys.stdout = sys.stderr

cylc/flow/xtriggers/wall_clock.py
@@ -17,6 +17,8 @@
 """xtrigger function to trigger off of a wall clock time."""
 
 from time import time
+from cylc.flow.cycling.iso8601 import interval_parse
+from cylc.flow.exceptions import WorkflowConfigError
 
 
 def wall_clock(trigger_time=None):
@@ -27,3 +29,32 @@ def wall_clock(trigger_time=None):
             Trigger time as seconds since Unix epoch.
     """
     return time() > trigger_time
+
+
+def validate_config(f_args, f_kwargs, f_signature):
+    """Validate and manipulate args parsed from the workflow config.
+
+    wall_clock()  # zero offset
+    wall_clock(PT1H)
+    wall_clock(offset=PT1H)
+
+    The offset must be a valid ISO 8601 interval.
+
+    If f_args used, convert to f_kwargs for clarity.
+
+    """
+    n_args = len(f_args)
+    n_kwargs = len(f_kwargs)
+
+    if n_args + n_kwargs > 1:
+        raise WorkflowConfigError(f"xtrigger: too many args: {f_signature}")
+
+    if n_args:
+        f_kwargs["offset"] = f_args[0]
+    elif not n_kwargs:
+        f_kwargs["offset"] = "P0Y"
+
+    try:
+        interval_parse(f_kwargs["offset"])
+    except ValueError:
+        raise WorkflowConfigError(f"xtrigger: invalid offset: {f_signature}")