e6e5ee83d9cb1da85f0986826a29ffb116644733
Hilary James Oliver <hilary.j.oliver@gmail.com>
2024-03-21 20:11:04 +1300

    Fix workflow-state alt run-dir.

--- cylc/flow/id_cli.py
+++ cylc/flow/id_cli.py
@@ -198,6 +198,7 @@ async def parse_ids_async(
     constraint: str = 'tasks',
     max_workflows: Optional[int] = None,
     max_tasks: Optional[int] = None,
+    alt_run_dir: Optional[str] = None,
 ) -> Tuple[Dict[str, List[Tokens]], Any]:
     """Parse IDs from the command line.
 
@@ -294,7 +295,8 @@ async def parse_ids_async(
 
     # infer the run number if not specified the ID (and if possible)
     if infer_latest_runs:
-        _infer_latest_runs(tokens_list, src_path=src_path)
+        _infer_latest_runs(
+            tokens_list, src_path=src_path, alt_run_dir=alt_run_dir)
 
     _validate_number(
         *tokens_list,
@@ -409,13 +411,14 @@ def _validate_workflow_ids(*tokens_list, src_path):
         detect_both_flow_and_suite(src_path)
 
 
-def _infer_latest_runs(tokens_list, src_path):
+def _infer_latest_runs(tokens_list, src_path, alt_run_dir=None):
     for ind, tokens in enumerate(tokens_list):
         if ind == 0 and src_path:
             # source workflow passed in as a path
             continue
         tokens_list[ind] = tokens.duplicate(
-            workflow=infer_latest_run_from_id(tokens['workflow'])
+            workflow=infer_latest_run_from_id(
+                tokens['workflow'], alt_run_dir)
         )
         pass
 
