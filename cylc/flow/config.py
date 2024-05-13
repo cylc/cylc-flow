d62428dfaa03a419fdf2bdb9fe241183094d78b1
Tim Pillinger <26465611+wxtim@users.noreply.github.com>
2024-05-13 08:12:59 +0100

    revert a mistaken "improvement"

--- cylc/flow/config.py
+++ cylc/flow/config.py
@@ -880,7 +880,7 @@ class WorkflowConfig:
             )
         # Allow implicit tasks in back-compat mode unless rose-suite.conf
         # present (to maintain compat with Rose 2019)
-        elif not (self.fdir / "rose-suite.conf").is_file():
+        elif not (self.fpath.parent / "rose-suite.conf").is_file():
             LOG.debug(msg)
             return
 
