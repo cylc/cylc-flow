379de8f997842d2548244b7e7bd0c02e7fb4e752
Oliver Sanders <oliver.sanders@metoffice.gov.uk>
2024-05-14 12:44:00 +0100

    Apply suggestions from code review

--- tests/conftest.py
+++ tests/conftest.py
@@ -25,13 +25,14 @@ from cylc.flow.cfgspec.glbl_cfg import glbl_cfg
 from cylc.flow.cfgspec.globalcfg import SPEC
 from cylc.flow.parsec.config import ParsecConfig
 from cylc.flow.parsec.validate import cylc_config_validate
+from cylc.flow import flags
 
 
 @pytest.fixture(autouse=True)
-def test_reset(monkeypatch):
+def test_reset():
     """Reset global state before all tests."""
-    monkeypatch.setattr('cylc.flow.flags.verbosity', 0)
-    monkeypatch.setattr('cylc.flow.flags.cylc7_back_compat', False)
+    flags.verbosity = 0
+    flags.cylc7_back_compat = False
 
 
 @pytest.fixture(scope='module')
