6c497085af6156f44bb5b1167718977ca119f426
Oliver Sanders <oliver.sanders@metoffice.gov.uk>
2024-10-15 13:30:38 +0100

    broadcast: reject truncated cycle points
    * Closes https://github.com/cylc/cylc-flow/issues/6407

--- cylc/flow/cycling/loader.py
+++ cylc/flow/cycling/loader.py
@@ -158,13 +158,13 @@ def standardise_point_string(
 
 
 def standardise_point_string(
-    point_string: Optional[str], cycling_type: Optional[str] = None
+    point_string: Optional[str], cycling_type: Optional[str] = None,
 ) -> Optional[str]:
     """Return a standardised version of point_string."""
     if point_string is None:
         return None
     point = get_point(point_string, cycling_type=cycling_type)
     if point is not None:
-        point.standardise()
+        point.standardise(allow_truncated=False)
         point_string = str(point)
     return point_string
