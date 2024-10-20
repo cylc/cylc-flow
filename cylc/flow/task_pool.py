957be706cb3622aeb04f6b97debac2ff1000c43b
Hilary James Oliver <hilary.j.oliver@gmail.com>
2024-10-21 12:15:12 +1300

    Don't trigger n=0 tasks with flow=none.

--- cylc/flow/task_pool.py
+++ cylc/flow/task_pool.py
@@ -1927,6 +1927,12 @@ class TaskPool:
 
         # Set existing task proxies.
         for itask in itasks:
+            if flow == ['none'] and itask.flow_nums != set():
+                LOG.warning(
+                    f"[{itask}] ignoring 'flow=none' set: task already has"
+                    f" {stringify_flow_nums(itask.flow_nums, full=True)}"
+                )
+                continue
             self.merge_flows(itask, flow_nums)
             if prereqs:
                 self._set_prereqs_itask(itask, prereqs, flow_nums)
@@ -2169,6 +2175,12 @@ class TaskPool:
 
         # Trigger active tasks.
         for itask in existing_tasks:
+            if flow == ['none'] and itask.flow_nums != set():
+                LOG.warning(
+                    f"[{itask}] ignoring 'flow=none' trigger: task already has"
+                    f" {stringify_flow_nums(itask.flow_nums, full=True)}"
+                )
+                continue
             if itask.state(TASK_STATUS_PREPARING, *TASK_STATUSES_ACTIVE):
                 LOG.warning(f"[{itask}] ignoring trigger - already active")
                 continue
