0cc5b9ef0cfd53ff10e6a54ddc209ce6530a458e
Tim Pillinger <26465611+wxtim@users.noreply.github.com>
2024-11-12 11:00:48 +0000

    fix merge

--- cylc/flow/scripts/validate.py
+++ cylc/flow/scripts/validate.py
@@ -57,9 +57,7 @@ from cylc.flow.task_proxy import TaskProxy
 from cylc.flow.templatevars import get_template_vars
 from cylc.flow.terminal import cli_function
 from cylc.flow.run_modes import RunMode
-from cylc.flow.scheduler_cli import RUN_MODE
 from cylc.flow.workflow_files import get_workflow_run_dir
-from cylc.flow.workflow_status import RunMode
 
 if TYPE_CHECKING:
     from cylc.flow.option_parsers import Values
