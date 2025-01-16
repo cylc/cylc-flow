02a025e1c563559273e8699411e288f57eff4c2f
Ronnie Dutta <61982285+MetRonnie@users.noreply.github.com>
2025-01-16 15:02:57 +0000

    Replace functional test with integration test

--- tests/integration/test_kill.py
+++ tests/integration/test_kill.py
@@ -14,12 +14,58 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+
+import asyncio
 import logging
+
+from async_timeout import timeout as async_timeout
 import pytest
 
-from cylc.flow.commands import kill_tasks, run_cmd
+from cylc.flow.commands import (
+    kill_tasks,
+    run_cmd,
+)
 from cylc.flow.scheduler import Scheduler
-from cylc.flow.task_state import TASK_STATUS_SUBMIT_FAILED, TASK_STATUS_WAITING
+from cylc.flow.task_proxy import TaskProxy
+from cylc.flow.task_state import (
+    TASK_STATUS_FAILED,
+    TASK_STATUS_PREPARING,
+    TASK_STATUS_RUNNING,
+    TASK_STATUS_SUBMIT_FAILED,
+)
+
+
+async def task_state(itask: TaskProxy, state: str, timeout=4, **kwargs):
+    """Await task state."""
+    async with async_timeout(timeout):
+        while not itask.state(state, **kwargs):
+            await asyncio.sleep(0.1)
+
+
+async def test_simulation(flow, scheduler, run):
+    """Test killing a running task in simulation mode."""
+    conf = {
+        'scheduling': {
+            'graph': {
+                'R1': 'foo',
+            },
+        },
+        'runtime': {
+            'root': {
+                'simulation': {
+                    'default run length': 'PT30S',
+                },
+            },
+        },
+    }
+    schd: Scheduler = scheduler(flow(conf), paused_start=False)
+    async with run(schd):
+        itask = schd.pool.get_tasks()[0]
+        await task_state(itask, TASK_STATUS_RUNNING)
+
+        await run_cmd(kill_tasks(schd, [itask.identity]))
+        await task_state(itask, TASK_STATUS_FAILED, is_held=True)
+        assert schd.check_workflow_stalled()
 
 
 async def test_kill_preparing(
