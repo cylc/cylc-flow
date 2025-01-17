475962bdda46efa67fc62bc73ba63b09bf986026
Ronnie Dutta <61982285+MetRonnie@users.noreply.github.com>
2025-01-17 15:51:29 +0000

    Add test

--- tests/integration/test_kill.py
+++ tests/integration/test_kill.py
@@ -17,6 +17,8 @@
 
 import asyncio
 import logging
+from secrets import token_hex
+from unittest.mock import Mock
 
 from async_timeout import timeout as async_timeout
 import pytest
@@ -27,6 +29,12 @@ from cylc.flow.commands import (
 )
 from cylc.flow.scheduler import Scheduler
 from cylc.flow.task_proxy import TaskProxy
+from cylc.flow.task_remote_mgr import (
+    REMOTE_FILE_INSTALL_DONE,
+    REMOTE_FILE_INSTALL_IN_PROGRESS,
+    REMOTE_INIT_DONE,
+    REMOTE_INIT_IN_PROGRESS,
+)
 from cylc.flow.task_state import (
     TASK_STATUS_FAILED,
     TASK_STATUS_PREPARING,
@@ -35,11 +43,19 @@ from cylc.flow.task_state import (
 )
 
 
+LOCALHOST = 'localhost'
+
+
 async def task_state(itask: TaskProxy, state: str, timeout=4, **kwargs):
     """Await task state."""
     async with async_timeout(timeout):
         while not itask.state(state, **kwargs):
             await asyncio.sleep(0.1)
+
+
+def patch_remote_init(schd: Scheduler, value: str):
+    """Set remote init state."""
+    schd.task_job_mgr.task_remote_mgr.remote_init_map[LOCALHOST] = value
 
 
 async def test_simulation(flow, scheduler, run):
@@ -69,26 +85,83 @@ async def test_simulation(flow, scheduler, run):
 
 
 async def test_kill_preparing(
-    one_conf,
-    flow,
-    scheduler,
-    start,
-    monkeypatch: pytest.MonkeyPatch,
-    log_filter,
+    flow, scheduler, run, monkeypatch: pytest.MonkeyPatch, log_filter
 ):
     """Test killing a preparing task."""
     schd: Scheduler = scheduler(
-        flow(one_conf), run_mode='live', paused_start=False
+        flow('foo'), run_mode='live', paused_start=False
     )
-    async with start(schd):
+    async with run(schd):
         # Make the task indefinitely preparing:
         monkeypatch.setattr(
-            schd.task_job_mgr, '_prep_submit_task_job', lambda *a, **k: None
+            schd.task_job_mgr, '_prep_submit_task_job', Mock(return_value=None)
         )
         itask = schd.pool.get_tasks()[0]
-        assert itask.state(TASK_STATUS_WAITING, is_held=False)
-        schd.start_job_submission([itask])
+        await task_state(itask, TASK_STATUS_PREPARING, is_held=False)
 
-        await run_cmd(kill_tasks(schd, [itask.tokens.relative_id]))
-        assert itask.state(TASK_STATUS_SUBMIT_FAILED, is_held=True)
+        await run_cmd(kill_tasks(schd, [itask.identity]))
+        await task_state(itask, TASK_STATUS_SUBMIT_FAILED, is_held=True)
         assert log_filter(logging.ERROR, 'killed in job prep')
+
+
+async def test_kill_preparing_pipeline(
+    flow, scheduler, run, monkeypatch: pytest.MonkeyPatch
+):
+    """Test killing a preparing task through various stages of the preparing
+    pipeline that involve submitting subprocesses and waiting for them to
+    complete."""
+    # Make localhost look like a remote target so we can test
+    # remote init/file install stages:
+    monkeypatch.setattr(
+        'cylc.flow.task_job_mgr.get_localhost_install_target',
+        Mock(return_value=token_hex()),
+    )
+
+    schd: Scheduler = scheduler(
+        flow('one'), run_mode='live', paused_start=False
+    )
+    async with run(schd):
+        remote_mgr = schd.task_job_mgr.task_remote_mgr
+        mock_eval_platform = Mock(return_value=None)
+        monkeypatch.setattr(remote_mgr, 'eval_platform', mock_eval_platform)
+        mock_remote_init = Mock()
+        monkeypatch.setattr(remote_mgr, 'remote_init', mock_remote_init)
+        mock_file_install = Mock()
+        monkeypatch.setattr(remote_mgr, 'file_install', mock_file_install)
+        itask = schd.pool.get_tasks()[0]
+
+        # Platform eval:
+        await task_state(itask, TASK_STATUS_PREPARING)
+        assert schd.release_tasks_to_run() is False
+        await run_cmd(kill_tasks(schd, [itask.identity]))
+        await task_state(itask, TASK_STATUS_SUBMIT_FAILED)
+        # Set to finished:
+        mock_eval_platform.return_value = LOCALHOST
+        # Should not submit after finish because it was killed:
+        assert schd.release_tasks_to_run() is False
+
+        # Remote init:
+        patch_remote_init(schd, REMOTE_INIT_IN_PROGRESS)
+        schd.pool._force_trigger(itask)
+        await task_state(itask, TASK_STATUS_PREPARING)
+        assert schd.release_tasks_to_run() is False
+        await run_cmd(kill_tasks(schd, [itask.identity]))
+        await task_state(itask, TASK_STATUS_SUBMIT_FAILED)
+        # Set to finished:
+        patch_remote_init(schd, REMOTE_INIT_DONE)
+        # Should not submit after finish because it was killed:
+        assert schd.release_tasks_to_run() is False
+        assert not mock_remote_init.called
+
+        # Remote file install:
+        patch_remote_init(schd, REMOTE_FILE_INSTALL_IN_PROGRESS)
+        schd.pool._force_trigger(itask)
+        await task_state(itask, TASK_STATUS_PREPARING)
+        assert schd.release_tasks_to_run() is False
+        await run_cmd(kill_tasks(schd, [itask.identity]))
+        await task_state(itask, TASK_STATUS_SUBMIT_FAILED)
+        # Set to finished:
+        patch_remote_init(schd, REMOTE_FILE_INSTALL_DONE)
+        # Should not submit after finish because it was killed:
+        assert schd.release_tasks_to_run() is False
+        assert not mock_file_install.called
