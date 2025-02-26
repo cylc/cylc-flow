37aa99a5467d6319b40838b9dbbca9db6a463897
Oliver Sanders <oliver.sanders@metoffice.gov.uk>
2025-02-26 13:24:41 +0000

    runahead: handle no sequences within start-stop cycle interval
    * Closes #6154
    * Fix a traceback which can occur in some niche circumstances where
      there are no sequences left within the
      `start cycle point` -> `stop cycle point` window after a restart.
    * This can happen as the result of a graph change.

--- cylc/flow/task_pool.py
+++ cylc/flow/task_pool.py
@@ -408,8 +408,11 @@ class TaskPool:
             self._prev_runahead_base_point = base_point
 
         if count_cycles:
-            # (len(list) may be less than ilimit due to sequence end)
-            limit_point = sorted(sequence_points)[:ilimit + 1][-1]
+            if not sequence_points:
+                limit_point = base_point
+            else:
+                # (len(list) may be less than ilimit due to sequence end)
+                limit_point = sorted(sequence_points)[:ilimit + 1][-1]
         else:
             limit_point = max(sequence_points)
 
