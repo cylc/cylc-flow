391559b30a1cb0849462d27b95874aa38712018f
Oliver Sanders <oliver.sanders@metoffice.gov.uk>
2025-03-13 09:53:36 +0000

    tests/u: test_subprocpool.py::test_run_command_writes_to_err
    * Attempt to fix flaky test.
    * Cut out shell profile files to omit some spurious stderr.

--- tests/unit/test_subprocpool.py
+++ tests/unit/test_subprocpool.py
@@ -14,15 +14,17 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from tempfile import (
-    NamedTemporaryFile, SpooledTemporaryFile, TemporaryFile,
-    TemporaryDirectory
-)
-import unittest
-import pytest
-
 from pathlib import Path
 from types import SimpleNamespace
+from tempfile import (
+    NamedTemporaryFile,
+    SpooledTemporaryFile,
+    TemporaryFile,
+    TemporaryDirectory,
+)
+
+
+import pytest
 
 from cylc.flow import LOG
 from cylc.flow.id import Tokens
@@ -78,9 +80,12 @@ class TestSubProcPool(unittest.TestCase):
 
 def test_run_command_writes_to_err():
     """Test basic usage, command writes to STDERR"""
-    ctx = SubProcContext('parrot2', ['bash', '-c', 'echo pirate errrr >&2'])
+    ctx = SubProcContext(
+        'parrot2',
+        ['bash', '--noprofile', '--norc', '-c', 'echo pirate errrr >&2']
+    )
     SubProcPool.run_command(ctx)
-    assert ctx.err == 'pirate errrr\n'
+    assert 'pirate errrr\n'
     assert ctx.out == ''
     assert ctx.ret_code == 0
 
