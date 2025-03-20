f3ee55c5319b127abd98554c4ef328106e501402
Oliver Sanders <oliver.sanders@metoffice.gov.uk>
2025-03-20 10:23:46 +0000

    tui: add Cylc version into app header

--- cylc/flow/tui/app.py
+++ cylc/flow/tui/app.py
@@ -25,6 +25,7 @@ import urwid
 from urwid.canvas import CanvasCache
 from urwid.widget import SelectableIcon
 
+from cylc.flow import __version__ as CYLC_VERSION
 from cylc.flow.id import Tokens
 from cylc.flow.task_state import (
     TASK_STATUS_SUBMITTED,
@@ -435,7 +436,8 @@ class TuiApp:
             ('Y', 'y'),
             ('G', 'l'),
             ('B', 'c'),
-            ('title', ' Tui')
+            ('title', ' Tui '),
+            ('diminished', CYLC_VERSION),
         ]
         if self.filters['tasks'] != DEFAULT_FILTERS['tasks']:
             # if task filters are active, display short help
