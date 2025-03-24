d1fe35b6cd766a41c1bb63cf0f5e3547d239c6d7
christopher.bennett <christopher.bennett@metoffice.gov.uk>
2025-03-24 15:22:24 +0000

    Fail gracefully if cgroups cannot be found

--- cylc/flow/etc/job.sh
+++ cylc/flow/etc/job.sh
@@ -208,10 +208,10 @@ cylc__set_return() {
 ###############################################################################
 # Save the data using cylc message and exit the profiler
 cylc__kill_profiler() {
-    if [[ -n "${cpu_time}" ]]; then
+    if [[ -n "${cpu_time:-}" ]]; then
       cylc message -- "${CYLC_WORKFLOW_ID}" "${CYLC_TASK_JOB}" "DEBUG: cpu_time $cpu_time" || true
     fi
-    if [[ -n "${max_rss}" ]]; then
+    if [[ -n "${max_rss:-}" ]]; then
       cylc message -- "${CYLC_WORKFLOW_ID}" "${CYLC_TASK_JOB}" "DEBUG: max_rss $max_rss" || true
     fi
     if [[ -n ${profiler_pid:-} ]]; then
