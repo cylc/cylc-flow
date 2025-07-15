162cfc9bd2b3cc42cc9cbfaf5aa28940391b6c43
Ronnie Dutta <61982285+MetRonnie@users.noreply.github.com>
2025-07-15 11:28:53 +0100

    `cylc set` should not update job states in the DB/data store

--- cylc/flow/task_job_mgr.py
+++ cylc/flow/task_job_mgr.py
@@ -350,10 +350,7 @@ class TaskJobManager:
                     self.task_remote_mgr.remote_init(platform)
                     for itask in itasks:
                         self.data_store_mgr.delta_job_msg(
-                            itask.tokens.duplicate(
-                                job=str(itask.submit_num)
-                            ),
-                            self.REMOTE_INIT_MSG,
+                            itask.job_tokens, self.REMOTE_INIT_MSG
                         )
                     continue
 
@@ -367,8 +364,7 @@ class TaskJobManager:
                     for itask in itasks:
                         msg = self.IN_PROGRESS[ri_map[install_target]]
                         self.data_store_mgr.delta_job_msg(
-                            itask.tokens.duplicate(job=str(itask.submit_num)),
-                            msg
+                            itask.job_tokens, msg
                         )
                     continue
                 elif ri_map[install_target] == REMOTE_INIT_255:
@@ -378,10 +374,7 @@ class TaskJobManager:
                     self.task_remote_mgr.remote_init(platform)
                     for itask in itasks:
                         self.data_store_mgr.delta_job_msg(
-                            itask.tokens.duplicate(
-                                job=str(itask.submit_num)
-                            ),
-                            self.REMOTE_INIT_MSG
+                            itask.job_tokens, self.REMOTE_INIT_MSG
                         )
                     continue
 
@@ -401,10 +394,7 @@ class TaskJobManager:
                 self.task_remote_mgr.remote_init(platform)
                 for itask in itasks:
                     self.data_store_mgr.delta_job_msg(
-                        itask.tokens.duplicate(
-                            job=str(itask.submit_num)
-                        ),
-                        self.REMOTE_INIT_MSG,
+                        itask.job_tokens, self.REMOTE_INIT_MSG
                     )
                 continue
 
@@ -433,10 +423,7 @@ class TaskJobManager:
                 self.task_remote_mgr.file_install(platform)
                 for itask in itasks:
                     self.data_store_mgr.delta_job_msg(
-                        itask.tokens.duplicate(
-                            job=str(itask.submit_num)
-                        ),
-                        REMOTE_FILE_INSTALL_IN_PROGRESS
+                        itask.job_tokens, REMOTE_FILE_INSTALL_IN_PROGRESS
                     )
                 continue
 
@@ -534,9 +521,7 @@ class TaskJobManager:
                             )
                         )
                     job_log_dirs.append(
-                        itask.tokens.duplicate(
-                            job=str(itask.submit_num),
-                        ).relative_id
+                        itask.job_tokens.relative_id
                     )
                     # The job file is now (about to be) used: reset the file
                     # write flag so that subsequent manual retrigger will
@@ -756,12 +741,7 @@ class TaskJobManager:
             log_msg = (
                 'ignoring job kill result, unexpected task state: %s' %
                 itask.state.status)
-        self.data_store_mgr.delta_job_msg(
-            itask.tokens.duplicate(
-                job=str(itask.submit_num)
-            ),
-            log_msg
-        )
+        self.data_store_mgr.delta_job_msg(itask.job_tokens, log_msg)
         LOG.log(log_lvl, f"[{itask}] {log_msg}")
 
     def _manip_task_jobs_callback(
@@ -865,17 +845,12 @@ class TaskJobManager:
         ctx.out = line
         ctx.ret_code = 0
         # See cylc.flow.job_runner_mgr.JobPollContext
-        job_tokens = itask.tokens.duplicate(job=str(itask.submit_num))
         try:
             job_log_dir, context = line.split('|')[1:3]
             items = json.loads(context)
             jp_ctx = JobPollContext(job_log_dir, **items)
-        except TypeError:
-            self.data_store_mgr.delta_job_msg(job_tokens, self.POLL_FAIL)
-            ctx.cmd = cmd_ctx.cmd  # print original command on failure
-            return
-        except ValueError:
-            self.data_store_mgr.delta_job_msg(job_tokens, self.POLL_FAIL)
+        except (TypeError, ValueError):
+            self.data_store_mgr.delta_job_msg(itask.job_tokens, self.POLL_FAIL)
             ctx.cmd = cmd_ctx.cmd  # print original command on failure
             return
         finally:
@@ -892,7 +867,7 @@ class TaskJobManager:
         if jp_ctx.run_signal == JOB_FILES_REMOVED_MESSAGE:
             LOG.error(
                 f"platform: {itask.platform['name']} - job log directory "
-                f"{job_tokens.relative_id} no longer exists"
+                f"{itask.job_tokens.relative_id} no longer exists"
             )
 
         if jp_ctx.run_status == 1 and jp_ctx.run_signal in ["ERR", "EXIT"]:
@@ -1018,11 +993,7 @@ class TaskJobManager:
                     ctx = SubProcContext(cmd_key, cmd, host=host)
 
             for itask in sorted(itasks, key=lambda task: task.identity):
-                job_log_dirs.append(
-                    itask.tokens.duplicate(
-                        job=str(itask.submit_num)
-                    ).relative_id
-                )
+                job_log_dirs.append(itask.job_tokens.relative_id)
             cmd += job_log_dirs
             LOG.debug(f'{cmd_key} for {platform["name"]} on {host}')
             self.proc_pool.put_command(
@@ -1377,7 +1348,7 @@ class TaskJobManager:
 
         # Location of job file, etc
         self._create_job_log_path(itask)
-        job_d = itask.tokens.duplicate(job=str(itask.submit_num)).relative_id
+        job_d = itask.job_tokens.relative_id
         job_file_path = get_remote_workflow_run_job_dir(
             self.workflow, job_d, JOB_LOG_JOB
         )
