3c32069f8e916335d8b366d173397420197b267b
Oliver Sanders <oliver.sanders@metoffice.gov.uk>
2025-10-21 11:53:34 +0100

    profiler: remove global variables

--- cylc/flow/scripts/profiler.py
+++ cylc/flow/scripts/profiler.py
@@ -20,57 +20,24 @@ Profiler which periodically polls cgroups to track
 the resource usage of jobs running on the node.
 """
 
+import asyncio
+from dataclasses import dataclass
+from functools import partial
 import os
+from pathlib import Path
 import re
+import signal
 import sys
 import time
-import signal
-import asyncio
-from pathlib import Path
-from dataclasses import dataclass
-from cylc.flow.terminal import cli_function
+
 from cylc.flow.network.client_factory import get_client
 from cylc.flow.option_parsers import CylcOptionParser as COP
+from cylc.flow.terminal import cli_function
+
 
 INTERNAL = True
 PID_REGEX = re.compile(r"([^:]*\d{6,}.*)")
 RE_INT = re.compile(r'\d+')
-max_rss_location = None
-cpu_time_location = None
-memory_allocated_path = None
-cgroup_version = None
-comms_timeout = None
-
-
-def get_option_parser() -> COP:
-    parser = COP(
-        __doc__,
-        comms=True,
-        argdoc=[
-        ],
-    )
-    parser.add_option(
-        "-i", type=int,
-        help="interval between query cycles in seconds", dest="delay")
-    parser.add_option(
-        "-m", type=str, help="Location of cgroups directory",
-        dest="cgroup_location")
-
-    return parser
-
-
-@cli_function(get_option_parser)
-def main(parser: COP, options) -> None:
-    """CLI main."""
-    global comms_timeout
-    # Register the stop_profiler function with the signal library
-    signal.signal(signal.SIGINT, stop_profiler)
-    signal.signal(signal.SIGHUP, stop_profiler)
-    signal.signal(signal.SIGTERM, stop_profiler)
-
-    comms_timeout = options.comms_timeout
-
-    get_config(options)
 
 
 @dataclass
@@ -79,25 +46,26 @@ class Process:
     cgroup_memory_path: str
     cgroup_cpu_path: str
     memory_allocated_path: str
+    cgroup_version: int
 
 
-def stop_profiler(*args):
+def stop_profiler(process, comms_timeout, *args):
     """This function will be executed when the SIGINT signal is sent
      to this process"""
     # If a task fails instantly, or finishes very quickly (< 1 second),
     # the get config function doesn't have time to run
-    if (max_rss_location is None
-            or cpu_time_location is None
-            or cgroup_version is None):
+    if (process.max_rss_location is None
+            or process.cpu_time_location is None
+            or process.cgroup_version is None):
         max_rss = 0
         cpu_time = 0
         memory_allocated = 0
     else:
-        max_rss = parse_memory_file(max_rss_location, cgroup_version)
-        cpu_time = parse_cpu_file(cpu_time_location, cgroup_version)
-        memory_allocated = parse_memory_allocated(memory_allocated_path, cgroup_version)
+        max_rss = parse_memory_file(process)
+        cpu_time = parse_cpu_file(process)
+        memory_allocated = parse_memory_allocated(process)
 
-    GRAPHQL_MUTATION = """
+    graphql_mutation = """
     mutation($WORKFLOWS: [WorkflowID]!,
         $MESSAGES: [[String]], $JOB: String!, $TIME: String) {
             message(workflows: $WORKFLOWS, messages:$MESSAGES,
@@ -107,7 +75,7 @@ def stop_profiler(*args):
     }
     """
 
-    GRAPHQL_REQUEST_VARIABLES = {
+    graphql_request_variables = {
         "WORKFLOWS": [os.environ.get('CYLC_WORKFLOW_ID')],
         "MESSAGES": [["DEBUG", f"cpu_time {cpu_time} max_rss {max_rss} mem_alloc {memory_allocated}"]],
         "JOB": os.environ.get('CYLC_TASK_JOB'),
@@ -120,35 +88,33 @@ def stop_profiler(*args):
     async def send_cylc_message():
         await pclient.async_request(
             'graphql',
-            {'request_string': GRAPHQL_MUTATION,
-             'variables': GRAPHQL_REQUEST_VARIABLES},
+            {'request_string': graphql_mutation,
+             'variables': graphql_request_variables},
         )
 
     asyncio.run(send_cylc_message())
     sys.exit(0)
 
 
-def parse_memory_file(cgroup_memory_path, cgroup_version):
+def parse_memory_file(process: Process):
     """Open the memory stat file and copy the appropriate data"""
+    cgroup_memory_path = Path(process.cgroup_memory_path)
 
-    cgroup_memory_path = Path(cgroup_memory_path)
-
-    if cgroup_version == 2:
+    if process.cgroup_version == 2:
         with open(cgroup_memory_path, 'r') as f:
             for line in f:
                 if "anon" in line:
                     return int(''.join(filter(str.isdigit, line)))
     else:
-        with open(cgroup_memory_path, 'r') as f:
+        with open(process.cgroup_memory_path, 'r') as f:
             for line in f:
                 return int(line)
 
 
-def parse_memory_allocated(cgroup_memory_path, cgroup_version) -> int:
+def parse_memory_allocated(process: Process) -> int:
     """Open the memory stat file and copy the appropriate data"""
-
-    if cgroup_version == 2:
-        cgroup_memory_path = Path(cgroup_memory_path)
+    if process.cgroup_version == 2:
+        cgroup_memory_path = Path(process.cgroup_memory_path)
 
         for i in range(5):
             with open(cgroup_memory_path / "memory.max", 'r') as f:
@@ -158,23 +124,22 @@ def parse_memory_allocated(cgroup_memory_path, cgroup_version) -> int:
             cgroup_memory_path = cgroup_memory_path.parent
             if i == 5:
                 break
-    elif cgroup_version == 1:
+    elif process.cgroup_version == 1:
         return 0  # Memory limit not tracked for cgroups v1
 
     raise FileNotFoundError("Could not find memory.max file")
 
 
-def parse_cpu_file(cgroup_cpu_path, cgroup_version) -> int:
+def parse_cpu_file(process: Process) -> int:
     """Open the memory stat file and return the appropriate data"""
-
-    if cgroup_version == 2:
-        with open(cgroup_cpu_path, 'r') as f:
+    if process.cgroup_version == 2:
+        with open(process.cgroup_cpu_path, 'r') as f:
             for line in f:
                 if "usage_usec" in line:
                     return int(RE_INT.findall(line)[0]) // 1000
             raise ValueError("Unable to find cpu usage data")
     else:
-        with open(cgroup_cpu_path, 'r') as f:
+        with open(process.cgroup_cpu_path, 'r') as f:
             for line in f:
                 # Cgroups v2 uses nanoseconds
                 return int(line) // 1000000
@@ -182,13 +147,10 @@ def parse_cpu_file(cgroup_cpu_path, cgroup_version) -> int:
 
 
 def get_cgroup_version(cgroup_location: str, cgroup_name: str) -> int:
-    global cgroup_version
     if Path.exists(Path(cgroup_location + cgroup_name)):
-        cgroup_version = 2
-        return cgroup_version
+        return 2
     elif Path.exists(Path(cgroup_location + "/memory" + cgroup_name)):
-        cgroup_version = 1
-        return cgroup_version
+        return 1
     else:
         raise FileNotFoundError("Cgroup not found at " +
                                 cgroup_location + cgroup_name)
@@ -217,39 +179,33 @@ def get_cgroup_name() -> str:
         raise AttributeError("No cgroup found for process:", pid) from err
 
 
-def get_cgroup_paths(version, location, name) -> Process:
-    global max_rss_location
-    global cpu_time_location
-    global memory_allocated_path
-
-    if version == 2:
-        max_rss_location = location + name + "/" + "memory.stat"
-        cpu_time_location = location + name + "/" + "cpu.stat"
-        memory_allocated_path = location + name
+def get_cgroup_paths(location) -> Process:
+    cgroup_name = get_cgroup_name()
+    cgroup_version = get_cgroup_version(location, cgroup_name)
+    if cgroup_version == 2:
         return Process(
             cgroup_memory_path=location +
-                               name + "/" + "memory.stat",
+                               cgroup_name + "/" + "memory.stat",
             cgroup_cpu_path=location +
-                            name + "/" + "cpu.stat",
-            memory_allocated_path=location + name)
+                            cgroup_name + "/" + "cpu.stat",
+            memory_allocated_path=location + cgroup_name,
+            cgroup_version=cgroup_version,
+        )
 
-    elif version == 1:
-        max_rss_location = (location + "/memory" +
-                            name + "/memory.max_usage_in_bytes")
-        cpu_time_location = (location + "/cpu" +
-                             name + "/cpuacct.usage")
-        memory_allocated_path = location + name + "/" + "memory.limit_in_bytes"
+    elif cgroup_version == 1:
         return Process(
             cgroup_memory_path=location + "/memory" +
-                               name + "/memory.max_usage_in_bytes",
+                               cgroup_name + "/memory.max_usage_in_bytes",
             cgroup_cpu_path=location + "/cpu" +
-                            name + "/cpuacct.usage",
-            memory_allocated_path="")
+                            cgroup_name + "/cpuacct.usage",
+            memory_allocated_path="",
+            cgroup_version=cgroup_version,
+        )
 
     raise ValueError("Unable to determine cgroup version")
 
 
-def profile(process, version, delay, keep_looping=lambda: True):
+def profile(_process: Process, delay, keep_looping=lambda: True):
     # The infinite loop that will constantly poll the cgroup
     # The lambda function is used to allow the loop to be stopped in unit tests
 
@@ -259,18 +215,43 @@ def profile(process, version, delay, keep_looping=lambda: True):
         time.sleep(delay)
 
 
-def get_config(args):
-    # Find the cgroup that this process is running in.
-    # Cylc will put this profiler in the same cgroup
-    # as the job it is profiling
-    cgroup_name = get_cgroup_name()
-    cgroup_version = get_cgroup_version(args.cgroup_location, cgroup_name)
-    process = get_cgroup_paths(cgroup_version,
-                               args.cgroup_location,
-                               cgroup_name)
-    profile(process, cgroup_version, args.delay)
+def get_option_parser() -> COP:
+    parser = COP(
+        __doc__,
+        comms=True,
+        argdoc=[
+        ],
+    )
+    parser.add_option(
+        "-i", type=int,
+        help="interval between query cycles in seconds", dest="delay")
+    parser.add_option(
+        "-m", type=str, help="Location of cgroups directory",
+        dest="cgroup_location")
+
+    return parser
+
+
+@cli_function(get_option_parser)
+def main(_parser: COP, options) -> None:
+    """CLI main."""
+    _main(options)
+
+
+def _main(options) -> None:
+    # get cgroup information
+    process = get_cgroup_paths(options.cgroup_location)
+
+    # Register the stop_profiler function with the signal library
+    _stop_profiler = partial(stop_profiler, process, options.comms_timeout)
+    signal.signal(signal.SIGINT, _stop_profiler)
+    signal.signal(signal.SIGHUP, _stop_profiler)
+    signal.signal(signal.SIGTERM, _stop_profiler)
+
+    # run profiler run
+    profile(process, options.delay)
 
 
 if __name__ == "__main__":
     arg_parser = get_option_parser()
-    get_config(arg_parser.parse_args([]))
+    _main(arg_parser.parse_args([]))
