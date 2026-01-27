00567d3b944de1228b744dee30804efba1d56825
Tim Pillinger <26465611+wxtim@users.noreply.github.com>
2026-01-27 13:19:50 +0000

    Update tests/integration/test_data_store_mgr.py

--- tests/integration/test_data_store_mgr.py
+++ tests/integration/test_data_store_mgr.py
@@ -14,7 +14,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import asyncio
 import logging
 from logging import INFO
 from typing import (
