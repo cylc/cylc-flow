710944605dd9313b4ff536cc7034bf763cee9893
Ronnie Dutta <61982285+MetRonnie@users.noreply.github.com>
2026-06-15 11:40:48 +0100

    Remove unnecessary duplicate xtrigger logging (#7326)

--- tests/unit/test_xtrigger_mgr.py
+++ tests/unit/test_xtrigger_mgr.py
@@ -26,7 +26,6 @@ from cylc.flow.task_proxy import TaskProxy
 from cylc.flow.taskdef import TaskDef
 from cylc.flow.xtrigger_mgr import (
     RE_STR_TMPL,
-    XTRIG_DUP_WARNING,
     XtriggerCollator
 )
 
@@ -320,43 +319,3 @@ def test_callback(xtrigger_mgr):
     xtrigger_mgr.callback(get_name)
     # this means that the xtrigger was satisfied
     assert xtrigger_mgr.sat_xtrig
-
-
-def test_report_duplicates(
-    caplog: pytest.LogCaptureFixture
-):
-    x1 = SubFuncContext(
-        label="x1",
-        func_name="echo",
-        func_args=[],
-        func_kwargs={"succeed": False}
-    )
-    x2 = SubFuncContext(
-        label="x2",
-        func_name="echo",
-        func_args=[],
-        func_kwargs={"succeed": False}
-    )
-    y = SubFuncContext(
-        label="y",
-        func_name="echo",
-        func_args=["arg1"],
-        func_kwargs={"succeed": False}
-    )
-
-    # x1 and x2 both have the same function signature.
-
-    xtriggers = XtriggerCollator()
-    xtriggers.functx_map = {
-        'x1': x1,
-        'x2': x2,
-        'y': y
-    }
-
-    caplog.set_level(logging.INFO, CYLC_LOG)
-    xtriggers.report_duplicates()
-
-    assert caplog.messages == [
-        "Duplicate xtriggers: x1, x2 = echo(succeed=False)",
-        XTRIG_DUP_WARNING
-    ]
