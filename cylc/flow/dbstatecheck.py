720bb4fd9954d214237a3f20d3ea9a50e5cb8bbc
James Frost <james.frost@metoffice.gov.uk>
2026-09-25 17:46:08 +0100

    Remove Cylc 7 back compat code from dbstatecheck
    Backwards compatibility mode is removed at Cylc 8.7, which is the next
    release.

--- cylc/flow/dbstatecheck.py
+++ cylc/flow/dbstatecheck.py
@@ -56,13 +56,10 @@ class CylcWorkflowDBChecker:
     """Object for querying task status or outputs from a workflow database.
 
     Back-compat and task outputs:
-        # Cylc 7 stored {trigger: message} for custom outputs only.
-        1|foo|{"x": "the quick brown"}
-
         # Cylc 8 (pre-8.3.0) stored [message] only, for all outputs.
         1|foo|[1]|["submitted", "started", "succeeded", "the quick brown"]
 
-        # Cylc 8 (8.3.0+) stores {trigger: message} for all ouputs.
+        # Cylc 8 (8.3.0+) stores {trigger: message} for all outputs.
         1|foo|[1]|{"submitted": "submitted", "started": "started",
                    "succeeded": "succeeded", "x": "the quick brown"}
     """
@@ -82,18 +79,10 @@ class CylcWorkflowDBChecker:
         # Get workflow point format.
         try:
             self.db_point_fmt = self._get_db_point_format()
-            self.c7_back_compat_mode = False
-        except sqlite3.OperationalError as exc:
-            # BACK COMPAT: Cylc 7 DB (see method below).
-            # FROM: 7.x
-            # REMOVE AT: 8.7
-            try:
-                self.db_point_fmt = self._get_db_point_format_compat()
-                self.c7_back_compat_mode = True
-            except sqlite3.OperationalError:
-                with suppress(Exception):
-                    self.conn.close()
-                raise exc from None  # original error
+        except sqlite3.OperationalError:
+            with suppress(Exception):
+                self.conn.close()
+            raise
 
     def __enter__(self):
         return self
@@ -188,29 +177,6 @@ class CylcWorkflowDBChecker:
         ):
             return row[0]
 
-    def _get_db_point_format_compat(self):
-        """Query a Cylc 7 suite database for 'cycle point format'."""
-        # BACK COMPAT: Cylc 7 DB
-        # Workflows parameters table name change.
-        # from:
-        #    8.0.x
-        # to:
-        #    8.1.x
-        # remove at:
-        #    8.7
-        for row in self.conn.execute(
-            rf'''
-                SELECT
-                    value
-                FROM
-                    {CylcWorkflowDAO.TABLE_SUITE_PARAMS}
-                WHERE
-                    key==?
-            ''',  # nosec (table name is code constant)
-            ['cycle_point_format']
-        ):
-            return row[0]
-
     def workflow_state_query(
         self,
         task: Optional[str] = None,
@@ -259,10 +225,7 @@ class CylcWorkflowDBChecker:
         else:
             target_table = CylcWorkflowDAO.TABLE_TASK_STATES
             mask = "name, cycle, status"
-
-        if not self.c7_back_compat_mode:
-            # Cylc 8 DBs only
-            mask += ", flow_nums"
+        mask += ", flow_nums"
 
         stmt = rf'''
             SELECT
@@ -313,17 +276,13 @@ class CylcWorkflowDBChecker:
         for row in self.conn.execute(stmt, stmt_args):
             # name, cycle, status_or_outputs, [flow_nums]
             res = list(row[:3])
-            if row[2] is None:
-                # status can be None in Cylc 7 DBs
+            flow_nums = deserialise_set(row[3])
+            if flow_num is not None and flow_num not in flow_nums:
+                # skip result, wrong flow
                 continue
-            if not self.c7_back_compat_mode:
-                flow_nums = deserialise_set(row[3])
-                if flow_num is not None and flow_num not in flow_nums:
-                    # skip result, wrong flow
-                    continue
-                fstr = repr_flow_nums(flow_nums)
-                if fstr:
-                    res.append(fstr)
+            fstr = repr_flow_nums(flow_nums)
+            if fstr:
+                res.append(fstr)
             db_res.append(res)
 
         if target_table == CylcWorkflowDAO.TABLE_TASK_STATES:
